9b875cac0dce8bbc44d9799666940f3f36d93102
Kamil Balus <kamil.balus@synerise.com>
2019-09-11 09:51:44 +0200

    chore(modal): modal

--- packages/components/modal/src/__specs__/Modal.spec.tsx
+++ packages/components/modal/src/__specs__/Modal.spec.tsx
@@ -4,10 +4,29 @@ import Modal from '../index';
 
 describe('Modal', () => {
     const TITLE = 'Test';
+    const CANCEL_TEXT = 'CANCEL';
+
+    const onCancel = jest.fn();
 
     it('should render', () => {
-        const { getByText } = render(<Modal title={TITLE} />);
+        // ARRANGE
+        const { getByText } = render(<Modal visible={true} title={TITLE} />);
+
+        // ASSERT
         getByText(TITLE);
     });
+
+    it('onCancel should be called', () => {
+
+        // ARRANGE
+        const {getByText} = render(<Modal visible={true} onCancel={onCancel} />);
+
+        // ACT
+        fireEvent.click(getByText(CANCEL_TEXT));
+
+        // ASSERT
+        expect(onCancel).toHaveBeenCalled();
+    });
+
 });
 
