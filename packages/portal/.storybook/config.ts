016a6bbcb028250f58de37f98885fb8345b7196c
Daniel Kita <daniel.kita@synerise.com>
2019-09-25 14:17:17 +0200

    chore: add graphik to storybook preview

--- packages/portal/.storybook/config.ts
+++ packages/portal/.storybook/config.ts
@@ -2,6 +2,7 @@ import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
 import syneriseTheme from './theme';
+import './style/index.css';
 const req = require.context('../stories', true, /.stories.tsx$/);
 
 function loadStories() {
--- packages/portal/.storybook/theme.ts
+++ packages/portal/.storybook/theme.ts
@@ -2,6 +2,7 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'light',
+  fontBase: "'Graphik LCG Web', sans-serif",
   brandTitle: 'Synerise Design System',
   brandUrl: 'https://synerise.com',
   brandImage: 'https://synerise.com/synerise/assets/svg/logos/logo.svg',
