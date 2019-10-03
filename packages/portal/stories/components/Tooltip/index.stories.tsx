242f7fda0465b91da69a65ee4dbf552326837a83
Mykyta Vlasov <mykyta.vlasov@synerise.com>
2019-10-03 16:24:18 +0200

    fix(tooltip): add styling fixes to tooltip
    affects: @synerise/ds-tooltip, portal
    
    ISSUES CLOSED: STOR-64

--- packages/portal/stories/components/Tooltip/index.stories.tsx
+++ packages/portal/stories/components/Tooltip/index.stories.tsx
@@ -4,28 +4,50 @@ import { storiesOf } from '@storybook/react';
 import { boolean, text, select } from '@storybook/addon-knobs';
 import Tooltip from '@synerise/ds-tooltip';
 
+const wrapperStyles = {
+  padding: '40px',
+};
 
 const props = () => ({
-    title: text('Tooltip text', 'example text'),
-    placement: select('Placement', ['top', 'left', 'right', 'bottom',
-        'topLeft', 'topRight', 'bottomLeft', 'bottomRight', 'leftTop', 'leftBottom', 'rightTop', 'rightBottom'], 'top'),
-    trigger: select('Trigger', ['hover', 'focus', 'click', 'contextMenu'], 'hover'),
+  title: text('Tooltip text', 'example text'),
+  placement: select(
+    'Placement',
+    [
+      'top',
+      'left',
+      'right',
+      'bottom',
+      'topLeft',
+      'topRight',
+      'bottomLeft',
+      'bottomRight',
+      'leftTop',
+      'leftBottom',
+      'rightTop',
+      'rightBottom',
+    ],
+    'top'
+  ),
+  trigger: select('Trigger', ['hover', 'focus', 'click', 'contextMenu'], 'hover'),
 });
 
-
 storiesOf('Components|Tooltip', module)
-    .add('default', () => {
-        return (
-            <Tooltip {...props()}>
-                <span>Tooltip will show on mouse enter.</span>
-            </Tooltip>
-        )
-    })
+  .add('default', () => {
+    return (
+      <div style={wrapperStyles}>
+        <Tooltip {...props()}>
+          <span>Tooltip will show on mouse enter.</span>
+        </Tooltip>
+      </div>
+    );
+  })
 
-    .add('force visibility', () => {
-        return (
-            <Tooltip {...props()} visible={boolean('visible', true)}>
-                <span>Tooltip will show on mouse enter.</span>
-            </Tooltip>
-        )
-    });
+  .add('force visibility', () => {
+    return (
+      <div style={wrapperStyles}>
+        <Tooltip {...props()} visible={boolean('visible', true)}>
+          <span>Tooltip will show on mouse enter.</span>
+        </Tooltip>
+      </div>
+    );
+  });
