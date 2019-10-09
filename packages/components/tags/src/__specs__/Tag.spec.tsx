03eb502174fb25a218f94cee978e10776cd6c782
Adam Jean-Pierre Dolinski <aram.tgfan@synerise.com>
2019-10-09 13:15:42 +0200

    feat(tags): add Tag Component tests

--- packages/components/tags/src/__specs__/Tag.spec.tsx
+++ packages/components/tags/src/__specs__/Tag.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import { renderWithProvider } from '@synerise/ds-utils';
 import Button from '@synerise/ds-button';
 import { Tag, TagShape } from '../index';
