0bcef4ee6ac0037df21bbf953617301e893b7e13
Adam Jean-Pierre Dolinski <aram.tgfan@synerise.com>
2019-10-10 14:42:20 +0200

    feat(card): add story for CardGroup component

--- packages/portal/stories/components/Card/index.stories.tsx
+++ packages/portal/stories/components/Card/index.stories.tsx
@@ -1,49 +1,86 @@
 import * as React from 'react';
+import range from 'lodash/range';
 
 import { DSProvider } from '@synerise/ds-core';
 import Button from '@synerise/ds-button';
-import Card from '@synerise/ds-card';
+import Card, { CardGroup } from '@synerise/ds-card';
 
 import { storiesOf } from '@storybook/react';
-import { boolean, text } from '@storybook/addon-knobs';
+import { boolean, text, number } from '@storybook/addon-knobs';
+
+const init = () => {
+  const props = {
+    raised: boolean('Raised', false),
+    disabled: boolean('Disabled', false),
+    lively: boolean('Lively', false),
+    withHeader: boolean('With header', true),
+    showContent: boolean('Show content', true),
+    withHeaderSide: boolean('With header side children', true),
+    withIcon: text('With icon', ''),
+    compactHeader: boolean('Compact header', false),
+  };
+
+  return { props };
+};
+
+const renderCard = props => (
+  <Card
+    lively={props.lively}
+    disabled={props.disabled}
+    raised={props.raised}
+    withHeader={props.withHeader}
+    title="Card title"
+    description="Description of the card contents"
+    icon={props.withIcon}
+    compactHeader={props.compactHeader}
+    headerSideChildren={props.withHeaderSide && (
+      <Button type="primary">Button</Button>
+    )}
+  >
+    {props.showContent &&
+      <div style={{width: '100%', height: 300}}>
+        Wow so great, such content!1
+      </div>
+    }
+  </Card>
+);
 
 storiesOf('Components|Card', module)
-  .add('default', () => {
-    const raised = boolean('Raised', false);
-    const disabled = boolean('Disabled', false);
-    const lively = boolean('Lively', false);
-    const withHeader = boolean('With header', true);
-    const showContent = boolean('Show content', true);
-    const withHeaderSide = boolean('With header side children', true);
-    const withIcon = text('With icon', '');
-    const compactHeader = boolean('Compact header', false);
+  .add('Single', () => {
+    const { props } = init();
 
     return (
       <DSProvider code="en_GB">
-        <div style={{ background: '#f2f5f6', padding: '16px', display: 'flex' }}>
-          <div style={{ marginLeft: 6, marginRight: 6, width: '100%' }}>
-            <Card
-              lively={lively}
-              disabled={disabled}
-              raised={raised}
-              withHeader={withHeader}
-              title="Card title"
-              description="Description of the card contents"
-              icon={withIcon}
-              compactHeader={compactHeader}
-              headerSideChildren={withHeaderSide && (
-                <Button type="primary">Button</Button>
-              )}
-            >
-              {showContent &&
-                <div style={{width: '100%', height: 300}}>
-                  Wow so great, such content!1
-                </div>
-              }
-            </Card>
+        <div style={{margin: 24}}>
+          <h3>Single card</h3>
+          <div style={{ paddingTop: 12, width: '100%' }}>
+            {renderCard(props)}
           </div>
         </div>
       </DSProvider>
     );
   })
+  
+  .add('Group', () => {
+    const { props } = init();
+    const itemsInGroup = number('Number of cards rendered', 9);
+    const rowItems = number('Items per row', 3);
+
+    return (
+      <DSProvider code="en_GB">
+        <div style={{ margin: 24 }}>
+          <h3>Card Group</h3>
+          <div style={{ paddingTop: 12, width: '100%' }}>
+            <CardGroup columns={rowItems}>
+              {range(1, itemsInGroup).map(i =>
+                <React.Fragment key={i}>
+                  {renderCard(props)}
+                </React.Fragment>
+              )}
+            </CardGroup>
+          </div>
+        </div>
+      </DSProvider>
+    );
+  }) 
 ;
