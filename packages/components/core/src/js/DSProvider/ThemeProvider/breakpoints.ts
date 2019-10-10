77d54a79a6c832fad307c4fb557e938c034b54b5
Daniel Kita <daniel.kita@synerise.com>
2019-10-10 13:44:36 +0200

    feat(core): add breakpoints and spaces config to ThemeProvider
    affects: @synerise/ds-core

---
+++ packages/components/core/src/js/DSProvider/ThemeProvider/breakpoints.ts
@@ -0,0 +1,18 @@
+export default {
+  small: {
+    min: 0,
+    max: 640,
+  },
+  medium: {
+    min: 641,
+    max: 1024,
+  },
+  large: {
+    min: 1025,
+    max: 1200,
+  },
+  xlarge: {
+    min: 1201,
+    max: 0,
+  },
+};
