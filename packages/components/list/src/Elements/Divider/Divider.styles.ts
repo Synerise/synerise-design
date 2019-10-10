893691463d4232925ca0397e04f81f0a913cadb1
Dariusz Osiadacz <dariusz.osiadacz@synerise.com>
2019-10-10 10:13:51 +0200

    feat(list): fix list styles and examples

--- packages/components/list/src/Elements/Divider/Divider.styles.ts
+++ packages/components/list/src/Elements/Divider/Divider.styles.ts
@@ -2,11 +2,7 @@ import styled from 'styled-components';
 import BaseDivider from '@synerise/ds-divider';
 
 export const Wrapper = styled.div`
-  padding: 0 12px;
+  padding: 0 20px;
 `;
 
-export const Divider = styled(BaseDivider)`
-  && {
-    margin: 8px 0;
-  }
-`;
+export const Divider = styled(BaseDivider)``;
