dd954b46b51ee8e74d31172c94b1787d16d236ea
Kamil Balus <kamil.balus@synerise.com>
2019-10-14 11:47:23 +0200

    chore: add icons

--- packages/components/icon/build/rename.js
+++ packages/components/icon/build/rename.js
@@ -4,7 +4,7 @@ const ICON_PATH = './src/icons/';
 const files = fs.readdirSync(ICON_PATH);
 
 const renameFun = files.map(i => {
-  return fs.rename(`${ICON_PATH + i}`, `${ICON_PATH + i.replace(/[^-]*-(.+)[0-9]+./g, '')}`, err => {
+  return fs.rename(`${ICON_PATH + i}`, `${ICON_PATH + i.replace(/^[0-9]+..../g, '')}`, err => {
     if (err) throw err;
   });
 });
