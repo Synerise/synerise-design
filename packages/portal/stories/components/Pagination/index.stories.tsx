dbfd75f2c08c52561c4e66f5fd133b258f5ef5dd
Kamil Balus <kamil.balus@synerise.com>
2019-10-15 18:36:01 +0200

    chore: add misign ico into pagination

--- packages/portal/stories/components/Pagination/index.stories.tsx
+++ packages/portal/stories/components/Pagination/index.stories.tsx
@@ -5,12 +5,27 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import centered from '@storybook/addon-centered/react';
 
+import Icon from '@synerise/ds-icon'
+import AngleLeftM from '@synerise/ds-icon/dist/icons/angle-left-m.svg';
+import AngleRighttM from '@synerise/ds-icon/dist/icons/angle-right-m.svg';
+
 const stories = storiesOf('Components|Pagination', module);
+
+
+function itemRender(current, type, originalElement) {
+    if (type === 'prev') {
+        return <Icon component={<AngleLeftM />}/>;
+    }
+    if (type === 'next') {
+        return <Icon component={<AngleRighttM />}/>;
+    }
+    return originalElement;
+}
 
 stories
     .addDecorator(centered)
     .add('default', () => {
-  return <Pagination onChange={action('onChange')} defaultCurrent={1} total={50} />;
+  return <Pagination onChange={action('onChange')} itemRender={itemRender} defaultCurrent={1} total={50} />;
 });
 
 export default stories;
