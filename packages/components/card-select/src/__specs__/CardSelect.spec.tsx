82a382813b6906dbc0335e53b021b5fc1baf05e6
Adam Jean-Pierre Dolinski <aram.tgfan@synerise.com>
2019-10-15 14:55:39 +0200

    feat(card-select): add onClick test

--- packages/components/card-select/src/__specs__/CardSelect.spec.tsx
+++ packages/components/card-select/src/__specs__/CardSelect.spec.tsx
@@ -4,7 +4,6 @@ import { renderWithProvider } from '@synerise/ds-utils';
 import CardSelect from '../index';
 
 describe('CardSelect', () => {
-  const onClick = jest.fn();
   it('should render', function() {
     // ARRANGE
     const { getByText } = renderWithProvider(
@@ -64,5 +63,29 @@ describe('CardSelect', () => {
 
     // ASSERT
     expect(onChange).toBeCalled();
+  });
+
+  it('should onClick be called', function() {
+    // ARRANGE
+    const onChange = jest.fn();
+    const onClick = jest.fn();
+    const TEST_ID = 'test-id';
+
+    const { getByTestId } = renderWithProvider(
+      <CardSelect
+        icon="ad-simple-push-l"
+        iconSize={32}
+        title="Disabled Card"
+        disabled={true}
+        onClick={onClick}
+      />
+    );
+
+    // ACT
+    fireEvent.click(getByTestId(TEST_ID));
+
+    // ASSERT
+    expect(onChange).toBeCalledTimes(0);
+    expect(onClick).toBeCalled();
   });
 });
