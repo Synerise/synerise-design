64fba886e967fd4c208614156991d85f3a529adb
Adamus Jean-Pierre Dolinkus <aram.tgfan@synerise.com>
2019-10-31 16:18:58 +0100

    chore(file-uploader): drop area

--- packages/components/file-uploader/src/FileUploader.styles.ts
+++ packages/components/file-uploader/src/FileUploader.styles.ts
@@ -6,14 +6,31 @@ export const Container = styled.div``;
 
 export const Description = styled(Typography.Text)`
   && {
+    margin: 8px 0 0;
+    display: block;
     color: ${(props): string => props.theme.palette['grey-500']};
+  }
+`;
+
+export const DropAreaContainer = styled.div``;
+
+export const DropArea = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px dashed ${(props): string => props.theme.palette['grey-300']};
+  padding: 15px 12px;
+  border-radius: 3px;
+
+  span {
+    display: inline-block;
+    margin: 0 0 0 12px;
   }
 `;
 
 export const Label = styled(Typography.Title)`
   && {
+    margin: 0 0 8px;
     font-size: 13px;
-    margin: 0;
   }
 `;
 
