076e398f8da1c29d0797578f5c08e3caea44ee3a
jakubroch <jaku.roch@synerise.com>
2019-10-31 10:54:01 +0100

    fix: structure for stories in new component process

--- scripts/create-component/stories-template/index.stories.tsx
+++ scripts/create-component/stories-template/index.stories.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { DSProvider } from '@synerise/ds-core';
 
 import {{componentName}} from '@synerise/ds-{{name}}';
 
-storiesOf('Components|{{componentName}}', module)
-.add('default', () => (
-    <DSProvider code="en_GB">
-        <{{componentName}} />
-    </DSProvider>
-));
+const stories = {
+  default: () => ({}),
+};
+
+export default {
+  name: 'Components|{{componentName}}',
+  config: {},
+  stories,
+  Component: {{componentName}},
+}
