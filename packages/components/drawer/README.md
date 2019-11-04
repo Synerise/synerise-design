8727277fa586b84a89a5b4b4fd1d193e37d0e585
Michał Osajda <michal.osajda@synerise.com>
2019-11-04 12:36:05 +0100

    feat(drawer): adds drawer component

---
+++ packages/components/drawer/README.md
@@ -0,0 +1,33 @@
+---
+id: drawer
+title: Drawer
+---
+
+Drawer UI Component
+
+## Installation
+
+```
+npm i @synerise/ds-drawer
+or
+yarn add @synerise/ds-drawer
+```
+
+## Usage
+
+```
+import Drawer from '@synerise/ds-drawer'
+
+<Drawer />
+
+```
+
+## Demo
+
+<iframe src="/storybook-static/iframe.html?id=components-drawer--default"></iframe>
+
+## API
+
+| Property | Description | Type | Default |
+| -------- | ----------- | ---- | ------- |
+
---
+++ packages/components/drawer/src/Drawer.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { DrawerProps as AntdDrawerProps } from 'antd/lib/drawer';
+import { AntdDrawer } from './Drawer.styles';
+import './style/index.less';
+
+export type DrawerProps = {
+  children: React.ReactChild;
+  title: string;
+  closable: string;
+};
+
+const Drawer: React.FC<DrawerProps & AntdDrawerProps> = ({ children, title, closable, ...drawerProps }) => {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <AntdDrawer {...drawerProps} closable={false}>
+      {children}
+    </AntdDrawer>
+  );
+};
+export default Drawer;
