e382301ebaae170aef932d028575ae4a9f43be91
Adam Jean-Pierre Dolinski <aram.tgfan@synerise.com>
2019-11-04 16:55:55 +0100

    chore(file-uploader): add accept filetype support, add file preview

---
+++ packages/components/file-uploader/src/FileView/FileView.styles.ts
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import Typography, { Label } from '@synerise/ds-typography';
+import { IconContainer } from '@synerise/ds-icon/dist/Icon.styles';
+
+export const FileViewContainer = styled.div`
+  background-color: ${(props): string => props.theme.palette.white};
+  border-radius: 3px;
+  border: 1px solid ${(props): string => props.theme.palette['grey-200']};
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  height: 48px;
+
+  img {
+    width: 32px;
+    height: 32px;
+  }
+`;
+
+export const PreviewImage = styled.div<{ source: string }>`
+  background: url('${(props): string => props.source}') 50% 50% no-repeat;
+  width: 32px;
+  height: 32px;
+  border-radius: 3px;
+  overflow: hidden;
+`;
+
+export const PlaceholderImage = styled.div`
+  background-color: ${(props): string => props.theme.palette['grey-200']};
+  width: 32px;
+  height: 32px;
+  border-radius: 3px;
+  padding: 4px;
+
+  ${IconContainer} {
+    fill: ${(props): string => props.theme.palette['grey-500']};
+  }
+`;
+
+export const Info = styled.div`
+  overflow: hidden;
+  margin: 0 0 0 10px;
+`;
+
+export const Name = styled(Label)`
+  && {
+    color: ${(props): string => props.theme.palette['grey-600']};
+    display: block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 100%;
+  }
+`;
+
+export const Size = styled(Typography.Text)`
+  && {
+    color: ${(props): string => props.theme.palette['grey-600']};
+  }
+`;
