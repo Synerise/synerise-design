9cbc1fbbaac8206661486b518c0531ba39cae175
Adamus Jean-Pierre Dolinkus <aram.tgfan@synerise.com>
2019-11-05 09:31:14 +0100

    chore(file-uploader); add support for file errors

--- packages/components/file-uploader/src/FileView/FileView.styles.ts
+++ packages/components/file-uploader/src/FileView/FileView.styles.ts
@@ -2,28 +2,6 @@ import styled, { SimpleInterpolation } from 'styled-components';
 import Typography, { Label } from '@synerise/ds-typography';
 import { IconContainer } from '@synerise/ds-icon/dist/Icon.styles';
 
-export const FileViewContainer = styled.div<{ disabled?: boolean; error?: string }>`
-  background-color: ${(props): string => props.theme.palette.white};
-  border-radius: 3px;
-  border: 1px solid ${(props): string => props.theme.palette['grey-200']};
-  display: flex;
-  align-items: center;
-  padding: 8px;
-  height: 48px;
-
-  img {
-    width: 32px;
-    height: 32px;
-  }
-
-  ${(props): SimpleInterpolation =>
-    props.disabled &&
-    `
-    background-color: ${props.theme.palette['grey-050']};
-    opacity: 0.4;
-  `};
-`;
-
 export const PreviewImage = styled.div<{ source: string }>`
   background: url('${(props): string => props.source}') 50% 50% no-repeat;
   width: 32px;
@@ -61,8 +39,42 @@ export const Name = styled(Label)`
   }
 `;
 
-export const Size = styled(Typography.Text)`
+export const SizeOrError = styled(Typography.Text)`
   && {
     color: ${(props): string => props.theme.palette['grey-600']};
   }
+`;
+
+export const FileViewContainer = styled.div<{ disabled?: boolean; error?: boolean }>`
+  background-color: ${(props): string => props.theme.palette.white};
+  border-radius: 3px;
+  border: 1px solid ${(props): string => props.theme.palette['grey-200']};
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  height: 48px;
+
+  img {
+    width: 32px;
+    height: 32px;
+  }
+
+  ${(props): SimpleInterpolation =>
+    props.disabled &&
+    `
+    background-color: ${props.theme.palette['grey-050']};
+    opacity: 0.4;
+  `};
+
+  ${(props): SimpleInterpolation =>
+    props.error &&
+    `
+    && {
+      border: 1px solid ${props.theme.palette['red-600']};
+
+      ${SizeOrError}, ${Name} {
+        color: ${props.theme.palette['red-600']};
+      }
+    }
+  `};
 `;
