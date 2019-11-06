c73be426b899391369959d6138e281bf68d171d7
Adam Jean-Pierre Dolinski <aram.tgfan@synerise.com>
2019-11-06 10:41:11 +0100

    chore(file-uploader): add more tests

--- packages/components/file-uploader/src/__specs__/FileUploader.spec.tsx
+++ packages/components/file-uploader/src/__specs__/FileUploader.spec.tsx
@@ -4,7 +4,15 @@ import { fireEvent } from '@testing-library/react';
 import FileUploader from '../index';
 import { defaultTexts } from '../FileUploader';
 
+window.URL.createObjectURL = function() {};
+
+const getFile = () => new File(['foo'], 'hello from the other side.png', {
+  type: 'image/png',
+});
+
 describe('FileUploader', () => {
+  const REMOVE_BUTTON_TESTID = 'fileview-remove';
+
   it('should render with drop area', () => {
     // ARRANGE
     const BUTTON_LABEL = 'UPLOAD NOW';
@@ -85,10 +93,7 @@ describe('FileUploader', () => {
     // ARRANGE
     const DROPAREA_TESTID = 'droparea';
     const onUpload = jest.fn();
-
-    const file = new File(['foo'], 'hello from the other side.png', {
-      type: 'image/png',
-    });
+    const file = getFile();
 
     const { getByTestId } = renderWithProvider(
       <FileUploader
@@ -108,5 +113,78 @@ describe('FileUploader', () => {
 
     // ASSERT
     expect(onUpload).not.toHaveBeenCalled();
+  });
+
+  it('should fire onRemove', () => {
+    // ARRANGE
+    const onRemove = jest.fn();
+    const file = getFile();
+
+    const { getByTestId } = renderWithProvider(
+      <FileUploader
+        mode="single"
+        files={[{file}]}
+        accept={['image/*']}
+        texts={defaultTexts}
+        onRemove={onRemove}
+      />
+    );
+
+    // ACT
+    fireEvent.click(getByTestId(REMOVE_BUTTON_TESTID));
+
+    // ASSERT
+    expect(onRemove).toHaveBeenCalled();
+  });
+
+  it('should not show remove button if `removable` prop is false', () => {
+    // ARRANGE
+    const file = getFile();
+
+    const { queryByTestId } = renderWithProvider(
+      <FileUploader
+        mode="single"
+        files={[{file}]}
+        texts={defaultTexts}
+        removable={false}
+      />
+    );
+
+    // ASSERT
+    expect(queryByTestId(REMOVE_BUTTON_TESTID)).toBeFalsy();
+  });
+
+  it('should not show remove button if file is disabled', () => {
+    // ARRANGE
+    const file = getFile();
+
+    const { queryByTestId } = renderWithProvider(
+      <FileUploader
+        mode="single"
+        files={[{file, disabled: true}]}
+        texts={defaultTexts}
+      />
+    );
+
+    // ASSERT
+    expect(queryByTestId(REMOVE_BUTTON_TESTID)).toBeFalsy();
+  });
+
+  it('should render individual file error', () => {
+    // ARRANGE
+    const ERROR_TEXT = 'SOME ERROR HAPPENED';
+    const file = getFile();
+
+    const { getByTestId, getByText } = renderWithProvider(
+      <FileUploader
+        mode="single"
+        files={[{file, error: ERROR_TEXT}]}
+        accept={['image/*']}
+        texts={defaultTexts}
+      />
+    );
+
+    // ASSERT
+    expect(getByText(ERROR_TEXT)).toBeTruthy();
   });
 });
