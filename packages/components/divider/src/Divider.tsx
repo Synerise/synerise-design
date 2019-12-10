1c608662ec1314e2cc182bb91bdeb20dd9d4ad34
Daniel Kita <daniel.kita@synerise.com>
2019-12-10 07:47:02 +0100

    fix: omit unneeded props which emit warnings
    affects: @synerise/ds-avatar, @synerise/ds-divider

--- packages/components/divider/src/Divider.tsx
+++ packages/components/divider/src/Divider.tsx
@@ -1,18 +1,24 @@
 import * as React from 'react';
 import '@synerise/ds-core/dist/js/style';
 import './style/index.less';
-import { DividerProps } from 'antd/lib/divider';
-import * as S from './Divider.styles';
+import AntdDivider, { DividerProps } from 'antd/lib/divider';
 
 interface Props extends DividerProps {
   marginTop?: number;
   marginBottom?: number;
 }
 
-const Divider: React.FC<Props> = ({ marginBottom, marginTop, ...antdDividerProps }) => {
+const Divider: React.FC<Props> = ({ marginBottom, marginTop, style, ...antdDividerProps }) => {
   return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <S.Divider {...antdDividerProps} marginBottom={marginBottom} marginTop={marginTop} />
+    <AntdDivider
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...antdDividerProps}
+      style={{
+        marginBottom,
+        marginTop,
+        ...style,
+      }}
+    />
   );
 };
 
