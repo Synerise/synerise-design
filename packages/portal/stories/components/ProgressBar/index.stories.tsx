ba4724895a4c8b0485dfbbc1f7fd53d956cbf1f5
Michał Osajda <michal.osajda@synerise.com>
2020-01-16 11:12:27 +0100

    fix(progress-bar): fix width of ant-progress-inner element

--- packages/portal/stories/components/ProgressBar/index.stories.tsx
+++ packages/portal/stories/components/ProgressBar/index.stories.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import ProgressBar from '@synerise/ds-progress-bar';
+import { PROGRESS_COLORS, PROGRESS_STROKE_LINECAP } from '@synerise/ds-progress-bar/dist/ProgressBar';
 
 const decorator = (storyFn) => (
   <div style={{ background: "#fff", padding: '16px', width: '600px' }}>
@@ -23,6 +24,7 @@ const stories = {
     percent: 60,
     showLabel: true,
     description: 'Description',
+    strokeColor: PROGRESS_COLORS.MARS,
   },
 };
 
