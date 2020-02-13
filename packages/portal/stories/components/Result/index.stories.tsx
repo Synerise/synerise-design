ca75d1c87dd6735925d24b8cc40c9fdd9d317094
Michał Osajda <michal.osajda@synerise.com>
2020-02-13 15:04:06 +0100

    feat(result): add stories with Result on Modal

--- packages/portal/stories/components/Result/index.stories.tsx
+++ packages/portal/stories/components/Result/index.stories.tsx
@@ -2,14 +2,12 @@ import * as React from 'react';
 import { text, boolean, select } from '@storybook/addon-knobs';
 
 import Button from '@synerise/ds-button';
-import Card from '@synerise/ds-card';
 import Result from '@synerise/ds-result';
 import List from '@synerise/ds-list';
-import Icon from '@synerise/ds-icon';
-import WthTemperatureM from '@synerise/ds-icon/dist/icons/WthTemperatureM';
 import Avatar from '@synerise/ds-avatar';
 import { action } from '@storybook/addon-actions';
 import { TextArea } from '@synerise/ds-input';
+import ModalProxy from '@synerise/ds-modal';
 
 const decorator = storyFn => (
   <div style={{ width: '520px' }}>
@@ -41,24 +39,19 @@ const textareaExample = (
   <TextArea resetMargin rows={8} disabled value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in dignissim odio, et luctus risus. Suspendisse vitae dignissim dolor. Nunc vel mollis massa. Cras laoreet nulla in velit elementum sollicitudin. Mauris ut erat nisi. Sed sapien ex, commodo sit amet neque varius, mollis egestas orci. Aenean maximus nibh nec arcu dapibus varius. Nulla lorem magna, maximus vel neque ac, consectetur finibus massa. Etiam bibendum augue in finibus tincidunt. Etiam dui risus, vehicula et massa sed, congue consectetur enim. Integer aliquet purus vitae elit congue, laoreet faucibus odio iaculis. In tincidunt viverra lacus id aliquet." />
 );
 
-const panelExample = (
+const listExample = (
   <List
-    header="Select chicken to cook"
     dataSource={[
       [
-        { text: 'Chicken ID 20291', disabled: true },
-        { text: 'Chicken ID 11925', disabled: false },
-        { text: 'Chicken ID 69209', disabled: false },
-        { text: 'Chicken ID 96022', disabled: false },
-        { text: 'Chicken ID 10921', disabled: false, danger: true },
+        { text: 'Automation name #1' },
+        { text: 'Automation name #2' },
+        { text: 'Automation name #3' },
+        { text: 'Automation name #4' },
+        { text: 'Automation name #5' },
       ],
     ]}
     renderItem={(item => (
-      <List.Item
-        disabled={item.disabled}
-        danger={item.danger}
-        icon={<Icon component={<WthTemperatureM />} />}
-      >
+      <List.Item>
         {item.text}
       </List.Item>
     ))}
@@ -69,7 +62,7 @@ const getDefaultProps = () => ({
   type: select('type', types, 'success'),
   customIcon: boolean('Custom icon', false),
   title: text('Title', 'File upload is in progress…'),
-  description: text('Description', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+  description: text('Description', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales elit ut justo tristique hendrerit.'),
   // panel: boolean('Show panel', true) && panelExample,
 });
 
@@ -94,20 +87,27 @@ const stories = {
       <Result {...props} customIcon={props.customIcon ? exampleAvatar : null} buttons={buttonSetExample} panel={textareaExample} />
     )
   },
-
-  insideCard: () => (
-    <Card>
-      <Result
-        type={select('type', types, 'success')}
-        onClose={() => alert('Close event')}
-        closable={boolean('Closable', true)}
-        title={text('Title', 'Chicken has been successfully cooked')}
-        description={text('Description', 'Would you like to cook any other chickens?')}
-        buttons={boolean('Show buttons', true) && buttonSetExample}
-        panel={boolean('Show panel', true) && panelExample}
-      />
-    </Card>
-  ),
+  withList: () => {
+    const props = getDefaultProps();
+    return (
+      <ModalProxy blank closable onCancel={action('onClick: Cancel')} visible size={'small'} footer={null}>
+        <Result {...props} customIcon={props.customIcon ? exampleAvatar : null} buttons={buttonSetExample} panel={listExample} />
+      </ModalProxy>
+    )
+  },
+  withModal: () => {
+    const props = getDefaultProps();
+    return (
+      <ModalProxy blank closable onCancel={action('onClick: Cancel')} visible size={'small'} footer={null}>
+        <Result {...props} customIcon={props.customIcon ? exampleAvatar : null} buttons={
+          <Button type="default" onClick={action('onClick: Close')}>
+            Close
+          </Button>
+        }
+        panel={textareaExample} />
+     </ModalProxy>
+    )
+  },
 };
 
 export default {
