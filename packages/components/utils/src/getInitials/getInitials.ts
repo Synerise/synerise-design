3f8703511174b955b30d65b10a7f71236f7752dc
Michał Osajda <michal.osajda@synerise.com>
2020-02-17 16:48:41 +0100

    fix(utils): simplify getInitials helper

--- packages/components/utils/src/getInitials/getInitials.ts
+++ packages/components/utils/src/getInitials/getInitials.ts
@@ -2,7 +2,7 @@ const getInitials = (firstname = '', lastname = ''): string => {
   const fName = typeof firstname === 'string' ? firstname : '';
   const lName = typeof lastname === 'string' ? lastname : '';
 
-  return `${fName}${lName}`.toUpperCase();
+  return `${fName[0] || ''}${lName[0] || ''}`.toUpperCase();
 };
 
 export default getInitials;
