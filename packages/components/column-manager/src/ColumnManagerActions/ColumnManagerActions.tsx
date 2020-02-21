02571897d6cb405a7f9f5f1fcad1700d4023a9ce
Michał Osajda <michal.osajda@synerise.com>
2020-02-21 12:39:03 +0100

    refactor(column-manager): refactor

--- packages/components/column-manager/src/ColumnManagerActions/ColumnManagerActions.tsx
+++ packages/components/column-manager/src/ColumnManagerActions/ColumnManagerActions.tsx
@@ -1,22 +1,121 @@
 import * as React from 'react';
 import Button from '@synerise/ds-button';
+import Modal from '@synerise/ds-modal/dist/Modal';
+import { Input, TextArea } from '@synerise/ds-input';
+import { Field, Form as FinalForm } from 'react-final-form';
 import * as S from './ColumnManagerActions.styles';
+import { ViewMeta } from '../ColumnManager';
 
-const ColumnManagerActions: React.FC = () => {
+type Props = {
+  onSave: (viewMeta: ViewMeta) => void;
+};
+
+const ColumnManagerActions: React.FC<Props> = ({ onSave }) => {
+  const [modalVisible, setModalVisible] = React.useState(false);
+
+  const formRef = React.useRef<HTMLFormElement>(null);
+
+  const handleCancel = React.useCallback(() => {
+    setModalVisible(false);
+  }, [setModalVisible]);
+
+  const handleShowModal = React.useCallback(() => {
+    setModalVisible(true);
+  }, [setModalVisible]);
+
+  let submit = React.useCallback(
+    values => {
+      const { name, description } = values;
+
+      onSave({ name, description });
+    },
+    [onSave]
+  );
+
+  const validate = React.useCallback((values: ViewMeta) => {
+    const errors: {
+      [k: string]: string;
+    } = {};
+
+    if (!values.name) {
+      errors.name = 'Must not be empty';
+    }
+
+    return errors;
+  }, []);
+
   return (
-    <S.ColumnManagerActions>
-      <Button type="secondary" mode="simple">
-        Save view
-      </Button>
-      <S.RightButtons>
-        <Button type="ghost" mode="simple">
-          Cancel
+    <>
+      <S.ColumnManagerActions>
+        <Button type="secondary" mode="simple" onClick={handleShowModal}>
+          Save view
         </Button>
-        <Button type="primary" mode="simple">
-          Apply
-        </Button>
-      </S.RightButtons>
-    </S.ColumnManagerActions>
+        <S.RightButtons>
+          <Button type="ghost" mode="simple">
+            Cancel
+          </Button>
+          <Button type="primary" mode="simple">
+            Apply
+          </Button>
+        </S.RightButtons>
+      </S.ColumnManagerActions>
+      <Modal
+        visible={modalVisible}
+        closable
+        onCancel={handleCancel}
+        title="Save view"
+        footer={
+          <div style={{ display: 'flex', flexWrap: 'nowrap' }}>
+            <div style={{ display: 'flex' }}>
+              <Button type="secondary" onClick={handleCancel}>
+                Cancel
+              </Button>
+              <Button type="primary" onClick={(event): void => submit(event)}>
+                Apply
+              </Button>
+            </div>
+          </div>
+        }
+      >
+        <FinalForm
+          onSubmit={submit}
+          validate={validate}
+          initialValues={{ name: '', description: '' }}
+          render={({ handleSubmit }): React.ReactNode => {
+            submit = handleSubmit;
+            return (
+              <form ref={formRef} onSubmit={handleSubmit}>
+                <Field
+                  name="name"
+                  render={({ input, meta }): React.ReactNode => (
+                    <Input
+                      // eslint-disable-next-line react/jsx-props-no-spreading
+                      {...input}
+                      label="View name"
+                      placeholder="Placeholder"
+                      errorText={meta.touched && meta.error}
+                    />
+                  )}
+                />
+                <Field
+                  name="description"
+                  render={({ input, meta }): React.ReactNode => (
+                    <TextArea
+                      // eslint-disable-next-line react/jsx-props-no-spreading
+                      {...input}
+                      label="View description"
+                      placeholder="Placeholder"
+                      rows={2}
+                      errorText={meta.touched && meta.error}
+                    />
+                  )}
+                />
+              </form>
+            );
+          }}
+        />
+      </Modal>
+    </>
   );
 };
 
--- packages/portal/stories/components/ColumnManager/index.stories.tsx
+++ packages/portal/stories/components/ColumnManager/index.stories.tsx
@@ -2,10 +2,71 @@ import * as React from 'react';
 
 import ColumnManager from '@synerise/ds-column-manager';
 import { action } from '@storybook/addon-actions';
+import { Column } from '@synerise/ds-column-manager/dist/ColumnManagerItem/ColumnManagerItem';
+
+
+const COLUMNS: Column[] = [
+  {
+    id: '0',
+    name: 'User name',
+    visible: true,
+    type: 'text',
+    fixed: 'left',
+  },
+  {
+    id: '1',
+    name: 'City',
+    visible: true,
+    type: 'text',
+    fixed: undefined,
+  },
+  {
+    id: '2',
+    name: 'Language',
+    visible: true,
+    type: 'text',
+    fixed: undefined,
+  },
+  {
+    id: '3',
+    name: 'Clients numbers',
+    visible: false,
+    type: 'number',
+    fixed: undefined,
+  },
+  {
+    id: '4',
+    name: 'Clients - last 3 years dasdad dasdas dasdad as',
+    visible: false,
+    type: 'list',
+    fixed: undefined,
+  },
+  {
+    id: '5',
+    name: 'Client - last week',
+    visible: false,
+    type: 'list',
+    fixed: undefined,
+  },
+  {
+    id: '6',
+    name: 'Client - all time',
+    visible: false,
+    type: 'list',
+    fixed: undefined,
+  },
+  {
+    id: '7',
+    name: 'Active',
+    visible: false,
+    type: 'boolean',
+    fixed: undefined,
+  },
+];
 
 const stories = {
   default: () => (
-    <ColumnManager hide={action('hide Column Manager')} showList={action('show Item Filter')} visible />
+    <ColumnManager hide={action('hide Column Manager')} showSavedViews={action('show Item Filter')} visible columns={COLUMNS}  onSave={action('SAVE')}/>
   ),
 };
 
