8c3b1fddae63c57b69d6762ede1adcc5a9d8686f
Michał Osajda <michal.osajda@synerise.com>
2020-02-24 08:58:34 +0100

    feat(column-manager): add story with ItemFilter

--- packages/components/column-manager/src/ColumnManager.tsx
+++ packages/components/column-manager/src/ColumnManager.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
+import { withTheme } from 'styled-components';
+import { FormattedMessage } from 'react-intl';
+import Typography from 'antd/lib/typography';
 import Drawer from '@synerise/ds-drawer';
 import Button from '@synerise/ds-button';
-import Typography from 'antd/lib/typography';
-import { CloseM, FolderM, SearchM } from '@synerise/ds-icon/dist/icons';
 import Icon from '@synerise/ds-icon';
-import { FormattedMessage } from 'react-intl';
+import ItemFilter from '@synerise/ds-item-filter';
+import { CloseM, FolderM, SearchM } from '@synerise/ds-icon/dist/icons';
 import SearchInput from '@synerise/ds-dropdown/dist/elements/SearchInput/SearchInput';
-import { withTheme } from 'styled-components';
+import { ItemFilterProps } from '@synerise/ds-item-filter/dist/ItemFilter';
 import ColumnManagerActions from './ColumnManagerActions/ColumnManagerActions';
 import ColumnManagerList from './ColumnManagerList/ColumnManagerList';
 import { Column } from './ColumnManagerItem/ColumnManagerItem';
 
 export type ColumnManagerProps = {
-  showSavedViews: () => void;
   hide: () => void;
   visible: boolean;
   onSave: (savedView: SavedView) => void;
@@ -23,9 +24,10 @@ export type ColumnManagerProps = {
   theme: {
     [k: string]: string;
   };
+  itemFilterConfig?: ItemFilterProps;
 };
 
-type SavedView = {
+export type SavedView = {
   meta: ViewMeta;
   columns: Column[];
 };
@@ -36,12 +38,12 @@ export type ViewMeta = {
 };
 
 const ColumnManager: React.FC<ColumnManagerProps> = ({
-  showSavedViews,
   hide,
   visible,
   theme,
   onSave,
   columns,
+  itemFilterConfig,
   texts = {
     title: <FormattedMessage id="DS.COLUMN-MANAGER.TITLE" />,
     searchPlaceholder: <FormattedMessage id="DS.COLUMN-MANAGER.SEARCH-PLACEHOLDER" />,
@@ -64,6 +66,7 @@ const ColumnManager: React.FC<ColumnManagerProps> = ({
   const [searchQuery, setSearchQuery] = React.useState<string>('');
   const [visibleList, setVisibleList] = React.useState(columns.filter(column => column.visible));
   const [hiddenList, setHiddenList] = React.useState(columns.filter(column => !column.visible));
+  const [itemFilterVisible, setItemFilterVisible] = React.useState(false);
 
   const searchResults = React.useMemo(() => {
     return [...visibleList, ...hiddenList].filter(column =>
@@ -105,6 +108,14 @@ const ColumnManager: React.FC<ColumnManagerProps> = ({
     }
   };
 
+  const hideItemFilter = React.useCallback(() => {
+    setItemFilterVisible(false);
+  }, [setItemFilterVisible]);
+
+  const showItemFilter = React.useCallback(() => {
+    setItemFilterVisible(true);
+  }, [setItemFilterVisible]);
+
   const handleSave = (viewMeta: { name: string; description: string }): void => {
     onSave({
       meta: viewMeta,
@@ -113,13 +124,13 @@ const ColumnManager: React.FC<ColumnManagerProps> = ({
   };
 
   return (
-    <Drawer visible={visible} width={338}>
+    <Drawer visible={visible} width={338} onClose={hide}>
       <Drawer.DrawerHeader>
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingBottom: 24 }}>
           <Typography.Title style={{ flex: 1, margin: 0 }} level={4}>
             {texts.title}
           </Typography.Title>
-          <Button type="ghost" mode="single-icon" onClick={showSavedViews}>
+          <Button type="ghost" mode="single-icon" onClick={showItemFilter}>
             <Icon component={<FolderM />} />
           </Button>
           <Button style={{ marginLeft: '8px' }} mode="single-icon" type="ghost" onClick={hide}>
@@ -150,6 +161,14 @@ const ColumnManager: React.FC<ColumnManagerProps> = ({
         </Drawer.DrawerContent>
         <ColumnManagerActions onSave={handleSave} texts={texts} />
       </Drawer.DrawerBody>
+      {itemFilterConfig && (
+        <ItemFilter
+          /* eslint-disable-next-line react/jsx-props-no-spreading */
+          {...itemFilterConfig}
+          visible={itemFilterVisible}
+          hide={hideItemFilter}
+        />
+      )}
     </Drawer>
   );
 };
