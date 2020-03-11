59d5648db8c2d05eabf9c3a24e1950af0d5e0be4
Tomasz Taranek <tomasz.taranek@synerise.com>
2020-03-11 13:19:34 +0100

    fix(avatar): ran prettier on the code
    affects: @synerise/ds-avatar, portal

--- packages/portal/stories/components/Avatar/index.stories.tsx
+++ packages/portal/stories/components/Avatar/index.stories.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import {text, select, number, boolean} from '@storybook/addon-knobs';
+import { text, select, number, boolean } from '@storybook/addon-knobs';
 import Avatar from '@synerise/ds-avatar';
 import Badge from '@synerise/ds-badge';
 import Icon from '@synerise/ds-icon';
-import DuplicateS from "@synerise/ds-icon/dist/icons/DuplicateS";
+import DuplicateS from '@synerise/ds-icon/dist/icons/DuplicateS';
 
 const wrapperStyles = {
   padding: '40px',
@@ -14,18 +14,11 @@ const wrapperStyles = {
   alignItems: 'center',
 };
 
-const decorator = (storyFn) => (
-  <div style={wrapperStyles}>
-    {storyFn()}
-  </div>
-);
+const decorator = storyFn => <div style={wrapperStyles}>{storyFn()}</div>;
 
-const shapes = ['circle', 'square'] as
-const ;
-const sizes = ['small', 'medium', 'large', 'extraLarge'] as
-const ;
-const statuses = ['error', 'default', 'success'] as
-const ;
+const shapes = ['circle', 'square'] as const;
+const sizes = ['small', 'medium', 'large', 'extraLarge'] as const;
+const statuses = ['error', 'default', 'success'] as const;
 const backgroundColors = [
   'red',
   'green',
@@ -41,38 +34,24 @@ const backgroundColors = [
   'violet',
 ] as const;
 const placeholderColors = backgroundColors;
-const backgroundColorHue = [
-  '900',
-  '800',
-  '700',
-  '600',
-  '500',
-  '400',
-  '300',
-  '200',
-  '100',
-  '050',
-] as const;
+const backgroundColorHue = ['900', '800', '700', '600', '500', '400', '300', '200', '100', '050'] as const;
 const placeholderColorHue = backgroundColorHue;
 const imgSrc = 'https://www.w3schools.com/howto/img_avatar.png';
 
-const image = [
-  '',
-  imgSrc
-] as
-const ;
+const image = ['', imgSrc] as const;
 
 const stories = {
   sizes: () => (
     <div
-      style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', paddingTop: 200}}>
+      style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', paddingTop: 200 }}
+    >
       <Avatar
         backgroundColor={select('backgroundColors', backgroundColors, 'blue')}
         backgroundColorHue={select('backgroundColorHue', backgroundColorHue, '800')}
         disabled={boolean('disabled', false)}
         size={'small'}
         shape={select('shape', shapes, 'circle')}
-        tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+        tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
         src={imgSrc}
         hasStatus={boolean('has status', true)}
       >
@@ -84,7 +63,7 @@ const stories = {
         disabled={boolean('disabled', false)}
         size={'medium'}
         shape={select('shape', shapes, 'circle')}
-        tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+        tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
         src={imgSrc}
         hasStatus={boolean('has status', true)}
       >
@@ -96,7 +75,7 @@ const stories = {
         disabled={boolean('disabled', false)}
         size={'large'}
         shape={select('shape', shapes, 'circle')}
-        tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+        tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
         src={imgSrc}
         hasStatus={boolean('has status', true)}
       >
@@ -108,7 +87,7 @@ const stories = {
         disabled={boolean('disabled', false)}
         size={'extraLarge'}
         shape={select('shape', shapes, 'circle')}
-        tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+        tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
         src={imgSrc}
         hasStatus={boolean('has status', true)}
       >
@@ -118,14 +97,15 @@ const stories = {
   ),
   statuses: () => (
     <div
-      style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', paddingTop: 200}}>
+      style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', paddingTop: 200 }}
+    >
       <Avatar
         backgroundColor={select('backgroundColors', backgroundColors, 'blue')}
         backgroundColorHue={select('backgroundColorHue', backgroundColorHue, '800')}
         disabled={boolean('disabled', false)}
         size={select('sizeString', sizes, 'small')}
         shape={select('shape', shapes, 'circle')}
-        tooltip={{name: 'Silvia Jobs'}}
+        tooltip={{ name: 'Silvia Jobs' }}
         hasStatus={boolean('has status', true)}
       >
         AK
@@ -137,7 +117,7 @@ const stories = {
           disabled={boolean('disabled', false)}
           size={'medium'}
           shape={select('shape', shapes, 'circle')}
-          tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+          tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
           hasStatus={boolean('has status', true)}
         >
           AK
@@ -150,7 +130,7 @@ const stories = {
           disabled={boolean('disabled', false)}
           size={'large'}
           shape={select('shape', shapes, 'circle')}
-          tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+          tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
           hasStatus={boolean('has status', true)}
         >
           AK
@@ -162,7 +142,7 @@ const stories = {
           backgroundColorHue={select('backgroundColorHue', backgroundColorHue, '800')}
           disabled={boolean('disabled', false)}
           size={'extraLarge'}
-          tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+          tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
           hasStatus={boolean('has status', true)}
         >
           AK
@@ -174,29 +154,28 @@ const stories = {
           backgroundColorHue={select('backgroundColorHue', backgroundColorHue, '800')}
           disabled={boolean('disabled', false)}
           size={'extraLarge'}
-          tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+          tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
           hasStatus={boolean('has status', true)}
           placeholderColor={select('placeholderColors', placeholderColors, 'blue')}
           placeholderColorHue={select('placeholderColorHue', placeholderColorHue, '300')}
-        >
-          
-        </Avatar>
+        ></Avatar>
       </Badge>
     </div>
   ),
   allOptions: () => (
     <div
-      style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', paddingTop: 200}}>
+      style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', paddingTop: 200 }}
+    >
       <Avatar
         backgroundColor={select('backgroundColors', backgroundColors, 'blue')}
         backgroundColorHue={select('backgroundColorHue', backgroundColorHue, '800')}
         disabled={boolean('disabled', false)}
         size={select('sizeString', sizes, 'small')}
         shape={select('shape', shapes, 'circle')}
-        tooltip={{name: 'Silvia Jobs'}}
+        tooltip={{ name: 'Silvia Jobs' }}
         hasStatus={boolean('has status', true)}
         iconComponent={
-          <Icon color={text('IconColor', '#fff')} size={number('iconSize', 20)} component={<DuplicateS/>}/>
+          <Icon color={text('IconColor', '#fff')} size={number('iconSize', 20)} component={<DuplicateS />} />
         }
       >
         AK
@@ -208,7 +187,7 @@ const stories = {
           disabled={boolean('disabled', false)}
           size={'medium'}
           shape={select('shape', shapes, 'circle')}
-          tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+          tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
           src={imgSrc}
           hasStatus={boolean('has status', true)}
         >
@@ -222,7 +201,7 @@ const stories = {
           disabled={boolean('disabled', false)}
           size={'large'}
           shape={select('shape', shapes, 'circle')}
-          tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+          tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
           src={select('withImage', image, '')}
           hasStatus={boolean('has status', true)}
         >
@@ -236,7 +215,7 @@ const stories = {
           disabled={boolean('disabled', false)}
           size={'extraLarge'}
           shape={'square'}
-          tooltip={{name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com'}}
+          tooltip={{ name: 'Silvia Jobs', email: 'silvia.jobs@gmail.com' }}
           src={imgSrc}
           hasStatus={boolean('has status', true)}
         >
