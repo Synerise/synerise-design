392f22168054fd12f8cfbbade43f5319debdcac7
Michał Osajda <michal.osajda@synerise.com>
2020-03-22 16:11:42 +0100

    feat(tooltip): add tutorial type

--- packages/components/tooltip/src/Tooltip.types.ts
+++ packages/components/tooltip/src/Tooltip.types.ts
@@ -2,11 +2,18 @@ import * as React from 'react';
 
 export type tooltipTypes = 'default' | 'icon' | 'largeSimple' | 'tutorial' | 'avatar';
 export type descriptionType = string | React.ReactNode;
+export type Tutorial = {
+  title: string | React.ReactNode;
+  description: string | React.ReactNode;
+};
 // TODO support descriptionType as array of element
 export default interface TooltipExtendedProps {
   type?: tooltipTypes;
   icon?: React.ReactNode;
   title?: string | React.ReactNode;
   description?: descriptionType;
+  tutorials?: Tutorial[];
+  tutorialAutoplay?: boolean;
+  tutorialAutoplaySpeed?: number;
   theme: { [k: string]: string };
 }
