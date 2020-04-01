98a91a16fbb05a1c0afd6b303d1da9702da826d8
Michał Osajda <michal.osajda@synerise.com>
2020-04-01 09:03:14 +0200

    feat(button): update props, export package version

--- packages/components/button/src/index.ts
+++ packages/components/button/src/index.ts
@@ -1,1 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import * as packageJson from '../package.json';
+
+const packageVersion = packageJson.version;
+
 export { default } from './Button';
+export { packageVersion };
