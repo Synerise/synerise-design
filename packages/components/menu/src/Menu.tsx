1b87c78fe2e5664c8d3fd4f97ea789755098349e
tomasztaranek <tomasz.taranek@synerise.com>
2020-04-09 09:39:32 +0200

    feat(menu): experimenting with react-virtualzied
    affects: @synerise/ds-menu

--- packages/components/menu/src/Menu.tsx
+++ packages/components/menu/src/Menu.tsx
@@ -1,103 +1,79 @@
 import * as React from 'react';
 import '@synerise/ds-core/dist/js/style';
 import './style/index.less';
+import { List } from 'react-virtualized';
 import * as S from './Menu.styles';
-
 import { TextItem } from './Elements';
 import SubMenuItem from './Elements/SubMenu/SubMenu';
-import { AntdMenuProps, AntdMenuState } from './Menu.types';
+import { AntdMenuProps } from './Menu.types';
 import MenuItem from './Elements/Item/MenuItem';
-import { MenuItemProps } from './Elements/Item/MenuItem.types';
 
-class Menu extends React.Component<AntdMenuProps, AntdMenuState> {
+class Menu extends React.Component<AntdMenuProps> {
   static Item: typeof TextItem = TextItem;
-  private virtualize = this.shouldVirtualizeItems();
 
-  // eslint-disable-next-line react/destructuring-assignment
-  state: AntdMenuState = { toRender: this.props.dataSource, itemsCountToShow: 7 }; // hardcoded - needs to be calculated in constructor
-
-  handleScroll = (e: any): void => {
-    const { dataLength, rowHeight } = this.props;
-    const element = e.target;
-    const { itemsCountToShow } = this.state;
-    const lastVisibleElement = Math.floor(element.scrollHeight / rowHeight);
-    if (itemsCountToShow < dataLength && element.scrollHeight > (lastVisibleElement - 1) * rowHeight) {
-      // eslint-disable-next-line no-console
-      console.log('Top level menu State changed', this.state);
-      this.setState((prevState: AntdMenuState) => ({ ...prevState, itemsCountToShow: prevState.itemsCountToShow + 1 }));
-    }
+  // contactRenderer = (list: any, rest: any): any => ({index,key,style,}: {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  contactRenderer = (list: any, rest: any): any => ({
+    index,
+  }: {
+    index: number;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  }): any => {
+    console.log(list);
+    const item = list[index];
+    return item.subMenu ? (
+      <SubMenuItem
+        parent={item.parent}
+        danger={item.danger}
+        prefixel={item.prefixel}
+        suffixel={item.suffixel}
+        disabled={item.disabled}
+        index={item.index}
+        text={item.text}
+        description={item.description}
+        subMenu={item.subMenu}
+        copyable={item.copyable}
+        copyHint={item.copyHint}
+        copyValue={item.copyValue}
+        key={`${item.text}${index}`} // eslint-disable-line react/no-array-index-key
+        {...rest}
+      />
+    ) : (
+      <MenuItem
+        parent={item.parent}
+        danger={item.danger}
+        prefixel={item.prefixel}
+        suffixel={item.suffixel}
+        disabled={item.disabled}
+        index={item.index}
+        text={item.text}
+        description={item.description}
+        subMenu={item.subMenu}
+        copyable={item.copyable}
+        copyHint={item.copyHint}
+        copyValue={item.copyValue}
+        key={`${item.text}${index}`} // eslint-disable-line react/no-array-index-key
+        {...rest}
+      />
+    );
   };
-
-  shouldVirtualizeItems(): boolean {
-    const { virtualized, rowHeight, height, dataLength } = this.props;
-    return !!virtualized && !!rowHeight && !!height && !!dataLength;
-  }
-
-  prepareDataForRender(items: MenuItemProps[]): MenuItemProps[] {
-    if (this.virtualize) {
-      const { itemsCountToShow } = this.state;
-      if (itemsCountToShow !== undefined && itemsCountToShow > 0) {
-        return items.slice(0, itemsCountToShow);
-      }
-    }
-    return items;
-  }
 
   render(): React.ReactNode {
     const { dataSource, dataLength, ordered, height, rowHeight, virtualized, ...rest } = this.props;
     return (
       <>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <S.AntdMenu
-          ordered={ordered}
-          mode="inline"
-          inlineIndent={24}
-          {...rest}
-          height={height}
-          onScroll={(e): void => this.handleScroll(e)}
-        >
-          {dataSource.map(items =>
-            this.prepareDataForRender(items).map((item: MenuItemProps, index: number) =>
-              item.subMenu ? (
-                <SubMenuItem
-                  parent={item.parent}
-                  danger={item.danger}
-                  prefixel={item.prefixel}
-                  suffixel={item.suffixel}
-                  disabled={item.disabled}
-                  index={item.index}
-                  text={item.text}
-                  description={item.description}
-                  subMenu={item.subMenu}
-                  ordered={ordered}
-                  copyable={item.copyable}
-                  copyHint={item.copyHint}
-                  copyValue={item.copyValue}
-                  key={`${item.text}${index}`} // eslint-disable-line react/no-array-index-key
-                  {...rest}
-                />
-              ) : (
-                <MenuItem
-                  parent={item.parent}
-                  danger={item.danger}
-                  prefixel={item.prefixel}
-                  suffixel={item.suffixel}
-                  disabled={item.disabled}
-                  index={item.index}
-                  text={item.text}
-                  description={item.description}
-                  subMenu={item.subMenu}
-                  ordered={ordered}
-                  copyable={item.copyable}
-                  copyHint={item.copyHint}
-                  copyValue={item.copyValue}
-                  show
-                  key={`${item.text}${index}`} // eslint-disable-line react/no-array-index-key
-                  {...rest}
-                />
-              )
-            )
-          )}
+        <S.AntdMenu ordered={ordered} mode="inline" inlineIndent={24} {...rest} height={height}>
+          {dataSource.map(items => (
+            // eslint-disable-next-line react/jsx-key
+            <List
+              width={200}
+              height={items.length < 10 ? items.length * 32 : 200}
+              rowCount={items.length}
+              rowHeight={32}
+              rowRenderer={this.contactRenderer(items, { ...rest })}
+            />
+          ))}
         </S.AntdMenu>
       </>
     );
