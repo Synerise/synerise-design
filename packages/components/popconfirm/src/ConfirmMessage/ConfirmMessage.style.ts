f3d9dd5e94c99ee49cb2af0071dd89e26c6a0095
Michał Osajda <michal.osajda@synerise.com>
2020-04-21 17:27:12 +0200

    feat(popconfirm): add confirm message

---
+++ packages/components/popconfirm/src/ConfirmMessage/ConfirmMessage.style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { IconContainer } from '@synerise/ds-icon/dist/Icon.styles';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Message = styled.div``;
+
+export const ConfirmMessageTitle = styled.span`
+  font-size: 14px;
+  line-height: 1.43;
+  color: #404c5a;
+`;
+
+export const ConfirmMessage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props): string => props.theme.palette.white};
+  padding: 16px;
+  border-radius: 3px;
+  box-shadow: 0 16px 32px 0 rgba(35, 41, 54, 0.1);
+  ${IconContainer} {
+    margin-right: 8px;
+  }
+`;
---
+++ packages/components/popconfirm/src/__specs__/ConfirmMessage.spec.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { renderWithProvider } from '@synerise/ds-utils/dist/testing';
+import { fireEvent } from '@testing-library/react';
+
+import Popconfirm from '../Popconfirm';
+
+const CONFIRM_MESSAGE = 'CONFIRM MESSAGE';
+const BUTTON_LABEL = 'BUTTON_LABEL';
+
+describe('Popcofirm.ConfirmMessage', () => {
+  it('should render child component', () => {
+    // ARRANGE
+    const {getByText} = renderWithProvider(<Popconfirm.ConfirmMessage title={CONFIRM_MESSAGE} onClick={(showConfirmMessage: () => void) => { showConfirmMessage() }}>
+      <button>{ BUTTON_LABEL }</button>
+    </Popconfirm.ConfirmMessage>);
+
+    // ASSERT
+    expect(getByText(BUTTON_LABEL)).toBeTruthy();
+  });
+
+  it('should show confirm message', () => {
+    // ARRANGE
+    const {getByText} = renderWithProvider(<Popconfirm.ConfirmMessage title={CONFIRM_MESSAGE} onClick={(showConfirmMessage: () => void) => { showConfirmMessage() }}>
+      <button>{ BUTTON_LABEL }</button>
+    </Popconfirm.ConfirmMessage>);
+
+    // ACT
+    fireEvent.click(getByText(BUTTON_LABEL));
+
+    // ASSERT
+    expect(getByText(CONFIRM_MESSAGE)).toBeTruthy();
+  });
+});
