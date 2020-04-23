4c0bc228ea6a3287854048cdb2762c155d0c16e0
Michał Osajda <michal.osajda@synerise.com>
2020-04-23 12:31:59 +0200

    feat(table): add table with virtualization

---
+++ packages/components/table/src/Virtualized/Virtualized.tsx
@@ -0,0 +1,163 @@
+import * as React from 'react';
+import { VariableSizeGrid as Grid } from 'react-window';
+import ResizeObserver from 'rc-resize-observer';
+import classNames from 'classnames';
+import Checkbox from '@synerise/ds-checkbox';
+import DSTable, { AntTableProps } from '../Table';
+
+interface Props extends AntTableProps<{ key: React.ReactText }> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  columns: any[];
+  scroll: {
+    x: number;
+    y: number;
+  };
+  onRowClick: (row: object) => void;
+  cellHeight: number;
+}
+
+const VirtualTable: React.FC<Props> = (props: Props) => {
+  const { columns, scroll, className, cellHeight = 52, rowSelection, onRowClick } = props;
+  const [tableWidth, setTableWidth] = React.useState(0);
+  let virtualColumns = columns;
+  if (rowSelection) {
+    virtualColumns = [
+      {
+        width: 64,
+        key: 'key',
+        dataIndex: 'key',
+        render: (key: string): React.ReactNode => {
+          return (
+            <Checkbox
+              checked={rowSelection.selectedRowKeys && rowSelection.selectedRowKeys.indexOf(key) >= 0}
+              onChange={(event): void => {
+                const { selectedRowKeys, onChange } = rowSelection;
+                if (event.target.checked) {
+                  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+                  // @ts-ignore
+                  onChange && onChange([...selectedRowKeys, key], props.dataSource);
+                } else {
+                  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+                  // @ts-ignore
+                  onChange &&
+                    onChange(
+                      selectedRowKeys.filter(k => k !== key),
+                      props.dataSource
+                    );
+                }
+              }}
+            />
+          );
+        },
+      },
+      ...virtualColumns,
+    ];
+  }
+
+  const widthColumnCount = virtualColumns.filter(({ width }) => !width).length;
+  const mergedColumns = virtualColumns?.map(column => {
+    if (column.width) {
+      return column;
+    }
+
+    return {
+      ...column,
+      width: Math.floor(tableWidth / widthColumnCount),
+    };
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const gridRef = React.useRef<any>();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [connectObject] = React.useState<any>(() => {
+    const obj = {};
+    Object.defineProperty(obj, 'scrollLeft', {
+      get: () => null,
+      set: (scrollLeft: number) => {
+        if (gridRef.current) {
+          gridRef.current.scrollTo({ scrollLeft });
+        }
+      },
+    });
+
+    return obj;
+  });
+
+  const resetVirtualGrid = (): void => {
+    gridRef.current.resetAfterIndices({
+      columnIndex: 0,
+      shouldForceUpdate: false,
+    });
+  };
+
+  React.useEffect(() => resetVirtualGrid, []);
+  React.useEffect(() => resetVirtualGrid, [tableWidth]);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const renderVirtualList = (rawData: object[], { scrollbarSize, ref, onScroll }: any): React.ReactNode => {
+    // eslint-disable-next-line no-param-reassign
+    ref.current = connectObject;
+
+    return (
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      <Grid
+        ref={gridRef}
+        className="virtual-grid"
+        columnCount={mergedColumns.length}
+        columnWidth={(index: number): number => {
+          const { width } = mergedColumns[index];
+          return index === mergedColumns.length - 1 ? width - scrollbarSize - 1 : width;
+        }}
+        height={scroll.y}
+        rowCount={rawData.length}
+        rowHeight={(): number => cellHeight}
+        width={tableWidth}
+        onScroll={({ scrollLeft }: { scrollLeft: number }): void => {
+          onScroll({ scrollLeft });
+        }}
+      >
+        {/* eslint-disable-next-line @typescript-eslint/explicit-function-return-type */}
+        {({ columnIndex, rowIndex, style }: { columnIndex: number; rowIndex: number; style: object }) => (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+          <div
+            className={classNames('virtual-table-cell', {
+              'virtual-table-cell-last': columnIndex === mergedColumns.length - 1,
+              'ant-table-selection-column': columnIndex === 0 && rowSelection,
+            })}
+            onClick={(): void => onRowClick(rawData[rowIndex])}
+            style={style}
+          >
+            {mergedColumns[columnIndex].render
+              ? mergedColumns[columnIndex].render(
+                  rawData[rowIndex][mergedColumns[columnIndex].dataIndex],
+                  rawData[rowIndex]
+                )
+              : rawData[rowIndex][mergedColumns[columnIndex].dataIndex]}
+          </div>
+        )}
+      </Grid>
+    );
+  };
+
+  return (
+    <ResizeObserver
+      onResize={({ width }): void => {
+        setTableWidth(width);
+      }}
+    >
+      <DSTable
+        {...props}
+        className={classNames(className, 'virtual-table')}
+        columns={mergedColumns}
+        rowSelection={rowSelection}
+        pagination={false}
+        components={{
+          body: renderVirtualList,
+        }}
+      />
+    </ResizeObserver>
+  );
+};
+
+export default VirtualTable;
--- packages/portal/stories/components/Table/index.stories.tsx
+++ packages/portal/stories/components/Table/index.stories.tsx
@@ -3,6 +3,8 @@ import faker from 'faker';
 import { action } from '@storybook/addon-actions';
 import { boolean } from '@storybook/addon-knobs';
 import Table from '@synerise/ds-table';
+import VirtualTable from '@synerise/ds-table/dist/Virtualized/Virtualized';
+import { withState } from '@dump247/storybook-state';
 
 const decorator = (storyFn) => (
   <div style={{ padding: 20, width: '100%', minWidth: '1500px' }}>
@@ -55,6 +57,30 @@ const stories = {
     rowSelection: (boolean('show selection', false) ? rowSelection : undefined),
     onSearch: action('onSearch'),
   }),
+  withVirtualization: withState({
+    selectedRowKeys: [],
+  })(({store}) => {
+    return (
+      <div style={{width: 500}}>
+        <VirtualTable
+          scroll={{ x: 0, y: 480 }}
+          rowSelection={{ onChange: (selectedRows, records) => store.set({selectedRowKeys: selectedRows}), selectedRowKeys: store.state.selectedRowKeys }}
+          dataSource={dataSource}
+          columns={[{name: 'Segment name', key: 'name', dataIndex: 'name'}, {name: 'Age', key: 'age', dataIndex: 'age'}]}
+          rowKey="key"
+          pagination={false}
+          onRowClick={(record) => {
+            if(store.state.selectedRowKeys.indexOf(record.key) >= 0) {
+              store.set({selectedRowKeys: store.state.selectedRowKeys.filter(k => k !== record.key)});
+            }else {
+              store.set({selectedRowKeys: [...store.state.selectedRowKeys, record.key]});
+            }
+          }}
+          cellHeight={64}
+        />
+      </div>
+    )
+  })
 };
 
 export default {
