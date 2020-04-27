51d9d079fc61cba9f0b2278f5b659735309b165e
Mykyta Vlasov <mykyta.vlasov@synerise.com>
2020-04-27 14:45:42 +0000

    fix(items-roll): minor fixes
    affects: @synerise/ds-items-roll, @synerise/ds-scrollbar, @synerise/ds-search, portal

--- packages/components/items-roll/src/ItemsRoll.types.ts
+++ packages/components/items-roll/src/ItemsRoll.types.ts
@@ -2,18 +2,8 @@ import * as React from 'react';
 import { IntlShape } from 'react-intl';
 import { MenuItemProps } from '@synerise/ds-menu/dist/Elements/Item/MenuItem.types';
 
-type ClickParam = {
-  key: string;
-  keyPath: string[];
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  item: any;
-  domEvent: Event;
-};
-
 export type ItemRollElement = MenuItemProps & {
   id: string;
-  onRemoveElement?: (id: string) => void;
-  onClick?: (el: ClickParam) => void;
   group?: string;
 };
 
@@ -42,6 +32,8 @@ export type ItemsRollProps = {
   maxToShowItems?: number;
   onClearAll?: () => void;
   onChangeSelection?: () => void;
+  onItemRemove?: (id: string) => void;
+  onItemClick?: (id: string) => void;
   style?: React.CSSProperties;
   showMoreStep?: number;
   texts?: {
