1681d2326ef6370af4d8be5c6c0f480016e06a8c
Michał Osajda <michal.osajda@synerise.com>
2020-05-05 14:52:40 +0200

    feat(table): add story with fixed columns

--- packages/portal/stories/components/Table/withFixedColumns.stories.tsx
+++ packages/portal/stories/components/Table/withFixedColumns.stories.tsx
@@ -3,12 +3,7 @@ import faker from 'faker';
 import { action } from '@storybook/addon-actions';
 import { boolean, select } from '@storybook/addon-knobs';
 import Table from '@synerise/ds-table';
-import {
-  MailM,
-} from '@synerise/ds-icon/dist/icons';
-import { TagShape } from '@synerise/ds-tags';
-import { COLUMNS } from './content/withAllCellTypes.data';
-
+import { COLUMNS } from './content/withFixedColumns.data';
 
 const decorator = (storyFn) => (
   <div style={{ padding: 20, width: '100vw', minWidth: '100%' }}>
@@ -19,31 +14,11 @@ const decorator = (storyFn) => (
 const dataSource = [...new Array(55)].map((i, k) => ({
   key: k + 1,
   name: faker.name.findName(),
-  active: faker.random.boolean(),
-  country: faker.random.arrayElement(['us', 'pl', 'de', 'it', 'es', 'ru']),
+  city: faker.random.arrayElement(['Berlin', 'London', 'Paris', 'Warsaw', 'New York', 'Denver']),
   age: (Math.random() * 50 + 10).toFixed(0),
-  address: faker.address.streetAddress(),
-  status: faker.random.arrayElement(['active', 'inactive', 'blocked']),
-  avatar: {
-    initials: 'AN',
-    icon: <MailM />,
-    title: 'Top 10 product add to cart',
-    status: 'active',
-    labels: [
-      'Text AB/X',
-      'Edited 11 Jun 2019 18:47',
-    ],
-  },
-  select: {
-    value: faker.random.arrayElement(['option 1', 'option 2', 'option 3']),
-    options: ['option 1', 'option 2', 'option 3'],
-  },
-  tag: {
-    label: 'status',
-    shape: TagShape.STATUS_NEUTRAL,
-  },
-  enabled: faker.random.boolean(),
-  editable: faker.name.findName(),
+  system: faker.random.arrayElement(['OSX', 'Windows', 'Linux']),
+  format: faker.random.arrayElement(['JPG', 'Zip', 'png']),
+  lang: faker.random.arrayElement(['pl', 'en', 'es']),
 }));
 
 const CELL_SIZES = {
@@ -53,7 +28,7 @@ const CELL_SIZES = {
 };
 
 const stories = {
-  default: () => ({
+  fixedColumns: () => ({
     title: `${dataSource.length} records`,
     dataSource,
     columns: COLUMNS,
@@ -73,7 +48,7 @@ const stories = {
 };
 
 export default {
-  name: 'Table|Table with all types of content',
+  name: 'Table|Table with fixed columns',
   decorator,
   stories,
   Component: Table,
