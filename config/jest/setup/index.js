9580ba9376c073304211a294873335b850dcdffb
Michał Osajda <michal.osajda@synerise.com>
2020-05-05 15:11:31 +0200

    chore(table): add mock of matchMedia in jest configuration

--- config/jest/setup/index.js
+++ config/jest/setup/index.js
@@ -6,3 +6,17 @@ import '@formatjs/intl-pluralrules/polyfill';
 import '@testing-library/jest-dom/extend-expect'
 
 import './enzyme.js'
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  }))
+})
