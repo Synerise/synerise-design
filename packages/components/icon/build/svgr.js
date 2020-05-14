5e182591c535489a8fc15955b7d466435078c669
Michał Osajda <michal.osajda@synerise.com>
2020-05-14 11:29:22 +0200

    feat(icon): add additional icons set

--- packages/components/icon/build/svgr.js
+++ packages/components/icon/build/svgr.js
@@ -7,6 +7,9 @@ const tpl = require('./template.js');
 const LIB_DIR = 'src/icons';
 const INDEX_DIST_FILE = `${LIB_DIR}/index.ts`;
 
+const ADDITIONAL_LIB_DIR = 'src/icons/additional';
+const ADDITIONAL_INDEX_DIST_FILE = `${LIB_DIR}/additional/index.ts`;
+
 const titlecase = input => input[0].toLocaleUpperCase() + input.slice(1);
 
 const pascalCase = value => {
@@ -34,29 +37,42 @@ if (!fs.existsSync(LIB_DIR)) {
   fs.mkdirSync(LIB_DIR);
 }
 
+if (!fs.existsSync(ADDITIONAL_LIB_DIR)) {
+  fs.mkdirSync(ADDITIONAL_LIB_DIR);
+}
+
 fs.writeFile(INDEX_DIST_FILE, '', err => {
   console.log(err);
 });
 
-glob('src/svg/*.svg', {}, function(er, files) {
-  for (let file of files) {
-    const componentName = pascalCaseFilename(file);
-    fs.readFile(file, 'UTF-8', (err, content) => {
-      svgr(
-        content,
-        {
-          template: tpl,
-          plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx', '@svgr/plugin-prettier'],
-        },
-        { componentName }
-      ).then(jsCode => {
-        fs.writeFile(`${LIB_DIR}/${componentName}.tsx`, jsCode, function(err) {
-          fs.appendFileSync(INDEX_DIST_FILE, `export { default as ${componentName} } from './${componentName}';\n`);
-          if (err) {
-            return console.log(err);
-          }
+fs.writeFile(ADDITIONAL_INDEX_DIST_FILE, '', err => {
+  console.log(err);
+});
+
+const buildIconsSet = (path, libDir, indexDistFile) => {
+  glob(path, {}, function(er, files) {
+    for (let file of files) {
+      const componentName = pascalCaseFilename(file);
+      fs.readFile(file, 'UTF-8', (err, content) => {
+        svgr(
+          content,
+          {
+            template: tpl,
+            plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx', '@svgr/plugin-prettier'],
+          },
+          { componentName }
+        ).then(jsCode => {
+          fs.writeFile(`${libDir}/${componentName}.tsx`, jsCode, function(err) {
+            fs.appendFileSync(indexDistFile, `export { default as ${componentName} } from './${componentName}';\n`);
+            if (err) {
+              return console.log(err);
+            }
+          });
         });
       });
-    });
-  }
-});
+    }
+  });
+};
+
+buildIconsSet('src/svg/*.svg', LIB_DIR, INDEX_DIST_FILE);
+buildIconsSet('src/svg/additional/*.svg', ADDITIONAL_LIB_DIR, ADDITIONAL_INDEX_DIST_FILE);
