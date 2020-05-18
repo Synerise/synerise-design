d0eec73d75ff8a39d5779e427336d93fc67da240
Michał Osajda <michal.osajda@synerise.com>
2020-05-18 10:12:32 +0200

    fix(table): fix column width on rerender

--- packages/portal/stories/components/Table/onModal.stories.tsx
+++ packages/portal/stories/components/Table/onModal.stories.tsx
@@ -7,6 +7,7 @@ import { withState } from '@dump247/storybook-state';
 import { text } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import Modal from '@synerise/ds-modal';
+import Button from '@synerise/ds-button';
 
 const decorator = storyFn => <div style={{ padding: 20, width: '100vw', minWidth: '100%' }}>{storyFn()}</div>;
 
@@ -27,6 +28,7 @@ const stories = {
   default: withState({
     searchValue: '',
     selectedRows: [],
+    modalVisible: false,
   })(({ store }) => {
 
     const filteredDataSource = () => {
@@ -42,51 +44,54 @@ const stories = {
     };
 
     return (
-      <Modal size='medium' visible title={"Table"} bodyStyle={{padding: 0}}>
-        <VirtualTable
-          scroll={{ y: 500, x: 0 }}
-          initialWidth={792}
-          title={`${filteredDataSource().length} ${text('Set name of table items', 'records')}`}
-          dataSource={filteredDataSource()}
-          columns={columns}
-          cellHeight={50}
-          rowKey={row => row.key}
-          selection={{
-            onChange: handleSelectRow,
-            selectedRowKeys: store.state.selectedRows,
-            selections: [
-              Table.SELECTION_ALL,
-              Table.SELECTION_INVERT,
-              {
-                key: 'select_custom',
-                onClick: action('select_custom'),
-                label: 'Select custom',
-              },
-            ],
-          }}
-          onRowClick={record => {
-            store.state.selectedRows.indexOf(record.key) >= 0
-              ? store.set({ selectedRows: store.state.selectedRows.filter(k => k !== record.key) })
-              : store.set({ selectedRows: [...store.state.selectedRows, record.key] });
-          }}
-          searchComponent={
-            <SearchInput
-              placeholder='Search'
-              clearTooltip='Clear'
-              onChange={value => {
-                console.log('value', value);
-                store.set({searchValue: value});
-              }}
-              value={store.state.searchValue}
-              onClear={() => {
-                console.log('clear');
-                store.set({ searchValue: '' });
-              }}
-              closeOnClickOutside={true}
-            />
-          }
-        />
-      </Modal>
+      <>
+        <Button type="primary" onClick={() => store.set({modalVisible: true})}>Show table</Button>
+        <Modal size='medium' visible={store.state.modalVisible} title={"Table"} bodyStyle={{padding: 0}} onCancel={() => {store.set({modalVisible: false})}}>
+          <VirtualTable
+            scroll={{ y: 500, x: 0 }}
+            initialWidth={792}
+            title={`${filteredDataSource().length} ${text('Set name of table items', 'records')}`}
+            dataSource={filteredDataSource()}
+            columns={columns}
+            cellHeight={50}
+            rowKey={row => row.key}
+            selection={{
+              onChange: handleSelectRow,
+              selectedRowKeys: store.state.selectedRows,
+              selections: [
+                Table.SELECTION_ALL,
+                Table.SELECTION_INVERT,
+                {
+                  key: 'select_custom',
+                  onClick: action('select_custom'),
+                  label: 'Select custom',
+                },
+              ],
+            }}
+            onRowClick={record => {
+              store.state.selectedRows.indexOf(record.key) >= 0
+                ? store.set({ selectedRows: store.state.selectedRows.filter(k => k !== record.key) })
+                : store.set({ selectedRows: [...store.state.selectedRows, record.key] });
+            }}
+            searchComponent={
+              <SearchInput
+                placeholder='Search'
+                clearTooltip='Clear'
+                onChange={value => {
+                  console.log('value', value);
+                  store.set({searchValue: value});
+                }}
+                value={store.state.searchValue}
+                onClear={() => {
+                  console.log('clear');
+                  store.set({ searchValue: '' });
+                }}
+                closeOnClickOutside={true}
+              />
+            }
+          />
+        </Modal>
+      </>
     );
   }),
 };
