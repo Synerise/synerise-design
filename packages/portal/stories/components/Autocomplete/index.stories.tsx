ca233e37a318fc848a7ca3c8f1609f430574ba0c
Jakub Sidor <jakub.sidor@synerise.com>
2020-05-25 11:58:15 +0200

    fix(autocomplete): add Error, Placeholder, getErrorText, change styles

--- packages/portal/stories/components/Autocomplete/index.stories.tsx
+++ packages/portal/stories/components/Autocomplete/index.stories.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 
-import { text } from '@storybook/addon-knobs';
+import { text, boolean } from '@storybook/addon-knobs';
 import Autocomplete from '@synerise/ds-autocomplete';
+import { findAllByPlaceholderText } from '@testing-library/react';
 
 const dataSource = ['First position', 'Second position'];
 
 const AutocompleteWithState: React.FC = () => {
   const [value, setValue] = React.useState<string>('');
   const [results, setResults] = React.useState<string[]>([]);
+  const hasLabel = text('Label','Label');
+  const hasDescription = text('Description','Description');
+  const errorMessage = text('Error Text', 'Error' );
+  const hasError = boolean('Set validation state',false);
+  const hasPlaceholder = text('Placeholder', 'Placeholder')
 
   const handleSearch = value => {
     let result;
@@ -17,10 +23,18 @@ const AutocompleteWithState: React.FC = () => {
       result = dataSource.filter(item => item.includes(value));
     }
 
-    const newResults = result.map(item => item.replace(value, `<strong>${value}</strong>`));
+    const newResults = result.map(item => item.replace(value, `<strong style="font-weight: 600;">${value}</strong>`));
 
     setResults(newResults);
   };
+
+  const getErrorText = (hasError:boolean):string =>{
+    if(hasError){
+      return errorMessage
+    }else{
+      return''
+    }
+  }
 
   const extractContent = (str: string) => {
     const span = document.createElement('span');
@@ -31,22 +45,24 @@ const AutocompleteWithState: React.FC = () => {
   return (
     <Autocomplete
       style={{ width: 200 }}
+      placeholder={hasPlaceholder}
       onSearch={handleSearch}
-      allowClear
-      label={text('label', 'Label')}
-      description={text('description', 'Description')}
-      errorText={text('errorText', 'Error message')}
+      label={hasLabel}
+      description={hasDescription}
+      errorText={getErrorText(hasError)}
+      error={hasError}
       onChange={(value: string) => setValue(extractContent(value))}
       value={value === 'undefined' ? '' : value}
     >
       {results.map(result => (
         <Autocomplete.Option key={result}>
-          <span dangerouslySetInnerHTML={{ __html: result }} />
+          <span style={{fontWeight:"400"}} dangerouslySetInnerHTML={{ __html: result }} />
         </Autocomplete.Option>
       ))}
     </Autocomplete>
   );
 };
+
 
 const stories = {
   default: () => <AutocompleteWithState />,
