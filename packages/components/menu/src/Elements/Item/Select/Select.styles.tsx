23502db71002be155483907f3823af7fd566359f
tomasztaranek <tomasz.taranek@synerise.com>
2020-05-29 13:58:33 +0200

    fix(menu): made select item focusable

--- packages/components/menu/src/Elements/Item/Select/Select.styles.tsx
+++ packages/components/menu/src/Elements/Item/Select/Select.styles.tsx
@@ -3,19 +3,17 @@ import * as React from 'react';
 import { SuffixWraper } from '../Text/Text.styles';
 import Text from '../Text/Text';
 
-
 // eslint-disable-next-line import/prefer-default-export
 export const SelectItem = styled(({ children, disabled, ...rest }) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Text disabled={disabled} tabIndex={-1} {...rest}>
+  <Text disabled={disabled} {...rest}>
     {children}
   </Text>
-))
-  // Safari doesn't support animating opacity yet, therefore I am using filter property to simulate fading
-  `
+))// Safari doesn't support animating opacity yet, therefore I am using filter property to simulate fading
+`
   & {
     ${SuffixWraper} > * {
-      filter: opacity(0); 
+      filter: opacity(0);
       transition: filter 0.3s ease-in-out, color 0.3s ease-in-out;
     }
   }
