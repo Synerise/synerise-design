d2e8ef27fde732697d525d81c7ac3861a2efb2af
Michał Osajda <michal.osajda@synerise.com>
2020-05-29 11:48:03 +0200

    feat(scrollbar): add inifinite scroll

--- packages/components/scrollbar/src/Scrollbar.styles.tsx
+++ packages/components/scrollbar/src/Scrollbar.styles.tsx
@@ -1,7 +1,46 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const ScrollbarContent = styled.div``;
 
-export const ScrollbarWrapper = styled.div<{ absolute?: boolean }>`
+export const ScrollbarWrapper = styled.div<{ absolute?: boolean; loading?: boolean }>`
   padding-right: ${(props): string => (props.absolute ? '' : '11px !important')};
+  & > * {
+    opacity: ${(props): string => props.loading ? '0.2' : '1'};
+    transition: all .25s ease-in-out;
+  }
 `;
+
+const spinnerAnimation = keyframes`
+  from {
+    transform: rotateZ(0deg);
+  }
+  
+  to {
+    transform: rotateZ(360deg);
+  }
+`;
+
+export const Loader = styled.div<{loading?: boolean}>`
+  width: 0;
+  height: 0;
+  position: sticky;
+  top: calc(50% - 12px);
+  left: calc(50% - 12px);
+  z-index: 99;
+  pointer-events: none;
+  display: flex;
+  opacity: ${(props): string => props.loading ? '1' : '0'};
+  transform-origin: 12px 12px;
+  transition: opacity .3s ease-in-out;
+  animation: ${spinnerAnimation} 1s forwards linear infinite;
+  svg {
+    color: ${(props): string => props.theme.palette['blue-600']};
+    fill: ${(props): string => props.theme.palette['blue-600']};
+  }
+\`;
+
+`;
+
+export const LoadTrigger = styled.span`
+  background: transparent;
+`
--- packages/components/scrollbar/src/Scrollbar.tsx
+++ packages/components/scrollbar/src/Scrollbar.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import './style/index.less';
 import * as S from './Scrollbar.styles';
+import SpinnerM from '@synerise/ds-icon/dist/icons/SpinnerM';
+import Icon from '@synerise/ds-icon';
 
 export type ScrollbarProps = {
   children: React.ReactNode | string;
@@ -9,14 +11,55 @@ export type ScrollbarProps = {
   maxHeight?: string | number;
   absolute?: boolean;
   onScroll?: (e: React.UIEvent) => void;
+  loading?: boolean;
+  hasMore?: boolean;
+  fetchData?: () => void;
 };
 
-const Scrollbar: React.FC<ScrollbarProps> = ({ children, classes, maxHeight, absolute = false, onScroll }) => {
+const Scrollbar: React.FC<ScrollbarProps> = ({ loading, hasMore, fetchData,children, classes, maxHeight, absolute = false, onScroll }) => {
+  // eslint-disable-next-line no-undef
+  const observer = React.useRef(new IntersectionObserver(entries => {
+      const first = entries[0];
+      if (first.isIntersecting && fetchData) {
+        fetchData();
+      }
+    },
+    { threshold: 1 }
+    )
+  );
+
+  const [element, setElement] = React.useState(null);
+
+  React.useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return (): void => {
+      if (currentElement) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+
   return (
     <PerfectScrollbar onScroll={onScroll} options={{ minScrollbarLength: 48 }}>
       <S.ScrollbarContent className={classes} style={{ maxHeight }}>
-        <S.ScrollbarWrapper absolute={absolute}>{children}</S.ScrollbarWrapper>
+        <S.ScrollbarWrapper absolute={absolute} loading={loading}>
+          {children}
+          {!loading && hasMore && (
+            <S.LoadTrigger ref={setElement as React.Ref<HTMLSpanElement>} />
+          )}
+        </S.ScrollbarWrapper>
       </S.ScrollbarContent>
+      <S.Loader loading={loading}>
+        <Icon component={<SpinnerM />} color="#6a7580" />
+      </S.Loader>
     </PerfectScrollbar>
   );
 };
