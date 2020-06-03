e75595fe4c1c558e58db9650c5fd30c3dcb034f3
Michał Osajda <michal.osajda@synerise.com>
2020-06-03 16:04:30 +0200

    feat(item-filter): add unit tests

---
+++ packages/components/alert/src/__specs__/Alert.spec.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { renderWithProvider } from '@synerise/ds-utils/dist/testing';
+
+import Alert from '../index';
+import { fireEvent } from '@testing-library/react';
+
+describe('Alert component', () => {
+  it('should render properly', () => {
+    // ARRANGE
+    const msg = "Success!";
+    const { getByText } = renderWithProvider(<Alert message={msg} type="success"/>);
+
+    // ASSERT
+    expect(getByText(msg)).toBeTruthy();
+  });
+
+  it('should render with description', () => {
+    // ARRANGE
+    const msg = "Success!";
+    const description = "Success description";
+    const { getByText } = renderWithProvider(<Alert message={msg} description={description} type="success"/>);
+
+    // ASSERT
+    expect(getByText(msg)).toBeTruthy();
+    expect(getByText(description)).toBeTruthy();
+  });
+
+  it('should render with showMoreLabel', () => {
+    // ARRANGE
+    const msg = "Success!";
+    const description = "Success description";
+    const showMoreLabel = "Show more";
+    const onShowMore = jest.fn();
+    const { getByText } = renderWithProvider(<Alert message={msg} description={description} showMoreLabel={showMoreLabel} onShowMore={onShowMore} type="success"/>);
+
+    // ASSERT
+    expect(getByText(msg)).toBeTruthy();
+    expect(getByText(description)).toBeTruthy();
+    expect(getByText(showMoreLabel)).toBeTruthy();
+  });
+
+  it('should call onShowMore callback', () => {
+    // ARRANGE
+    const msg = "Success!";
+    const description = "Success description";
+    const showMoreLabel = "Show more";
+    const onShowMore = jest.fn();
+    const { getByText } = renderWithProvider(<Alert message={msg} description={description} showMoreLabel={showMoreLabel} onShowMore={onShowMore} type="success"/>);
+
+    // ACT
+    const showMore = getByText(showMoreLabel);
+    fireEvent.click(showMore);
+
+    // ASSERT
+    expect(getByText(msg)).toBeTruthy();
+    expect(getByText(description)).toBeTruthy();
+    expect(getByText(showMoreLabel)).toBeTruthy();
+    expect(onShowMore).toBeCalled();
+  });
+})
