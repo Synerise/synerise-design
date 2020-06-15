c4f01ed448546bbbf411c22398c2314af2adb1cf
Jakub Sidor <jakub.sidor@synerise.com>
2020-06-15 14:10:46 +0200

    fix(autocomplete): fix cursor

--- packages/components/autocomplete/src/Autocomplete.styles.ts
+++ packages/components/autocomplete/src/Autocomplete.styles.ts
@@ -79,7 +79,7 @@ export const AutocompleteWrapper = styled.div`
       }
     }
     .ant-select-selection-placeholder {
-      padding-left: 4px;
+      padding-left: 3px;
     }
 
     .ant-select-dropdown {
@@ -98,4 +98,6 @@ export const AutocompleteWrapper = styled.div`
       font-weight: 500;
     }
   }
+  .ant-select-selection-search-input{
+  padding: 0 0 0 0px;
 `;
