b67434f9553eee173c0f8217a129be416303afc0
tomasztaranek <tomasz.taranek@synerise.com>
2020-06-16 09:56:31 +0200

    fix(autocomplete): fixes for default story
    affects: @synerise/ds-autocomplete

--- packages/portal/stories/components/Autocomplete/index.stories.tsx
+++ packages/portal/stories/components/Autocomplete/index.stories.tsx
@@ -2,23 +2,20 @@ import * as React from 'react';
 
 import { text, boolean } from '@storybook/addon-knobs';
 import Autocomplete from '@synerise/ds-autocomplete';
-import { findAllByPlaceholderText } from '@testing-library/react';
 import { escapeRegEx } from '@synerise/ds-utils';
-import { action } from '@storybook/addon-actions';
 
 const dataSource = ['First position', 'Second position'];
 
 const AutocompleteWithState: React.FC = () => {
   const [value, setValue] = React.useState<string>('');
   const [results, setResults] = React.useState<string[]>([]);
-  const label = text('Label','Label');
-  const description = text('Description','Description');
-  const errorMessage = text('Error Text', 'Error' );
-  const hasError = boolean('Set validation state',false);
-  const placeholder = text('Placeholder', 'Placeholder')
-  const [isFocus, setFocus] = React.useState(false)
+  const label = text('Label', 'Label');
+  const description = text('Description', 'Description');
+  const errorMessage = text('Error Text', 'Error');
+  const hasError = boolean('Set validation state', false);
+  const placeholder = text('Placeholder', 'Placeholder');
 
-  const renderWithHighlightedText = (highlight,item): React.ReactNode => {
+  const renderWithHighlightedText = (highlight, item): React.ReactNode => {
     if (highlight && typeof item === 'string') {
       const index = item.toLocaleLowerCase().indexOf(highlight.toLocaleLowerCase());
       if (index === -1) {
@@ -29,7 +26,7 @@ const AutocompleteWithState: React.FC = () => {
       const endOfQuery = startOfQuery + highlight.length;
       const resultArray = [
         item.substring(0, startOfQuery),
-        <span key={item} style={{fontWeight:600}} className="search-highlight">
+        <span key={item} style={{ fontWeight: 600 }} className="search-highlight">
           {item.substring(startOfQuery, endOfQuery)}
         </span>,
         item.substring(endOfQuery, item.length),
@@ -46,19 +43,16 @@ const AutocompleteWithState: React.FC = () => {
     } else {
       result = dataSource.filter(item => item.toLowerCase().includes(value.toLowerCase()));
     }
-
-    const newResults = result.map(item => renderWithHighlightedText(value,item));
-
-    setResults(newResults);
+    setResults(result);
   };
 
-  const getErrorText = (hasError:boolean):string =>{
-    if(hasError){
-      return errorMessage
-    }else{
-      return''
+  const getErrorText = (hasError: boolean): string => {
+    if (hasError) {
+      return errorMessage;
+    } else {
+      return '';
     }
-  }
+  };
 
   const extractContent = (str: string) => {
     const span = document.createElement('span');
@@ -73,22 +67,22 @@ const AutocompleteWithState: React.FC = () => {
       onSearch={handleSearch}
       label={label}
       description={description}
-      errorText={!isFocus && getErrorText(hasError)}
-      error={!isFocus && hasError}
-      onBlur={()=>{action ('I am blurred'); setFocus(false)}}
-      onFocus={()=>{action('I am focused'); setFocus(true)}}
-      onChange={(value: string) => setValue(extractContent(value))}
+      errorText={getErrorText(hasError)}
+      error={hasError}
+      onChange={(value: string) => {
+        setValue(extractContent(value));
+        handleSearch(extractContent(value));
+      }}
       value={value === 'undefined' ? '' : value}
     >
       {results.map(result => (
         <Autocomplete.Option key={result}>
-          <span style={{fontWeight:400}}>{result}</span>
+          <span style={{ fontWeight: 400 }}>{renderWithHighlightedText(value, result)}</span>
         </Autocomplete.Option>
       ))}
     </Autocomplete>
   );
 };
-
 
 const stories = {
   default: () => <AutocompleteWithState />,
