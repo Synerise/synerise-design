f33685fb8a53ce1373d9fde4dd43d27ecbfb1f14
Jakub Sidor <jakub.sidor@synerise.com>
2020-06-17 13:54:11 +0200

    fix(select): add placeholder and change width of input

--- packages/portal/stories/components/InputNumber/index.stories.tsx
+++ packages/portal/stories/components/InputNumber/index.stories.tsx
@@ -8,20 +8,22 @@ const stories = {
     const validationState = boolean('Set validation state', false)
     const message = 'Error'
     const [isFocus, setFocus] = React.useState(false)
+    const placeholder = text('Placeholder', 'Placeholder')
     return {
       min: number('min', 1),
       max: number('max', 10),
-      defaultValue: number('defaultValue', 3),
       onChange: action('onChange'),
       label: text('label', 'Label'),
       description: text('description', 'Description'),
       errorText:!isFocus && getErrorText(validationState,message),
       error:!isFocus && validationState,
+      placeholder: placeholder,
       onBlur:()=>{ action ('I am blurred'); setFocus(false) },
       onFocus: ()=>{ action('I am focused'); setFocus(true)},
     }
   },
 };
+const decorator = storyFn => <div style={{ width: '286px' }}>{storyFn()}</div>;
 const getErrorText = (error: boolean, errorText: string): string => {
   if (error) {
     return errorText;
@@ -33,5 +35,6 @@ const getErrorText = (error: boolean, errorText: string): string => {
 export default {
   name: 'Components|Input',
   stories,
+  decorator,
   Component: InputNumber,
 };
