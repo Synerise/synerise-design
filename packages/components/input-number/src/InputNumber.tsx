babbce7043883dbbd9d1043be4ad718b456a9d77
Jakub Sidor <jakub.sidor@synerise.com>
2020-06-17 12:24:38 +0200

    fix(input-number): fix styles, add validation with knobs,focus to inputs

--- packages/components/input-number/src/InputNumber.tsx
+++ packages/components/input-number/src/InputNumber.tsx
@@ -9,6 +9,7 @@ export interface Props extends InputNumberProps {
   errorText?: React.ReactNode | string;
   label?: React.ReactNode | string;
   description?: React.ReactNode | string;
+  error?: boolean;
 }
 
 const InputNumber: React.FC<Props> = ({ label, description, errorText, ...antdProps }) => {
