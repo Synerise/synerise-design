fe70f69121e91bd871887aac395ae4ac5fdb8d24
tomasztaranek <tomasz.taranek@synerise.com>
2020-06-25 16:00:11 +0200

    fix(select): resolving conflicts

--- packages/portal/stories/components/Select/index.stories.tsx
+++ packages/portal/stories/components/Select/index.stories.tsx
@@ -5,10 +5,10 @@ import Select from '@synerise/ds-select';
 import Scrollbar from '@synerise/ds-scrollbar';
 import Result from '@synerise/ds-result';
 import Icon from '@synerise/ds-icon';
-import {LaptopM } from '@synerise/ds-icon/dist/icons';
+import { LaptopM } from '@synerise/ds-icon/dist/icons';
 import theme from '@synerise/ds-core/dist/js/DSProvider/ThemeProvider/theme';
 import { TagShape } from '@synerise/ds-tags';
-import  * as S from './stories.styles';
+import * as S from './stories.styles';
 
 const decorator = storyFn => <div style={{ padding: '20px', width: '322px' }}>{storyFn()}</div>;
 
@@ -50,20 +50,31 @@ export const addonType = {
   none: 'none',
 };
 type SizeType = 'default' | 'small' | 'large';
-export function renderAddonComponent(suffixElementType: string) {
+export function renderAddonComponent(suffixElementType: string, labelText: string) {
   switch (suffixElementType) {
     case addonType.icon:
-      return <S.IconWrapper>
-        <Icon color={theme.palette['grey-600']} component={<LaptopM />} />
-      </S.IconWrapper>
+      return (
+        <S.IconWrapper>
+          <Icon color={theme.palette['grey-600']} component={<LaptopM />} />
+        </S.IconWrapper>
+      );
     case addonType.label:
-      return <S.Label>Label</S.Label>;
+      return <S.Label>{labelText}</S.Label>;
     case addonType.avatar:
-      return <S.AvatarWithMargin size="small" backgroundColor="green" backgroundColorHue="400" shape="square">
-        AK
-      </S.AvatarWithMargin>
+      return (
+        <S.AvatarWithMargin size="small" backgroundColor="green" backgroundColorHue="400" shape="square">
+          AK
+        </S.AvatarWithMargin>
+      );
     case addonType.tag:
-      return <S.TagAddon name="A" shape={TagShape.SINGLE_CHARACTER_SQUARE} color={theme.palette['cyan-200']} textColor={theme.palette['cyan-600']} />
+      return (
+        <S.TagAddon
+          name="A"
+          shape={TagShape.SINGLE_CHARACTER_SQUARE}
+          color={theme.palette['cyan-200']}
+          textColor={theme.palette['cyan-600']}
+        />
+      );
     default:
       return null;
       break;
@@ -72,9 +83,9 @@ export function renderAddonComponent(suffixElementType: string) {
 
 const stories = {
   default: () => {
-    const validationState = boolean('Set validation state', false)
-    const message = 'Error'
-    const [isFocus, setFocus] = React.useState(false)
+    const validationState = boolean('Set validation state', false);
+    const message = 'Error';
+    const [isFocus, setFocus] = React.useState(false);
     return {
       tooltip: text('tooltip', 'This is example tooltip!'),
       clearTooltip: text('Clear tooltip', 'Clear'),
@@ -91,8 +102,14 @@ const stories = {
       dropdownMenuStyle: object('dropdownMenuStyle', dropdownStyles),
       loading: boolean('loading', false),
       mode: select('mode', modes, 'default'),
-      onBlur:()=>{ action ('I am blurred'); setFocus(false) },
-      onFocus: ()=>{ action('I am focused'); setFocus(true)},
+      onBlur: () => {
+        action('I am blurred');
+        setFocus(false);
+      },
+      onFocus: () => {
+        action('I am focused');
+        setFocus(true);
+      },
       placeholder: text('placeholder', 'Please select value...'),
       size: select<'default' | 'small' | 'large'>('size', sizes as any, 'default'),
       showArrow: boolean('showArrow', false),
@@ -113,13 +130,17 @@ const stories = {
     defaultValue: 'a10',
     onChange: action('OnChange'),
     dropdownRender: menu => <Scrollbar maxHeight={256}>{menu}</Scrollbar>,
+    dropdownStyle: { paddingRight: '0' },
     notFoundContent: <Result type="no-results" noSearchResults description={'No results'} />,
     listHeight: '100%',
     children,
   },
   withPrefixAndSuffix: () => {
     const prefixType = select('Set prefix type', addonType, addonType.none);
+    const prefixLabelText = text('Set prefix label text', 'Prefix');
     const suffixType = select('Set suffix type', addonType, addonType.none);
+    const suffixLabelText = text('Set suffix label text', 'Prefix');
+
     return {
       allowClear: false,
       defaultActiveFirstOption: true,
@@ -130,8 +151,8 @@ const stories = {
       onChange: action('OnChange'),
       style: { width: '100%' },
       children: values.map(opt => <Option value={opt}>{opt}</Option>),
-      prefixel: renderAddonComponent(prefixType),
-      suffixel: renderAddonComponent(suffixType),
+      prefixel: renderAddonComponent(prefixType,prefixLabelText),
+      suffixel: renderAddonComponent(suffixType,suffixLabelText),
       notFoundContent: <Result type="no-results" noSearchResults description={'No results'} />,
     };
   },
