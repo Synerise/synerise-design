b147f05436df399653acef333fd662867cf206e9
tomasztaranek <tomasz.taranek@synerise.com>
2020-07-08 09:12:02 +0200

    fix(date-range-picker): removed unused code

--- packages/components/date-range-picker/src/RangeFilter/WeeklyFilter/WeeklyFilter.types.ts
+++
@@ -1,8 +0,0 @@
-import { IntlShape } from 'react-intl';
-import { Day } from 'RangeFilter/RangeFilter.types';
-
-export type Props = {
-  value: { [key: string]: Day };
-  onChange: (value: string) => void;
-  intl: IntlShape;
-};
--- packages/components/date-range-picker/src/TimeWindow/utils.ts
+++
@@ -1,6 +0,0 @@
-import * as moment from 'moment';
-
-export const formatTime = (seconds: number, format = 'HH:mm:ss') => {
-  return moment.utc(seconds * 1000).format(format);
-};
-
