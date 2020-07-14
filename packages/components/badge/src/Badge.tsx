66e3b6c17db8f88440da334f0db8577c86b184b8
Jakub Sidor <jakub.sidor@synerise.com>
2020-07-14 12:55:46 +0200

    fix(badge): add props to readme and add types

--- packages/components/badge/src/Badge.tsx
+++ packages/components/badge/src/Badge.tsx
@@ -4,16 +4,32 @@ import './style/index.less';
 import { BadgeProps as AntBadgeProps } from 'antd/lib/badge';
 import AntdBadge from './Badge.styles';
 
+type color =
+  | 'red'
+  | 'green'
+  | 'grey'
+  | 'yellow'
+  | 'blue'
+  | 'pink'
+  | 'mars'
+  | 'orange'
+  | 'fern'
+  | 'cyan'
+  | 'purple'
+  | 'violet';
+
+type colorHue = '900' | '800' | '700' | '600' | '500' | '400' | '300' | '200' | '100' | '050';
+
 export type Status = 'active' | 'inactive' | 'blocked' | 'processing' | undefined;
 
 export interface BadgeProps extends Omit<AntBadgeProps, 'status'> {
   flag?: boolean;
   status?: Status;
   outlined?: boolean;
-  backgroundColor?: string;
-  textColor?: string;
-  backgroundColorHue?: string;
-  textColorHue?: string;
+  backgroundColor?: color;
+  textColor?: color;
+  backgroundColorHue?: colorHue;
+  textColorHue?: colorHue;
 }
 
 const Badge: React.FC<BadgeProps> = ({
