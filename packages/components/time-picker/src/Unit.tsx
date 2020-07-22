ebde31812094d47848fd2a2c2b11e1b1eac5a53f
tomasztaranek <tomasz.taranek@synerise.com>
2020-07-22 08:27:04 +0200

    fix(time-picker): added a scroll to selected functionality
    affects: @synerise/ds-time-picker

--- packages/components/time-picker/src/Unit.tsx
+++ packages/components/time-picker/src/Unit.tsx
@@ -16,20 +16,41 @@ export type UnitProps = UnitConfig & {
 };
 
 const Unit: React.FC<UnitProps> = ({ options, disabled, value, unit, onSelect }) => {
-  const selected = value && dayjs(value).get(unit);
+  const [selected, setSelected] = React.useState(value && dayjs(value).get(unit));
+  const selectedCellRef = React.useRef<HTMLButtonElement>(null);
+  const unitContainerRef = React.useRef<HTMLDivElement>(null);
+  const [isFirstRender, setFirstRender] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    if (isFirstRender) {
+      setFirstRender(false);
+    }
+  }, [isFirstRender]);
+
+  React.useEffect(() => {
+    if (selectedCellRef.current && unitContainerRef.current) {
+      const offsetToParent = selectedCellRef.current.offsetTop - unitContainerRef.current.offsetTop;
+      const scrollBehaviour = isFirstRender ? 'auto' : 'smooth';
+      unitContainerRef.current.scrollTo({ top: offsetToParent, behavior: scrollBehaviour });
+    }
+  }, [selectedCellRef, selectedCellRef.current, unitContainerRef, unitContainerRef.current, selected]);
 
   return (
-    <S.Unit data-testid={`ds-time-picker-unit-${unit}`}>
+    <S.Unit data-testid={`ds-time-picker-unit-${unit}`} ref={unitContainerRef}>
       {options.map((option: number) => {
         const normalizedStringValue = option < 10 ? `0${option}` : option.toString();
         const isDisabled = disabled && disabled.includes(option);
-
+        const isSelected = selected === option;
         return (
           <S.Cell
-            key={option}
+            key={`${unit}-${option}`}
             disabled={isDisabled}
-            onClick={(): void => onSelect(option)}
-            active={selected === option}
+            onClick={(): void => {
+              onSelect(option);
+              setSelected(option);
+            }}
+            active={isSelected}
+            ref={isSelected ? selectedCellRef : null}
           >
             <S.CellText>{normalizedStringValue}</S.CellText>
           </S.Cell>
