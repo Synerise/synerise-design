8f603a09e8e0e16b3380189606e0b256bc9347bf
Michał Osajda <michal.osajda@synerise.com>
2020-07-23 16:18:09 +0200

    feat(utils): update colorByLetter to works witch Avatar

--- packages/components/utils/src/selectColorByLetter/selectColorByLetter.spec.ts
+++ packages/components/utils/src/selectColorByLetter/selectColorByLetter.spec.ts
@@ -30,5 +30,37 @@ describe('selectColorByLetter', () => {
     expect(selectColorByLetter('W')).toBe(theme.palette['pink-500']);
     expect(selectColorByLetter('X')).toBe(theme.palette['orange-300']);
     expect(selectColorByLetter('Y')).toBe(theme.palette['fern-200']);
+    expect(selectColorByLetter('*')).toBe(theme.palette['orange-700']);
+  });
+
+  it('shoud return correct object with color and hue', () => {
+    expect(selectColorByLetter(undefined, true)).toStrictEqual({color: 'orange', hue: '700'});
+    expect(selectColorByLetter('*', true)).toStrictEqual({color: 'orange', hue: '700'});
+
+    expect(selectColorByLetter('A', true)).toStrictEqual({color: 'pink', hue: '700'});
+    expect(selectColorByLetter('B', true)).toStrictEqual({color: 'pink', hue: '600'});
+    expect(selectColorByLetter('C', true)).toStrictEqual({color: 'violet', hue: '600'});
+    expect(selectColorByLetter('D', true)).toStrictEqual({color: 'violet', hue: '500'});
+    expect(selectColorByLetter('E', true)).toStrictEqual({color: 'blue', hue: '700'});
+    expect(selectColorByLetter('F', true)).toStrictEqual({color: 'blue', hue: '600'});
+    expect(selectColorByLetter('G', true)).toStrictEqual({color: 'blue', hue: '400'});
+    expect(selectColorByLetter('H', true)).toStrictEqual({color: 'blue', hue: '300'});
+    expect(selectColorByLetter('I', true)).toStrictEqual({color: 'cyan', hue: '700'});
+    expect(selectColorByLetter('J', true)).toStrictEqual({color: 'fern', hue: '600'});
+    expect(selectColorByLetter('K', true)).toStrictEqual({color: 'fern', hue: '500'});
+    expect(selectColorByLetter('L', true)).toStrictEqual({color: 'yellow', hue: '600'});
+    expect(selectColorByLetter('M', true)).toStrictEqual({color: 'orange', hue: '400'});
+    expect(selectColorByLetter('N', true)).toStrictEqual({color: 'yellow', hue: '500'});
+    expect(selectColorByLetter('O', true)).toStrictEqual({color: 'orange', hue: '600'});
+    expect(selectColorByLetter('P', true)).toStrictEqual({color: 'red', hue: '800'});
+    expect(selectColorByLetter('R', true)).toStrictEqual({color: 'grey', hue: '600'});
+    expect(selectColorByLetter('S', true)).toStrictEqual({color: 'grey', hue: '700'});
+    expect(selectColorByLetter('T', true)).toStrictEqual({color: 'pink', hue: '400'});
+    expect(selectColorByLetter('U', true)).toStrictEqual({color: 'fern', hue: '400'});
+    expect(selectColorByLetter('V', true)).toStrictEqual({color: 'blue', hue: '400'});
+    expect(selectColorByLetter('W', true)).toStrictEqual({color: 'pink', hue: '500'});
+    expect(selectColorByLetter('X', true)).toStrictEqual({color: 'orange', hue: '300'});
+    expect(selectColorByLetter('Y', true)).toStrictEqual({color: 'fern', hue: '200'});
+    expect(selectColorByLetter('*', true)).toStrictEqual({color: 'orange', hue: '700'});
   });
 });
