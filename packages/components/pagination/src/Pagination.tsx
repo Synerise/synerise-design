a563e0d00f107ab0a6bd8edb712cb9217a3c48b2
Michał Osajda <michal.osajda@synerise.com>
2020-07-28 11:28:25 +0200

    fix(pagination): fix pagination component

--- packages/components/pagination/src/Pagination.tsx
+++ packages/components/pagination/src/Pagination.tsx
@@ -1,4 +1,36 @@
+import * as React from 'react';
 import '@synerise/ds-core/dist/js/style';
 import './style/index.less';
+import AntPagination, { PaginationProps } from 'antd/lib/pagination';
+import Icon from '@synerise/ds-icon';
+import Button from '@synerise/ds-button';
+import { AngleLeftS, AngleRightS } from '@synerise/ds-icon/dist/icons';
 
-export { default } from 'antd/lib/pagination';
+const ITEM_RENDER_TYPE = {
+  prev: 'prev',
+  next: 'next',
+};
+
+const Pagination: React.FC<PaginationProps> = props => {
+  const renderItem = React.useCallback((current, type, originalElement) => {
+    if (type === ITEM_RENDER_TYPE.prev) {
+      return (
+        <Button mode="single-icon" type="ghost">
+          <Icon component={<AngleLeftS />} />
+        </Button>
+      );
+    }
+    if (type === ITEM_RENDER_TYPE.next) {
+      return (
+        <Button mode="single-icon" type="ghost">
+          <Icon component={<AngleRightS />} />
+        </Button>
+      );
+    }
+    return originalElement;
+  }, []);
+
+  return <AntPagination {...props} itemRender={renderItem} />;
+};
+
+export default Pagination;
