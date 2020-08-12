e7ee24241d9aa7abeba2af32bf60a8e6cc82f4f0
tomasztaranek <tomasz.taranek@synerise.com>
2020-08-12 16:44:59 +0200

    fix(collector): bump search and input component

--- packages/portal/stories/components/Collector/index.stories.tsx
+++ packages/portal/stories/components/Collector/index.stories.tsx
@@ -2,23 +2,50 @@ import * as React from 'react';
 
 import Collector from '@synerise/ds-collector';
 import { boolean, text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import { InfoFillS } from '@synerise/ds-icon/dist/icons';
+import Icon from '@synerise/ds-icon';
+import Tooltip from '@synerise/ds-tooltip';
+import theme from '@synerise/ds-core/dist/js/DSProvider/ThemeProvider/theme';
 
 const decorator = storyFn => <div style={{ width: '588px' }}>{storyFn()}</div>;
 const getSuggestions = () => {
   const result = [];
   for (let i = 10; i < 36; i++) {
-    for (let j = 0; j < 3; j++) {
-      result.push(`Options ${i.toString(36).toUpperCase()} - ${j}`);
+    for (let j = 0; j < 36; j++) {
+      result.push(`Option ${i.toString(36).toUpperCase()}${j.toString(36).toUpperCase()}`);
     }
   }
   return result;
 };
+const renderLabel = (text: string, tooltip: string) => {
+  return (
+    <div
+      style={{
+        maxWidth: '200px',
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+        color: theme.palette['grey-800'],
+        display: 'flex',
+        alignItems: 'center',
+        height: '26px'
+      }}
+    >
+      <span>{text}</span>{' '}
+      <Tooltip title={tooltip}>
+        <Icon component={<InfoFillS />} color={theme.palette['grey-400']} />
+      </Tooltip>
+    </div>
+  );
+};
 const stories = {
   default: () => {
+    const tooltipText = text('Set tooltip text', 'Tooltip');
+    const labelText = text('Set label', 'Label');
     return (
       <Collector
-        values={[]}
-        label={text('Set label', 'Label')}
+        selected={[]}
+        label={renderLabel(labelText, tooltipText)}
         disabled={boolean('Set disabled', false)}
         description={text('Set description', 'Description')}
         errorText={text('Set error text', '')}
@@ -28,9 +55,10 @@ const stories = {
           add: 'Add',
           cancel: 'Cancel',
           placeholder: 'Type value',
-          toSelect: "to select",
+          toSelect: 'to select',
           toNavigate: 'to navigate',
         }}
+        onConfirm={action('onConfirm')}
       />
     );
   },
