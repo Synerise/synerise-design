99169fcb68cd88098f5c98c3960ceb5f0734046a
Jakub Sidor <jakub.sidor@synerise.com>
2020-08-14 13:25:20 +0200

    fix(progress-bar): fix percentage of bars

--- packages/components/progress-bar/src/Multivalue/Multivalue.tsx
+++ packages/components/progress-bar/src/Multivalue/Multivalue.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid';
 import { MultivalueProps } from './Multivalue.types';
 import * as S from './Mutlivalue.styles';
+
+const normalizePercent = (value: number): number => {
+  if (value < 0) return 0;
+  if (value > 100) return 100;
+  return value;
+};
 
 const Multivalue: React.FC<MultivalueProps> = (props: MultivalueProps) => {
   const { values } = props;
@@ -11,7 +17,7 @@ const Multivalue: React.FC<MultivalueProps> = (props: MultivalueProps) => {
   return (
     <S.Container>
       {sortedByPercent.map(val => (
-        <S.Multivalue key={uuid()} color={val.color}  percent={val.percent} />
+        <S.Multivalue key={uuid()} color={val.color} percent={normalizePercent(val.percent)} />
       ))}
     </S.Container>
   );
