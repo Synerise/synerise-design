6f8ef67b36acf466459f65387a41eb84c60ad62f
Jakub Sidor <jakub.sidor@synerise.com>
2020-08-18 10:27:22 +0200

    fix(card-select): change paddings and fix problem with number columns

--- packages/components/card-select/src/CardSelectGroup/CardSelectGroup.styles.ts
+++ packages/components/card-select/src/CardSelectGroup/CardSelectGroup.styles.ts
@@ -1,23 +1,22 @@
 import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
-export const Container = styled.div<{width: string; columns: number}>`
-  width:${(props): string => props.width === 'large'? '588px':'338px'};
-  height:100%;
+export const Container = styled.div<{ width: string; columns: number }>`
+  width: ${(props): string => (props.width === 'large' ? '588px' : '338px')};
+  height: 100%;
   justify-content: justify;
   background-color: white;
-  padding: 24px ;
+  padding: 24px;
   display: grid;
   grid-template-columns: ${(props): string => new Array(props.columns).fill('1fr').join(' ')};
-  column-gap: 24px;
+  column-gap: ${(props): string => (props.width === 'large' ? '24px' : '16px')};
   row-gap: 24px;
-  
+
   .ds-card-select {
-    width: 100% ;
+    width: 100%;
     display: flex;
     flex: 1;
     max-width: 100%;
     min-width: 0;
   }
 `;
-
