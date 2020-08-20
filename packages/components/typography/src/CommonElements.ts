c162f113da6b46c841ddf448d7e1c8e35cfc8457
Michał Osajda <michal.osajda@synerise.com>
2020-08-20 08:36:34 +0200

    feat(typography): add missing h5 and h6 elements

--- packages/components/typography/src/CommonElements.ts
+++ packages/components/typography/src/CommonElements.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled, { css, FlattenInterpolation } from 'styled-components';
+import { h200, h300, h400, h700, h500, h600 } from './style/macro-utils';
 
 export const Description = styled.div<{ disabled?: boolean }>`
   color: ${(props): string => props.theme.palette['grey-600']};
@@ -15,4 +16,40 @@ export const Label = styled.label`
   font-weight: 500;
   display: block;
   cursor: pointer;
+`;
+
+export const H1 = styled.h1`
+  ${(): FlattenInterpolation<any> => css`
+    ${h700};
+  `}
+`;
+
+export const H2 = styled.h1`
+  ${(): FlattenInterpolation<any> => css`
+    ${h600};
+  `}
+`;
+
+export const H3 = styled.h1`
+  ${(): FlattenInterpolation<any> => css`
+    ${h500};
+  `}
+`;
+
+export const H4 = styled.h1`
+  ${(): FlattenInterpolation<any> => css`
+    ${h400};
+  `}
+`;
+
+export const H5 = styled.h1`
+  ${(): FlattenInterpolation<any> => css`
+    ${h300};
+  `}
+`;
+
+export const H6 = styled.h1`
+  ${(): FlattenInterpolation<any> => css`
+    ${h200};
+  `}
 `;
--- packages/components/typography/src/index.ts
+++ packages/components/typography/src/index.ts
@@ -2,4 +2,5 @@ import * as macro from './style/macro-utils';
 
 export { default } from './Typography';
 export { Description, ErrorText, Label } from './CommonElements';
+export { default as Title } from './Title';
 export { macro };
