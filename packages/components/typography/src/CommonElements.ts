32e7419b780f1716d2f9e034086736bba348f112
Michał Osajda <michal.osajda@synerise.com>
2020-08-20 08:46:43 +0200

    feat(typography): fix types in styles

--- packages/components/typography/src/CommonElements.ts
+++ packages/components/typography/src/CommonElements.ts
@@ -1,4 +1,4 @@
-import styled, { css, FlattenInterpolation } from 'styled-components';
+import styled, { css, FlattenInterpolation, ThemeProps } from 'styled-components';
 import { h200, h300, h400, h700, h500, h600 } from './style/macro-utils';
 
 export const Description = styled.div<{ disabled?: boolean }>`
@@ -19,37 +19,37 @@ export const Label = styled.label`
 `;
 
 export const H1 = styled.h1`
-  ${(): FlattenInterpolation<any> => css`
+  ${(): FlattenInterpolation<ThemeProps<unknown>> => css`
     ${h700};
   `}
 `;
 
-export const H2 = styled.h1`
-  ${(): FlattenInterpolation<any> => css`
+export const H2 = styled.h2`
+  ${(): FlattenInterpolation<ThemeProps<unknown>> => css`
     ${h600};
   `}
 `;
 
-export const H3 = styled.h1`
-  ${(): FlattenInterpolation<any> => css`
+export const H3 = styled.h3`
+  ${(): FlattenInterpolation<ThemeProps<unknown>> => css`
     ${h500};
   `}
 `;
 
-export const H4 = styled.h1`
-  ${(): FlattenInterpolation<any> => css`
+export const H4 = styled.h4`
+  ${(): FlattenInterpolation<ThemeProps<unknown>> => css`
     ${h400};
   `}
 `;
 
-export const H5 = styled.h1`
-  ${(): FlattenInterpolation<any> => css`
+export const H5 = styled.h5`
+  ${(): FlattenInterpolation<ThemeProps<unknown>> => css`
     ${h300};
   `}
 `;
 
-export const H6 = styled.h1`
-  ${(): FlattenInterpolation<any> => css`
+export const H6 = styled.h6`
+  ${(): FlattenInterpolation<ThemeProps<unknown>> => css`
     ${h200};
   `}
 `;
