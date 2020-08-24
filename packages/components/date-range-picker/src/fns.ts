d751c00ff4f281b0ee3617fc7278f34ccdbad205
tomasztaranek <tomasz.taranek@synerise.com>
2020-08-24 10:50:52 +0200

    chore(date-range-picker): removed ts ignore comments

--- packages/components/date-range-picker/src/fns.ts
+++ packages/components/date-range-picker/src/fns.ts
@@ -1,62 +1,22 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsStartOfMonth from "date-fns/startOfMonth";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsSetDate from "date-fns/setDate";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsSetMonth from "date-fns/setMonth";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsSetYear from "date-fns/setYear";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsStartOfDay from "date-fns/startOfDay";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsEndOfDay from "date-fns/endOfDay";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsAddYears from "date-fns/addYears";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsEndOfMonth from "date-fns/endOfMonth";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsIsSameMonth from "date-fns/isSameMonth";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsIsBefore from "date-fns/isBefore";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsIsAfter from "date-fns/isAfter";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsAddMonths from "date-fns/addMonths";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsFormat from "date-fns/format";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsIsSameYear from "date-fns/isSameYear";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsGetYear from "date-fns/getYear";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsAddHours from "date-fns/addHours";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsAddMinutes from "date-fns/addMinutes";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsAddSeconds from "date-fns/addSeconds";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsAddDays from "date-fns/addDays";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsAddWeeks from "date-fns/addWeeks";
 
 export { fnsAddSeconds };
