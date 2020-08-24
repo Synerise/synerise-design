404f33d51d754ada8f758dbc2ff2c4ce209bccfc
tomasztaranek <tomasz.taranek@synerise.com>
2020-08-24 10:49:14 +0200

    chore(date-range-picker): removed ts ignore
    affects: @synerise/ds-date-range-picker

--- packages/components/date-range-picker/src/dateUtils/set.ts
+++ packages/components/date-range-picker/src/dateUtils/set.ts
@@ -1,11 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsSetHours from "date-fns/setHours";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsSetMinutes from "date-fns/setMinutes";
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 import fnsSetSeconds from "date-fns/setSeconds";
 
 export default {
