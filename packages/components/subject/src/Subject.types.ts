cd3b587b05272f6ce3de3e8d98ce619babbd7285
Michał Osajda <michal.osajda@synerise.com>
2020-09-02 15:26:33 +0200

    test(subject): add unit tests

--- packages/components/subject/src/Subject.types.ts
+++ packages/components/subject/src/Subject.types.ts
@@ -3,18 +3,20 @@ import * as React from 'react';
 export const ALL_TYPES = ['event', 'parameter', 'context'] as const;
 export type SubjectType = typeof ALL_TYPES[number] | string;
 
-export type SubjectItem = {
-  id: React.ReactText;
-  name: string;
-  icon: React.ReactNode;
-};
+export type SubjectItem =
+  | {
+      id: React.ReactText;
+      name: string;
+      icon: React.ReactNode;
+    }
+  | undefined;
 
 export type SubjectProps = {
   showPreview: () => void;
   placeholder: string | React.ReactNode;
   iconPlaceholder: React.ReactNode;
   selectItem: (item: SubjectItem) => void;
-  selectedItem: SubjectItem;
+  selectedItem?: SubjectItem;
   type?: SubjectType;
   items: SubjectItem[];
   texts: {
