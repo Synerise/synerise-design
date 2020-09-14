6a247ebf71dab9e90b75727cf80f3522f0116c38
Michał Osajda <michal.osajda@synerise.com>
2020-09-14 14:59:12 +0200

    fix(grid): fix strorybook example

--- packages/portal/stories/components/Grid/index.stories.tsx
+++ packages/portal/stories/components/Grid/index.stories.tsx
@@ -2,14 +2,43 @@ import * as React from 'react';
 
 import Grid from '@synerise/ds-grid';
 import Card from '@synerise/ds-card';
-import { number } from '@storybook/addon-knobs';
 import Description, { DescriptionRow } from '@synerise/ds-description';
+import * as S from './Grid.styles';
+import { boolean } from '@storybook/addon-knobs';
 
 const stories = {
   default: () => {
     console.log('test');
     return (
       <div style={{width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, padding: 24}}>
+        <S.FixedGrid visible={boolean('Show grid', true)}>
+          <Grid>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={1} xs={1}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={1} xs={1}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={1} xs={1}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={1} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={1} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={1} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={1} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={1} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={1} md={1} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={1} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={0} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={0} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={0} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={0} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={0} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={0} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={0} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+            <Grid.Item xxl={1} xl={0} lg={0} md={0} sm={0} xs={0}><S.Item /></Grid.Item>
+          </Grid>
+        </S.FixedGrid>
         <Grid>
           <Grid.Item xxl={8} xl={4} lg={4} md={2} sm={2} xs={3}>
             <Card showContent title="Item #1">
@@ -19,7 +48,7 @@ const stories = {
                 <DescriptionRow label={'LG'} value={'Span 4 of 12'}/>
                 <DescriptionRow label={'MD'} value={'Span 2 of 8'}/>
                 <DescriptionRow label={'SM'} value={'Span 2 of 8'}/>
-                <DescriptionRow label={'XS'} value={'Span 3 of 3'}/>
+                <DescriptionRow label={'XS'} value={' Span 3 of 3'}/>
               </Description>
             </Card>
           </Grid.Item>
@@ -29,8 +58,8 @@ const stories = {
                 <DescriptionRow label={'XXL'} value={'Span 8 of 24'}/>
                 <DescriptionRow label={'XL'} value={'Span 8 of 16'}/>
                 <DescriptionRow label={'LG'} value={'Span 4 of 12'}/>
-                <DescriptionRow label={'MD'} value={'Span 2 of 8'}/>
-                <DescriptionRow label={'SM'} value={'Span 2 of 8'}/>
+                <DescriptionRow label={'MD'} value={'Span 4 of 8'}/>
+                <DescriptionRow label={'SM'} value={'Span 4 of 8'}/>
                 <DescriptionRow label={'XS'} value={'Span 3 of 3'}/>
               </Description>
             </Card>
