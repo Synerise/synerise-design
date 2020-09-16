bed55784cfed8e794b2cc04c1e9f1804e7c7b07c
tomasztaranek <tomasz.taranek@synerise.com>
2020-09-16 19:30:36 +0200

    refactor(page-heder): moved types to seperate files

--- packages/components/page-header/src/PageHeader.tsx
+++ packages/components/page-header/src/PageHeader.tsx
@@ -6,38 +6,7 @@ import { CloseS, ArrowLeftM } from '@synerise/ds-icon/dist/icons';
 import Button from '@synerise/ds-button/';
 import { withTheme } from 'styled-components';
 import * as S from './PageHeader.styles';
-
-export type PageHeaderProps = {
-  className?: string;
-  rightSide?: React.ReactNode;
-  children?: React.ReactNode;
-  bar?: React.ReactNode;
-  tabs?: React.ReactNode;
-  avatar?: React.ReactNode;
-  title?: React.ReactNode | string;
-  description?: React.ReactNode | string;
-  more?: React.ReactNode;
-  onGoBack?: () => void;
-  goBackIcon?: React.ReactNode;
-  onClose?: () => void;
-  isolated?: boolean;
-  inlineEdit?: {
-    name?: string;
-    value: string | number;
-    maxLength?: number;
-    handleOnChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    handleOnBlur?: React.FocusEventHandler<HTMLInputElement>;
-    handleOnEnterPress?: React.KeyboardEventHandler<HTMLInputElement>;
-    placeholder?: string;
-    size: 'small' | 'normal';
-    style?: {
-      [key: string]: string | number;
-    };
-    error?: boolean;
-    disabled?: boolean;
-    hideIcon?: boolean;
-  };
-};
+import { PageHeaderProps } from './PageHeader.types';
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
   const {
