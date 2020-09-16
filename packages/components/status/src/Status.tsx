e1053244b163a14b4d7a57de4c43f462b2d870d3
tomasztaranek <tomasz.taranek@synerise.com>
2020-09-16 19:48:15 +0200

    refactor(status): moved types to seperate files

--- packages/components/status/src/Status.tsx
+++ packages/components/status/src/Status.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
 import { TagShape } from '@synerise/ds-tags';
 import * as S from './Status.styles';
-
-export interface StatusProps {
-  label: string;
-  type: 'primary' | 'success' | 'warning' | 'danger' | 'disabled';
-  className?: string;
-  onClick?: () => void;
-}
+import { StatusProps } from './Status.types';
 
 const Status: React.FC<StatusProps> = ({ type, onClick, className, label }) => {
   return (
