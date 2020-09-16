faffcf370be960f7e3706da826201db41f83290d
tomasztaranek <tomasz.taranek@synerise.com>
2020-09-16 17:27:28 +0200

    refactor(description): moved types to seperate files

---
+++ packages/components/description/src/Description.types.ts
@@ -0,0 +1,8 @@
+export type DescriptionType = 'inline' | 'table';
+export type DescriptionRatio = '20-80' | '30-70' | '40-60' | '50-50' | '60-40' | '70-30' | '80-20';
+
+export interface DescriptionProps {
+  type?: DescriptionType;
+  ratio?: DescriptionRatio;
+  children: JSX.Element | JSX.Element[];
+}
---
+++ packages/components/description/src/Row/Star.types.ts
@@ -0,0 +1,4 @@
+export interface StarProps {
+  starType: 'active' | 'inactive';
+  hasPrefixEl: boolean;
+}
