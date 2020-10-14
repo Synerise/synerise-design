44e2dfed567f3437344a380bcf2796257657f2ac
Jakub Sidor <jakub.sidor@synerise.com>
2020-10-14 10:27:48 +0200

    fix(sidebar-object): add props to change margin

--- packages/components/sidebar-object/src/Elements/Overview/Overview.tsx
+++ packages/components/sidebar-object/src/Elements/Overview/Overview.tsx
@@ -56,6 +56,7 @@ const Overview: React.FC<OverviewObjectProps> = ({
         )}
       </S.HeaderWrapper>
         <Content
+          onFolderSelect={onFolderSelect}
           texts={texts}
           autoSize={autoSize}
           onDescriptionChange={onDescriptionChange}
