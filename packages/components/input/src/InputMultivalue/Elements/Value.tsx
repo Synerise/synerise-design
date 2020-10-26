14305353658334df5b4427272c30b825280fa73f
Jakub Sidor <jakub.sidor@synerise.com>
2020-10-26 14:19:37 +0100

    fix(input): add tooltip to inputMultivalue

--- packages/components/input/src/InputMultivalue/Elements/Value.tsx
+++ packages/components/input/src/InputMultivalue/Elements/Value.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Icon from '@synerise/ds-icon';
 import { CloseS } from '@synerise/ds-icon/dist/icons';
+import Tooltip from '@synerise/ds-tooltip'
 import * as S from '../InputMultivalue.styles';
 import { Props } from './Value.types';
 
@@ -19,10 +20,11 @@ const Value: React.FC<Props> = ({ disabled, key, onRemoveClick, value, focused }
       disabled={disabled}
       key={key}
       shrink={hovered}
-    >
+    ><Tooltip align={{ offset: [8,0] }} offset="small" title={value} visible={focused && hovered}>
       <S.ValueText shrink={hovered} disabled={disabled}>
         {value}
       </S.ValueText>
+    </Tooltip>
       <S.IconWrapper onClick={onRemoveClick}>
         <Icon className="remove" component={<CloseS />} />
       </S.IconWrapper>
