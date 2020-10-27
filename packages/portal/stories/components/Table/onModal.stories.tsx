b7e12c49e66e79fc17f1f58a21cb2bc869b8f04c
tomasztaranek <tomasz.taranek@synerise.com>
2020-10-27 10:55:22 +0100

    fix(portal): added icons in header knobs in table stories
    affects: portal

--- packages/portal/stories/components/Table/onModal.stories.tsx
+++ packages/portal/stories/components/Table/onModal.stories.tsx
@@ -9,7 +9,8 @@ import { action } from '@storybook/addon-actions';
 import Modal from '@synerise/ds-modal';
 import Button from '@synerise/ds-button';
 import Icon from '@synerise/ds-icon';
-import { AddM } from '@synerise/ds-icon/dist/icons';
+import { AddM, InfoFillS, VarTypeStringM } from '@synerise/ds-icon/dist/icons';
+import { renderWithIconInHeaders } from './helpers/helpers';
 
 const decorator = storyFn => <div style={{ padding: 20, width: '100vw', minWidth: '100%' }}>{storyFn()}</div>;
 
@@ -23,7 +24,9 @@ const columns = [
     title: 'User name',
     key: 'name',
     dataIndex: 'name',
-  }
+    icon: { component: <VarTypeStringM/>},
+    iconTooltip: { component: <InfoFillS/>},
+  },
 ];
 
 const stories = {
@@ -32,42 +35,55 @@ const stories = {
     selectedRows: [],
     modalVisible: false,
   })(({ store }) => {
-
     const filteredDataSource = () => {
       return !store.state.searchValue
         ? dataSource
         : dataSource.filter(record => {
-          return record.name.toLowerCase().includes(store.state.searchValue.toLowerCase());
-        });
+            return record.name.toLowerCase().includes(store.state.searchValue.toLowerCase());
+          });
     };
 
-    const handleSelectRow = (selectedRowKeys) => {
+    const handleSelectRow = selectedRowKeys => {
       store.set({ selectedRows: selectedRowKeys });
     };
 
     const selectEven = () => {
-      const evenRows = filteredDataSource().map(row => row.key).filter((key, index) => index % 2);
-      store.set({selectedRows: evenRows});
+      const evenRows = filteredDataSource()
+        .map(row => row.key)
+        .filter((key, index) => index % 2);
+      store.set({ selectedRows: evenRows });
     };
 
     return (
       <>
-        <Button type="primary" onClick={() => store.set({modalVisible: true})}>Show table</Button>
-        <Modal size='medium' visible={store.state.modalVisible} title={"Table"} bodyStyle={{padding: 0}} onCancel={() => {store.set({modalVisible: false})}}>
+        <Button type="primary" onClick={() => store.set({ modalVisible: true })}>
+          Show table
+        </Button>
+        <Modal
+          size="medium"
+          visible={store.state.modalVisible}
+          title={'Table'}
+          bodyStyle={{ padding: 0 }}
+          onCancel={() => {
+            store.set({ modalVisible: false });
+          }}
+        >
           <VirtualTable
             scroll={{ y: 500, x: 0 }}
             initialWidth={792}
             title={`${filteredDataSource().length} ${text('Set name of table items', 'results')}`}
             dataSource={filteredDataSource()}
-            columns={columns}
+            columns={renderWithIconInHeaders(columns, boolean('Set icons in headers', false))}
             cellHeight={50}
             rowKey={row => row.key}
-            headerButton={boolean('Show header button', false) && (
-              <Button type="ghost" mode="icon-label" onClick={action('Header button action')}>
-                <Icon component={<AddM />} />
-                {text('Header button label', 'Add row')}
-              </Button>
-            )}
+            headerButton={
+              boolean('Show header button', false) && (
+                <Button type="ghost" mode="icon-label" onClick={action('Header button action')}>
+                  <Icon component={<AddM />} />
+                  {text('Header button label', 'Add row')}
+                </Button>
+              )
+            }
             selection={{
               onChange: handleSelectRow,
               selectedRowKeys: store.state.selectedRows,
@@ -78,7 +94,7 @@ const stories = {
                   key: 'even',
                   label: 'Select even',
                   onClick: selectEven,
-                }
+                },
               ],
             }}
             onRowClick={record => {
@@ -88,11 +104,11 @@ const stories = {
             }}
             searchComponent={
               <SearchInput
-                placeholder='Search'
-                clearTooltip='Clear'
+                placeholder="Search"
+                clearTooltip="Clear"
                 onChange={value => {
                   console.log('value', value);
-                  store.set({searchValue: value});
+                  store.set({ searchValue: value });
                 }}
                 value={store.state.searchValue}
                 onClear={() => {
