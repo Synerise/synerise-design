7d90233d8993bef84bab6854e6cdce52460f5fa2
Jakub Sidor <jakub.sidor@synerise.com>
2020-11-12 12:19:43 +0100

    fix(status): add customStatus and option to change color

--- packages/portal/stories/components/Status/index.stories.tsx
+++ packages/portal/stories/components/Status/index.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { boolean, select } from '@storybook/addon-knobs';
+import { boolean, select, text } from '@storybook/addon-knobs';
 import Status from '@synerise/ds-status';
 import { TagShape } from '@synerise/ds-tags';
 import Tags from '@synerise/ds-tags/dist/Tags';
@@ -17,6 +17,7 @@ const typeOptions = {
   success: 'success',
   warning: 'warning',
   danger: 'danger',
+  customStatus: 'customStatus',
 };
 const customColorOptions = {
   blue: theme.palette['blue-600'],
@@ -32,12 +33,34 @@ const customColorOptions = {
   purple: theme.palette['purple-600'],
   violet: theme.palette['violet-600'],
 };
+const customColorStatusOptions = {
+  blue: theme.palette['blue-600'],
+  grey: theme.palette['grey-500'],
+  red: theme.palette['red-600'],
+  green: theme.palette['green-600'],
+  yellow: theme.palette['yellow-600'],
+  pink: theme.palette['pink-600'],
+  mars: theme.palette['mars-600'],
+  orange: theme.palette['orange-600'],
+  fern: theme.palette['fern-600'],
+  cyan: theme.palette['cyan-600'],
+  purple: theme.palette['purple-600'],
+  violet: theme.palette['violet-600'],
+};
 
 const stories = {
-  status: () => ({
-    type: select('Type', typeOptions, 'default'),
-    label: 'This is a status',
-  }),
+  Status: () => {
+    const type = select('Type', typeOptions, 'default');
+    const colors = type === 'customStatus' && select('Set custom status color', customColorStatusOptions, customColorStatusOptions.grey);
+    const label = text('Label', 'Draft')
+    return(
+      <React.Fragment>
+        <div style={{ padding: 24 }}>
+          <Status label={label} type={type} color={colors}/>
+        </div>
+      </React.Fragment>
+    );
+  },
   pillSmall: () => {
     const shapes = {
       'Default Round': TagShape.SMALL_ROUND,
