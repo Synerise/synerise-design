f50b4f373471789cd17b3ba6cc0f62845bd72d74
Jakub Sidor <jakub.sidor@synerise.com>
2020-11-18 14:12:01 +0100

    fix(avatar): add small camera icon

--- packages/components/avatar/src/ProductAvatar/ProductAvatar.tsx
+++ packages/components/avatar/src/ProductAvatar/ProductAvatar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { CameraM } from '@synerise/ds-icon/dist/icons';
+import { CameraM, CameraS } from '@synerise/ds-icon/dist/icons';
 import theme from '@synerise/ds-core/dist/js/DSProvider/ThemeProvider/theme';
 import Icon from '@synerise/ds-icon';
 import { AvatarProps } from 'Avatar.types';
@@ -7,7 +7,7 @@ import Avatar from '../Avatar';
 
 const ProductAvatar: React.FC<Pick<AvatarProps, 'size' | 'src'>> = ({ size, src }) => (
   <Avatar
-    iconComponent={<Icon component={<CameraM />} color={theme.palette['grey-500']} />}
+    iconComponent={<Icon component={size === 'small' ? <CameraS/> : <CameraM />} color={theme.palette['grey-500']} />}
     shape="square"
     backgroundColor="grey"
     backgroundColorHue="050"
