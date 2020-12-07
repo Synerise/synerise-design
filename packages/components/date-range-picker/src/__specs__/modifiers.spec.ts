b9662676516b9aff6f04c2c0ffa1b5cfb2827f11
tomasztaranek <tomasz.taranek@synerise.com>
2020-12-07 13:44:46 +0100

    fix(date-range-picker): added modifiers specs
    affects: @synerise/ds-date-range-picker

---
+++ packages/components/date-range-picker/src/__specs__/modifiers.spec.ts
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom/extend-expect';
+import { getModifiers } from '../RangePicker/utils';
+
+describe('DateRangePicker modifiers', () => {
+  it('should calculate start and end modifiers', () => {
+    const fromDate = new Date(1980, 6, 31);
+    const toDate = new Date(1990, 3, 3);
+    const modifiers = getModifiers(fromDate, toDate, null);
+    expect(modifiers['start']).toBe(fromDate);
+    expect(modifiers['end']).toBe(toDate);
+  });
+  it('should preserve default "outside" modifier ', () => {
+    const fromDate = new Date(1980, 6, 31);
+    const toDate = new Date(1990, 3, 3);
+    const modifiers = getModifiers(fromDate, toDate, null);
+    expect(modifiers['outside']).toStrictEqual(undefined);
+  });
+  it('should handle entered-end modifier when endDate is falsy ', () => {
+    const fromDate = new Date(1980, 6, 31);
+    const toDate = undefined;
+    const enteredTo = new Date(1990, 3, 3);
+    const modifiers = getModifiers(fromDate, toDate, enteredTo);
+    expect(modifiers['entered-start']).toStrictEqual(fromDate);
+    expect(modifiers['entered-end']).toStrictEqual(enteredTo);
+  });
+  it('should handle entered-start modifier when endDate is falsy ', () => {
+    const fromDate = new Date(1980, 6, 31);
+    const toDate = undefined;
+    const enteredTo = new Date(1950, 3, 3);
+    const modifiers = getModifiers(fromDate, toDate, enteredTo);
+    expect(modifiers['entered-start']).toStrictEqual(enteredTo);
+    expect(modifiers['entered-end']).toStrictEqual(fromDate);
+  });
+});
