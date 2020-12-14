6f0a511687d96f3cedd082c67bf6fb360960b801
Michał Osajda <michal.osajda@synerise.com>
2020-12-14 20:02:01 +0100

    fix(page-header): fix stories

--- packages/portal/stories/components/PageHeader/index.stories.tsx
+++ packages/portal/stories/components/PageHeader/index.stories.tsx
@@ -319,10 +319,6 @@ const stories = {
       store.set({ name: e.target.value });
     };
 
-    const handleChangeName = e => {
-      store.set({ name: e.target.value });
-    };
-
     return (
       <PageHeader
         avatar={
