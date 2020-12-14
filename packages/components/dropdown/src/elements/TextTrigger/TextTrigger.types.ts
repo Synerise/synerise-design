111d56357540f044becfb1d1eb06f4cf8f75ae29
Michał Osajda <michal.osajda@synerise.com>
2020-12-14 08:57:16 +0100

    fix(dropdown): fix TextTrigger props

--- packages/components/dropdown/src/elements/TextTrigger/TextTrigger.types.ts
+++ packages/components/dropdown/src/elements/TextTrigger/TextTrigger.types.ts
@@ -1,5 +1,7 @@
+import * as React from 'react';
+
 export type TextTriggerProps = {
-  value: string;
+  value: string | React.ReactNode | undefined;
   inactiveColor?: string;
   size: 1 | 2 | 3 | 4 | 5 | 6;
   onClick: () => void;
