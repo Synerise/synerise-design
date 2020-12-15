fa1a1040553884eb1cc93c7520cfdea5041ad754
Jakub Sidor <jakub.sidor@synerise.com>
2020-12-15 08:59:55 +0100

    fix(dropdown): add option to close dropdown by click outside in example1 and withTabs

--- packages/portal/stories/components/Dropdown/examples/OverlayExample1.tsx
+++ packages/portal/stories/components/Dropdown/examples/OverlayExample1.tsx
@@ -12,17 +12,23 @@ import Menu from '@synerise/ds-menu';
 import Button from '@synerise/ds-button';
 import theme from '@synerise/ds-core/dist/js/DSProvider/ThemeProvider/theme';
 import Result from '@synerise/ds-result';
+import { useOnClickOutside } from '@synerise/ds-utils';
 
 interface Props {
   value: string;
   onSearchChange: (value: string) => void;
   onClearInput?: () => void;
   data: any;
+  onClickOutside?: () => void;
 }
 
-const OverlayExample1: React.FC<Props> = ({ value, onSearchChange, onClearInput, data}) => {
+const OverlayExample1: React.FC<Props> = ({ value, onSearchChange, onClearInput, data,onClickOutside}) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  useOnClickOutside(ref, () => {
+    onClickOutside();
+  });
   return (
-    <Dropdown.Wrapper>
+    <Dropdown.Wrapper ref={ref}>
       <Dropdown.SearchInput
         onSearchChange={onSearchChange}
         onClearInput={onClearInput}
