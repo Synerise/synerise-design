b00659f0bb3309b66b2b1d0c5640e15a519212f3
tomasztaranek <tomasz.taranek@synerise.com>
2020-12-22 08:57:54 +0100

    fix(subtle-form): fixed inactive element rendering, added stories

--- packages/components/subtle-form/src/Elements/Field/Field.tsx
+++ packages/components/subtle-form/src/Elements/Field/Field.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Icon from '@synerise/ds-icon';
 import Tooltip from '@synerise/ds-tooltip';
-import { AngleDownS } from '@synerise/ds-icon/dist/icons';
+import { EditS } from '@synerise/ds-icon/dist/icons';
 import theme from '@synerise/ds-core/dist/js/DSProvider/ThemeProvider/theme';
 import { Label } from '@synerise/ds-input';
 import { useOnClickOutside } from '@synerise/ds-utils';
 import * as S from '../../SubtleForm.styles';
 import { SelectContainer, ContentAbove } from './Field.styles';
 import { SubtleFieldProps } from '../../SubtleForm.types';
+import { MaskedDatePlaceholder } from '../DatePicker/DatePicker.styles';
 
 const SutbleField: React.FC<SubtleFieldProps> = ({
   disabled,
@@ -17,6 +18,8 @@ const SutbleField: React.FC<SubtleFieldProps> = ({
   labelTooltip,
   activeElement,
   inactiveElement,
+  mask,
+  maskVisible,
 }) => {
   const [active, setActive] = React.useState<boolean>(false);
   const [blurred, setBlurred] = React.useState<boolean>(false);
@@ -40,14 +43,22 @@ const SutbleField: React.FC<SubtleFieldProps> = ({
       </ContentAbove>
       <SelectContainer ref={containerRef} className="ds-subtle-select" active={active}>
         {active && !blurred && !!activeElement ? (
-          React.cloneElement(activeElement())
+          activeElement()
         ) : (
-          <S.Inactive onClick={!disabled ? handleActivate : undefined} blurred={blurred} disabled={disabled}>
-            <S.MainContent hasMargin>{inactiveElement}</S.MainContent>
+          <S.Inactive
+            onClick={!disabled ? handleActivate : undefined}
+            blurred={blurred}
+            disabled={disabled}
+            mask={maskVisible}
+          >
+            <S.MainContent hasMargin>
+              {inactiveElement && inactiveElement()}
+              {!disabled && maskVisible && <MaskedDatePlaceholder>{mask}</MaskedDatePlaceholder>}
+            </S.MainContent>
             {!active && !disabled && (
               <S.Suffix select>
                 <Tooltip title={suffixTooltip}>
-                  {suffix ?? <Icon component={<AngleDownS />} color={theme.palette['grey-600']} />}
+                  {suffix ?? <Icon component={<EditS />} color={theme.palette['grey-600']} />}
                 </Tooltip>
               </S.Suffix>
             )}
--- packages/portal/stories/components/SubtleForm/MaskedInput.stories.tsx
+++ packages/portal/stories/components/SubtleForm/MaskedInput.stories.tsx
@@ -4,7 +4,7 @@ import Select from '@synerise/ds-select';
 import { Cities } from './dataset';
 import { boolean, text } from '@storybook/addon-knobs';
 import Dropdown from '@synerise/ds-dropdown';
-import { Input } from '@synerise/ds-input';
+import { Input, MaskedInput } from '@synerise/ds-input';
 import InputNumber from '@synerise/ds-input-number';
 const decorator = storyFn => <div style={{ width: '314px', padding: '16px', background: '#fff' }}>{storyFn()}</div>;
 export const renderLabel = (text: string) => {
@@ -17,19 +17,25 @@ export const getErrorText = (error: boolean, errorText: string): string => {
     return '';
   }
 };
+
 const stories = {
-  default: () => {
-    const [value, setValue] = React.useState<number>(1);
-    const validationState = boolean('Set validation state', false);
-    const errorMessage = text('Error Text', 'Error');
-    const disabled = boolean('Set disabled', false);
+  maskedInput: () => {
+    const [value, setValue] = React.useState<string>('');
+    const placeholder = 'Number';
+    const mask = '111-111-111';
+    const mask_underscore = `_ _ _-_ _ _-_ _ _`;
     return (
       <div style={{ marginBottom: '16px', height: '57px' }}>
         <SubtleForm.Field
-          activeElement={()=><InputNumber autoFocus value={value} onChange={e => setValue(e)} />}
-          inactiveElement={'inactive'}
-          placeholder={'City'}
-          label={renderLabel('City')}
+          value={value}
+          activeElement={() => (
+            <MaskedInput autoFocus mask={mask} value={value} onChange={e => setValue(e.target.value)} />
+          )}
+          inactiveElement={() => value || placeholder}
+          label={renderLabel('Phone number')}
+          labelTooltip={'Phone number'}
+          mask={mask_underscore}
+          maskVisible={!value}
         ></SubtleForm.Field>
       </div>
     );
@@ -37,7 +43,7 @@ const stories = {
 };
 
 export default {
-  name: 'Components/SubtleForm/Field',
+  name: 'Components/SubtleForm/Input',
   config: {},
   stories,
   decorator,
