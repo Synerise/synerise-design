adbd633c6c27c7d6c8ee625de0df4acd1f0bd2e8
Michał Osajda <michal.osajda@synerise.com>
2021-01-27 11:33:03 +0100

    fix(sidebar-object): hide Menu.Divider when only onId action available

--- packages/components/sidebar-object/src/Elements/Header/Header.style.ts
+++ packages/components/sidebar-object/src/Elements/Header/Header.style.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import InlineEdit from '@synerise/ds-inline-edit';
 import { InlineEditProps } from '@synerise/ds-inline-edit/dist/InlineEdit.types';
 
-export const HeaderWrapper = styled.div<{dashed: boolean}>`
-  padding: ${(props): string => props.dashed ? '0 0 16px 0': '0' };
-  border-bottom: ${(props): string => props.dashed ? `1px dashed ${props.theme.palette['grey-300']}`: `0` };
+export const HeaderWrapper = styled.div<{ dashed: boolean }>`
+  padding: ${(props): string => (props.dashed ? '0 0 16px 0' : '0')};
+  border-bottom: ${(props): string => (props.dashed ? `1px dashed ${props.theme.palette['grey-300']}` : `0`)};
   margin-top: -8px;
 `;
 
@@ -12,30 +12,24 @@ export const ButtonWrapper = styled.div`
   padding: 0 0 0 8px;
 `;
 export const StyledInlineEdit = styled(InlineEdit)<InlineEditProps>`
-  
   && .autosize-input > input {
-    max-width:350px;
+    max-width: 350px;
     overflow: hidden;
   }
 `;
-export const DrawerHeaderBar = styled.div<{withTabs: boolean}>`
+export const DrawerHeaderBar = styled.div<{ withTabs: boolean }>`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-bottom: ${(props): string => props.withTabs ? '10px': '24px' };
+  padding-bottom: ${(props): string => (props.withTabs ? '10px' : '24px')};
   .ant-typography {
-  margin-bottom: 0px;
+    margin-bottom: 0;
   }
 `;
 export const ButtonsWrapper = styled.div`
   display: flex;
- `;
-
-export const MenuWrapper = styled.div`
-  border-top: 1px dashed ${(props): string => props.theme.palette['grey-300']};
-  margin: 8px 0 0;
-  padding: 8px 0 0 0px;
 `;
+
 export const DropdownWrapper = styled.div`
   background: ${(props): string => props.theme.palette.white};
   width: 216px;
@@ -45,12 +39,11 @@ export const OverviewWrapper = styled.div`
   width: 100%;
 `;
 export const SingleTitle = styled.div`
-font-size: 18px;
-padding-left: 1px;
-padding-top: 1px;
-max-width: 350px;
-overflow: hidden;
-text-overflow: ellipsis;
-white-space: nowrap;
-
+  font-size: 18px;
+  padding-left: 1px;
+  padding-top: 1px;
+  max-width: 350px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
