5ca035eb8cb06d1a74221f94034bdec658141493
Jakub Sidor <jakub.sidor@synerise.com>
2021-02-23 09:26:01 +0100

    fix(dropdown): add new prop to rotate icon

--- packages/components/dropdown/src/elements/TextTrigger/TextTrigger.styles.ts
+++ packages/components/dropdown/src/elements/TextTrigger/TextTrigger.styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { ThemeProps } from '@synerise/ds-core/dist/js/DSProvider/ThemeProvider/theme';
 
 // eslint-disable-next-line import/prefer-default-export
 export const TextTrigger = styled.div<{ inactiveColor: string; onFocus?: () => void }>`
@@ -29,6 +30,15 @@ export const TextTrigger = styled.div<{ inactiveColor: string; onFocus?: () => v
     svg {
       color: ${(props): string => props.theme.palette['blue-600']};
       fill: ${(props): string => props.theme.palette['blue-600']};
+    }
+  }
+`;
+
+export const IconWrapper = styled.div<{ expanded?: boolean } & ThemeProps>`
+  &&& {
+    svg {
+      transition: transform 0.1s linear;
+      transform: rotateZ(${(props): string => (props.expanded ? '180deg' : '0deg')});
     }
   }
 `;
