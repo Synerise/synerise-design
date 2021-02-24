d6f7bd30b627eccfddb22ff78948fce683a654d3
Jakub Sidor <jakub.sidor@synerise.com>
2021-02-24 12:18:25 +0100

    fix(unordered-list): fix styles of unordered list

--- packages/portal/stories/components/UnorderedList/Icon-list.stories.tsx
+++ packages/portal/stories/components/UnorderedList/Icon-list.stories.tsx
@@ -1,4 +1,4 @@
-import { select, text } from '@storybook/addon-knobs';
+import { boolean, select, text } from '@storybook/addon-knobs';
 import Icon from '@synerise/ds-icon';
 import CheckS from '@synerise/ds-icon/dist/icons/CheckS';
 import theme from '@synerise/ds-core/dist/js/DSProvider/ThemeProvider/theme';
@@ -21,13 +21,22 @@ const stories = {
     const emptyFormatter = () => ``;
     const setLevel = select('Set list level', level, level.first);
     const Icons = {
-      CheckS: <Icon size={20} style={{marginRight: '2px'}} color={theme.palette['grey-600']} component={<CheckS />} />,
-      Close3S: <Icon size={20} style={{marginRight: '2px'}} color={theme.palette['red-600']} component={<Close3S />} />,
-      Check3S: <Icon size={20} style={{marginRight: '2px'}} color={theme.palette['green-600']} component={<Check3S />} />,
-      Emoji:<Icon size={20}  component=' 👉 ' />
+      CheckS: <Icon size={20} style={{marginRight: '2px', marginLeft: '-4px'}} color={theme.palette['grey-600']} component={<CheckS />} />,
+      Close3S: <Icon size={20} style={{marginRight: '2px', marginLeft: '-4px'}} color={theme.palette['red-600']} component={<Close3S />} />,
+      Check3S: <Icon size={20} style={{marginRight: '2px', marginLeft: '-4px'}} color={theme.palette['green-600']} component={<Check3S />} />,
+      Emoji: <Icon size={20} style={{ display: 'flex', marginRight: '2px', marginLeft: '-4px',justifyContent: 'center'}}  component='👉' />
     };
     const textIcons = ['CheckS', 'Close3S', 'Check3S','Emoji'];
     const iconType = select('Set symbol', textIcons, 'CheckS');
+    const hasLabel = boolean('Set label', true);
+    const label = text('Label', 'Header label');
+    const getLabel = (hasLabel: boolean): string => {
+      if (hasLabel) {
+        return label;
+      } else {
+        return '';
+      }
+    };
     const data = [
       {
         label: 'Icon List level 1',
@@ -93,30 +102,39 @@ const stories = {
       },
     ];
 
-    return <OrderedList text={renderLabel(text('Label', 'Header label'))} data={data} indexFormatter={emptyFormatter} />;
+    return <OrderedList text={renderLabel(label && getLabel(hasLabel))} data={data} indexFormatter={emptyFormatter} />;
   },
-  WithIndividualIcons: () => {
+  withIndividualIcons: () => {
     const emptyFormatter = () => ``;
+    const hasLabel = boolean('Set label', true);
+    const label = text('Label', 'Header label');
+    const getLabel = (hasLabel: boolean): string => {
+      if (hasLabel) {
+        return label;
+      } else {
+        return '';
+      }
+    };
     const data = [
       {
         label: 'Icon List level 1',
-        prefixel: <Icon size={20} style={{marginRight: '2px'}} color={theme.palette['red-600']} component={<Close3S />} />,
+        prefixel: <Icon size={20} style={{marginRight: '2px', marginLeft: '-4px'}} color={theme.palette['red-600']} component={<Close3S />} />,
       },
       {
         label: 'Icon List level 1',
-        prefixel: <Icon size={20} style={{marginRight: '2px'}} color={theme.palette['red-600']} component={<Close3S />} />,
+        prefixel: <Icon size={20} style={{marginRight: '2px', marginLeft: '-4px'}} color={theme.palette['red-600']} component={<Close3S />} />,
       },
       {
         label: 'Icon List level 1',
-        prefixel: <Icon size={20} style={{marginRight: '2px'}} color={theme.palette['green-600']} component={<Check3S />} />,
+        prefixel: <Icon size={20} style={{marginRight: '2px', marginLeft: '-4px'}} color={theme.palette['green-600']} component={<Check3S />} />,
       },
       {
         label: 'Icon List level 1',
-        prefixel: <Icon size={20} style={{marginRight: '2px'}} color={theme.palette['green-600']} component={<Check3S />} />,
+        prefixel: <Icon size={20} style={{marginRight: '2px', marginLeft: '-4px'}} color={theme.palette['green-600']} component={<Check3S />} />,
       },
     ];
 
-    return <OrderedList text={renderLabel(text('Label', 'Header label'))} data={data} indexFormatter={emptyFormatter} />;
+    return <OrderedList text={renderLabel(label && getLabel(hasLabel))} data={data} indexFormatter={emptyFormatter} />;
   },
 };
 
