37f0d8658a5b1179361f05a72339ed09f8adca93
Jakub Sidor <jakub.sidor@synerise.com>
2021-02-26 10:44:17 +0100

    fix(ordered-list): fix margin between items

--- packages/components/ordered-list/src/Elements/Item/Item.styles.ts
+++ packages/components/ordered-list/src/Elements/Item/Item.styles.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 
 export const ItemWrapper = styled.li`
-  margin: 6px 0 6px 0;
+  margin: 5px 0 5px 0;
   display: list-item;
   justify-content: flex-start;
   align-items: center;
 `;
-export const IndexFormatterWrapper = styled.span`
-  `;
+export const IndexFormatterWrapper = styled.span``;
