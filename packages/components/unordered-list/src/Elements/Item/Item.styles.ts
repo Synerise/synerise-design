f70a979437586f514414c1be5106002057583814
Jakub Sidor <jakub.sidor@synerise.com>
2021-02-26 10:42:35 +0100

    fix(unordered-list): fix styles of icons in icon list

--- packages/components/unordered-list/src/Elements/Item/Item.styles.ts
+++ packages/components/unordered-list/src/Elements/Item/Item.styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 export const ItemWrapper = styled.li`
-  margin: 6px 0 6px 0;
+  margin: 5px 0 5px 2px;
   display: list-item;
   justify-content: flex-start;
   align-items: center;
 `;
-export const IndexFormatterWrapper = styled.span`
+export const IndexFormatterWrapper = styled.span<{ listStyle?: string }>`
+  margin-right: ${(props): string => (props.listStyle === 'none' ? '6px' : '-4px')};
 `;
