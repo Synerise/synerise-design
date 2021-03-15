64b9f12bb78837dc0fb11cd4414960bebff5c1d9
Dariusz Arciszewski <dariusz.arciszewski@synerise.com>
2021-03-15 17:22:26 +0100

    feat(utils): better color by letter function

--- packages/components/utils/src/selectColorByLetter/selectColorByLetter.ts
+++ packages/components/utils/src/selectColorByLetter/selectColorByLetter.ts
@@ -1,9 +1,25 @@
 import theme from '@synerise/ds-core/dist/js/DSProvider/ThemeProvider/theme';
+import * as latinize from 'latinize';
 
+export type ColorByLetter = {
+  [index: string]: string;
+};
 export type ColorObject = { color: string; hue: string };
 export type Color = string | ColorObject;
 
-const getColor = (colorString: string, forAvatar: boolean): Color => {
+export const palette = ['blue', 'cyan', 'fern', 'green', 'orange', 'yellow', 'red', 'mars', 'pink', 'violet', 'purple'];
+
+function getColorByLetter(): ColorByLetter {
+  const colors = {};
+  for(let i = 0; i<=25; i += 1) {
+    colors[String.fromCharCode(i+65)] = palette[i % palette.length];
+  }
+  return colors;
+}
+
+export const colorByLetter = getColorByLetter();
+
+export function getColor(colorString: string, forAvatar: boolean): Color {
   if (!forAvatar) return theme.palette[colorString];
   return {
     color: colorString.split('-')[0],
@@ -11,62 +27,16 @@ const getColor = (colorString: string, forAvatar: boolean): Color => {
   };
 };
 
-const selectColorByLetter = (letter?: string, forAvatar = false): Color => {
-  if (typeof letter !== 'string' || !/^[a-z]$/i.test(letter)) {
-    return getColor('orange-700', forAvatar);
+function selectColorByLetter(letter?: string, forAvatar = false): Color {
+  if (typeof letter !== 'string') {
+    return getColor('orange-500', forAvatar);
   }
 
-  switch (letter.toUpperCase()) {
-    case 'A':
-      return getColor('pink-700', forAvatar);
-    case 'B':
-      return getColor('pink-600', forAvatar);
-    case 'C':
-      return getColor('violet-600', forAvatar);
-    case 'D':
-      return getColor('violet-500', forAvatar);
-    case 'E':
-      return getColor('blue-700', forAvatar);
-    case 'F':
-      return getColor('blue-600', forAvatar);
-    case 'G':
-      return getColor('blue-400', forAvatar);
-    case 'H':
-      return getColor('blue-300', forAvatar);
-    case 'I':
-      return getColor('cyan-700', forAvatar);
-    case 'J':
-      return getColor('fern-600', forAvatar);
-    case 'K':
-      return getColor('fern-500', forAvatar);
-    case 'L':
-      return getColor('yellow-600', forAvatar);
-    case 'M':
-      return getColor('orange-400', forAvatar);
-    case 'N':
-      return getColor('yellow-500', forAvatar);
-    case 'O':
-      return getColor('orange-600', forAvatar);
-    case 'P':
-      return getColor('red-800', forAvatar);
-    case 'R':
-      return getColor('grey-600', forAvatar);
-    case 'S':
-      return getColor('grey-700', forAvatar);
-    case 'T':
-      return getColor('pink-400', forAvatar);
-    case 'U':
-      return getColor('fern-400', forAvatar);
-    case 'V':
-      return getColor('blue-400', forAvatar);
-    case 'W':
-      return getColor('pink-500', forAvatar);
-    case 'X':
-      return getColor('orange-300', forAvatar);
-    case 'Y':
-      return getColor('fern-200', forAvatar);
+  const latinized = latinize(letter.toUpperCase());
+
+  switch (latinized) {
     default:
-      return getColor('orange-700', forAvatar);
+      return getColor(`${colorByLetter[latinized] || 'orange'}-500`, forAvatar);
   }
 };
 
