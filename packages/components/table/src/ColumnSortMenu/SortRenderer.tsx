a6b194b19cf277e6bf526ef4f5f9f7352cc9e1d1
Dominik Depta <dominik.depta@synerise.com>
2021-03-26 10:16:56 +0100

    fix(table): fix sort dropdown overflow issues
    affects: @synerise/ds-table

--- packages/components/table/src/ColumnSortMenu/SortRenderer.tsx
+++ packages/components/table/src/ColumnSortMenu/SortRenderer.tsx
@@ -18,8 +18,8 @@ interface SortRendererProps<T> {
 
 const getPopupParent = (triggerNode: HTMLElement): HTMLElement =>
   // dropdown target node must be resolved relatively to triggerNode (which comes from getPopupContainer)
-  // go up to <thead> due to possible columns ellipsis overflow
-  triggerNode.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.parentElement as HTMLElement;
+  // go up to <th> due to possible columns ellipsis overflow
+  triggerNode.parentElement?.parentElement?.parentElement?.parentElement as HTMLElement;
 
 export const CommonRenderer = <T extends unknown>({
   column,
