7fa389fd122aed522c9b967a1ac89c174c6597cc
Dominik Depta <dominik.depta@synerise.com>
2021-04-09 12:39:59 +0200

    refactor(utils): update combined refs util to hook

---
+++ packages/components/utils/src/useCombinedRefs/useCombinedRefs.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+const useCombinedRefs = <T extends unknown>(...refs: React.Ref<T | null>[]): React.MutableRefObject<T | null> => {
+  const targetRef = React.useRef<T>(null);
+
+  React.useEffect(() => {
+    refs.forEach((ref) => {
+      if (!ref) {
+        return;
+      }
+
+      if (typeof ref === 'function') {
+        ref(targetRef.current);
+      } else {
+        // eslint-disable-next-line no-param-reassign
+        (ref as React.MutableRefObject<T | null>).current = targetRef.current;
+      }
+    });
+  }, [refs])
+
+  return targetRef;
+};
+
+export default useCombinedRefs;
