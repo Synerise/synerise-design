4575938731fdc53930e3a82d2764aebb7ec2f25d
Jakub Sidor <jakub.sidor@synerise.com>
2021-04-19 08:33:40 +0200

    fix(alert): fix types of Alert

--- packages/components/alert/src/AlertInfo/AlertInfo.types.ts
+++ packages/components/alert/src/AlertInfo/AlertInfo.types.ts
@@ -2,8 +2,8 @@ import * as React from 'react';
 
 
 export type AlertInfoProps = {
-  size?: AlertSize.SMALL | AlertSize.MEDIUM;
-  fontSize?: AlertSize.SMALL | AlertSize.MEDIUM;
+  size?: AlertSize;
+  fontSize?: AlertSize;
   text?: string | React.ReactNode;
   button?: string | React.ReactNode;
   label?: string | React.ReactNode;
@@ -12,6 +12,6 @@ export type AlertInfoProps = {
   customIcon?: React.ReactElement;
   type: string | 'warning' | 'error' | 'success';
 };
-enum AlertSize {  SMALL = "small", MEDIUM = "medium"  }
+export enum AlertSize {  SMALL = "small", MEDIUM = "medium"  }
 export const IconSize = { [AlertSize.SMALL] : 48, [AlertSize.MEDIUM]: 96  }
 export const FontSize = { [AlertSize.SMALL] : 14, [AlertSize.MEDIUM]: 18  }
