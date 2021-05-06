9c069f0f6c80a5070e9715aacf7e1449cbc16c74
Tomek Kaminski <tomasz.kaminski@synerise.com>
2021-05-06 10:01:25 +0200

    refactor(code-area): eslint fixes
    affects: @synerise/ds-code-area
    
    eslint fixes

--- packages/components/code-area/src/CodeArea.tsx
+++ packages/components/code-area/src/CodeArea.tsx
@@ -1,7 +1,6 @@
-import * as React  from 'react';
+import * as React from 'react';
 import MonacoEditorBase, { monaco } from 'react-monaco-editor';
 import { withTheme } from 'styled-components';
-// @ts-ignore
 import debounce from 'lodash/debounce';
 import { Label } from '@synerise/ds-input';
 import { Description, ErrorText } from '@synerise/ds-typography';
@@ -9,7 +8,6 @@ import Icon from '@synerise/ds-icon';
 import { FullScreenM } from '@synerise/ds-icon/dist/icons';
 import Modal from '@synerise/ds-modal';
 
-import { removeContextMenuElements } from './helpers/contextMenu';
 import * as S from './CodeArea.styles';
 import { CodeAreaProps } from './CodeArea.types';
 
@@ -17,11 +15,11 @@ const MONACO_EDITOR_LAYOUT_EVENT_NAME = 'monacoEditor:layout';
 
 const MONACO_EDITOR_DEFAULT_OPTIONS = {
   minimap: {
-    enabled: false
+    enabled: false,
   },
   scrollbar: {
-    horizontal: "hidden" as "hidden",
-    vertical: "hidden" as "hidden",
+    horizontal: 'hidden' as 'hidden',
+    vertical: 'hidden' as 'hidden',
   },
   overviewRulerLanes: 0,
   folding: false,
@@ -29,12 +27,6 @@ const MONACO_EDITOR_DEFAULT_OPTIONS = {
 };
 
 const MONACO_EDITOR_ON_CHANGE_DELAY = 300;
-const MONACO_EDITOR_MENU_ID = 'EditorContext';
-const MONACO_EDITOR_MENU_IDS_TO_REMOVE = [
-  'editor.action.clipboardPasteAction',
-  'editor.action.quickOutline', // Go to Symbol... context menu item,
-  'editor.action.rename',
-];
 
 const transparentColorCode = '#00000000';
 
@@ -51,134 +43,113 @@ const CodeArea: React.FC<CodeAreaProps> = ({
   errorText,
   ...props
 }) => {
-  const [size, setSize] = React.useState<{width: string, height: string}>({width: '284px', height: '120px'});
+  const [size, setSize] = React.useState<{ width: string; height: string }>({ width: '284px', height: '120px' });
   const [editor, setEditor] = React.useState<monaco.editor.IStandaloneCodeEditor>();
   const [isFullScreen, setFullScreen] = React.useState<boolean>(false);
 
-  const showError = React.useMemo(() => Boolean(error && errorText),[errorText, error]);
+  const showError = React.useMemo(() => Boolean(error && errorText), [errorText, error]);
 
   const editLayoutUpdate = React.useCallback(() => editor?.layout(), [editor]);
-
-  React.useEffect(
-    function removeMenuElements() {
-      removeContextMenuElements(MONACO_EDITOR_MENU_ID, MONACO_EDITOR_MENU_IDS_TO_REMOVE);
-    },
-    [editor]
-  );
 
   React.useLayoutEffect(
     function resizeMonacoEditor() {
       document.addEventListener(MONACO_EDITOR_LAYOUT_EVENT_NAME, editLayoutUpdate);
 
-      return function resizeMonacoEditorCleanup() {
+      return function resizeMonacoEditorCleanup(): void {
         document.removeEventListener(MONACO_EDITOR_LAYOUT_EVENT_NAME, editLayoutUpdate);
       };
     },
     [editLayoutUpdate]
   );
 
-  React.useEffect( () => {
+  React.useEffect(() => {
     monaco.editor.defineTheme('synerise-theme', {
       base: 'vs',
       inherit: true,
       rules: [],
       colors: {
         'editor.background': `${theme.palette[showError ? 'red-100' : 'white']}`,
-        'editorGutter.background': `${theme.palette[ showError ? 'red-200' : 'grey-200']}`,
+        'editorGutter.background': `${theme.palette[showError ? 'red-200' : 'grey-200']}`,
         'editorLineNumber.foreground': '#404c5a',
         'editor.lineHighlightBackground': transparentColorCode,
-        'editor.lineHighlightBorder': transparentColorCode
+        'editor.lineHighlightBorder': transparentColorCode,
       },
     });
-  },[theme, showError]);
+  }, [theme, showError]);
 
-  const editorDidMountWrapper = (currentEditor: monaco.editor.IStandaloneCodeEditor, monacoInstance: typeof monaco): void => {
-    editorDidMount?.(currentEditor, monacoInstance);
+  const editorDidMountWrapper = (
+    currentEditor: monaco.editor.IStandaloneCodeEditor,
+    monacoInstance: typeof monaco
+  ): void => {
+    if (editorDidMount) editorDidMount(currentEditor, monacoInstance);
     setEditor(currentEditor);
   };
 
-  const onChangeBase = (value: string, event: monaco.editor.IModelContentChangedEvent) => onChange?.(value, event);
+  const onChangeBase = (value: string, event: monaco.editor.IModelContentChangedEvent): void =>
+    onChange?.(value, event);
   const onChangeWrapper = onChangeDebounced ? debounce(onChangeBase, onChangeDebouncedWait) : onChangeBase;
 
-  const handleFullScreenClick = React.useCallback(() => {
+  const handleFullScreenClick = (): void => {
     setFullScreen(true);
-    setSize({width: '98%', height: '950px'});
-  },[]);
+    setSize({ width: '98%', height: '950px' });
+  };
 
-  const handleMinimize = React.useCallback(() => {
+  const handleMinimize = (): void => {
     setFullScreen(false);
-    setSize({width: '284px', height: '120px'});
-  },[])
+    setSize({ width: '284px', height: '120px' });
+  };
 
   return !isFullScreen ? (
-     <>
-       {label &&
-         <S.LabelWrapper>
-           <Label label={label}/>
-         </S.LabelWrapper>
-       }
-       <S.MonacoWrapper
-          error={error}
-       >
-         <MonacoEditorBase
-           options={{ ...MONACO_EDITOR_DEFAULT_OPTIONS, ...options }}
-           onChange={onChangeWrapper}
-           editorDidMount={editorDidMountWrapper}
-           theme="synerise-theme"
-           width={size.width}
-           height={size.height}
-           {...props}
-         />
-         <S.FullScreenWrapper>
-           <Icon
-             component={<FullScreenM/>}
-             color={theme.palette['grey-600']}
-             onClick={handleFullScreenClick}
-           />
-         </S.FullScreenWrapper>
-       </S.MonacoWrapper>
-       {showError &&
-         <ErrorText>
-           {errorText}
-         </ErrorText>
-       }
-       {description &&
-         <S.LabelWrapper>
-           <Description>
-             {description}
-           </Description>
-         </S.LabelWrapper>
-       }
-     </>
-   ) : (
-     <Modal
-       closable
-       visible
-       blank
-       onCancel={handleMinimize}
-       size="extraLarge"
-       bodyBackground="grey"
-       footer={null}
-       title={
-         <S.LabelModalWrapper>
-           {label}
-         </S.LabelModalWrapper>
-       }
-     >
-       <S.MonacoWrapper
-         error={error}
-       >
-         <MonacoEditorBase
-           options={{ ...MONACO_EDITOR_DEFAULT_OPTIONS, ...options }}
-           onChange={onChangeWrapper}
-           editorDidMount={editorDidMountWrapper}
-           theme="synerise-theme"
-           width={size.width}
-           height={size.height}
-           {...props}
-         />
-       </S.MonacoWrapper>
-     </Modal>
+    <>
+      {label && (
+        <S.LabelWrapper>
+          <Label label={label} />
+        </S.LabelWrapper>
+      )}
+      <S.MonacoWrapper error={error}>
+        <MonacoEditorBase
+          options={{ ...MONACO_EDITOR_DEFAULT_OPTIONS, ...options }}
+          onChange={onChangeWrapper}
+          editorDidMount={editorDidMountWrapper}
+          theme="synerise-theme"
+          width={size.width}
+          height={size.height}
+          {...props}
+        />
+        <S.FullScreenWrapper>
+          <Icon component={<FullScreenM />} color={theme.palette['grey-600']} onClick={handleFullScreenClick} />
+        </S.FullScreenWrapper>
+      </S.MonacoWrapper>
+      {showError && <ErrorText>{errorText}</ErrorText>}
+      {description && (
+        <S.LabelWrapper>
+          <Description>{description}</Description>
+        </S.LabelWrapper>
+      )}
+    </>
+  ) : (
+    <Modal
+      closable
+      visible
+      blank
+      onCancel={handleMinimize}
+      size="extraLarge"
+      bodyBackground="grey"
+      footer={null}
+      title={<S.LabelModalWrapper>{label}</S.LabelModalWrapper>}
+    >
+      <S.MonacoWrapper error={error}>
+        <MonacoEditorBase
+          options={{ ...MONACO_EDITOR_DEFAULT_OPTIONS, ...options }}
+          onChange={onChangeWrapper}
+          editorDidMount={editorDidMountWrapper}
+          theme="synerise-theme"
+          width={size.width}
+          height={size.height}
+          {...props}
+        />
+      </S.MonacoWrapper>
+    </Modal>
   );
 };
 
