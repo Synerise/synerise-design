d22663148697ef20beb05cc107e491ac10f4e45c
Michał Osajda <michal.osajda@synerise.com>
2021-05-11 20:37:45 +0200

    feat(treemenu): update stories

--- packages/portal/stories/components/TreeMenu/data.ts
+++ packages/portal/stories/components/TreeMenu/data.ts
@@ -1,56 +1,49 @@
+import { v4 as uuid } from 'uuid';
+
 export function generateItems(amount: number, start = 1) {
   const items = [];
-  for(let i = start; i<=amount+start; i+=1) {
+  for (let i = start; i <= amount + start; i += 1) {
+    const id = uuid();
     items.push({
-      id: i,
-      key: i,
-      title: `Option ${i}`,
-      type: "folder"
+      id,
+      key: id,
+      name: `Option ${i}`,
+      type: 'folder',
+      hidden: false,
     });
   }
   return items;
 }
 
+const id1 = uuid();
+const id2 = uuid();
+const id3 = uuid();
+
 export const dataSource = [
   {
-    id: 1,
-    key: 1,
-    title: 'With children',
-    type: "folder",
-    children: [
-      ...generateItems(5, 100),
-    ]
+    id: id1,
+    key: id1,
+    name: 'With children',
+    type: 'folder',
+    hidden: false,
+    children: [...generateItems(5, 100)],
   },
   {
-    id: 2,
-    key: 2,
-    title: 'With grandchildren',
-    type: "folder",
+    id: id2,
+    key: id2,
+    name: 'With grandchildren',
+    type: 'folder',
+    hidden: false,
     children: [
       {
-        id: 2000,
-        key: 2000,
+        id: id3,
+        key: id3,
         name: 'Subchildren',
-        title: 'Subchildren',
-        children: [
-          ...generateItems(5, 2001)
-        ]
+        type: 'folder',
+        children: [...generateItems(5, 2001)],
       },
-      ...generateItems(5, 200)
-    ]
+      ...generateItems(5, 200),
+    ],
   },
-  ...generateItems(1, 10)
-];
-
-export const dataSourceAlter = [
   ...generateItems(1, 10),
-  ...generateItems(2, 20),
-  {
-    id: 1,
-    title: 'Other options',
-    type: "folder",
-    children: [
-      ...generateItems(5, 100),
-    ]
-  },
-]
+];
