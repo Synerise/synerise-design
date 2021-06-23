6ec9be46681e59936e20ab13c4bb449f621adb01
Jakub Sidor <jakub.sidor@synerise.com>
2021-06-23 10:20:34 +0200

    fix(ordered-list): fix styles of skeleton-ordered-list

--- packages/components/skeleton/src/OrderedListSkeleton/OrderedListSkeleton.styles.ts
+++ packages/components/skeleton/src/OrderedListSkeleton/OrderedListSkeleton.styles.ts
@@ -1,28 +1,33 @@
-import styled, {  keyframes } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { SkeletonSize, WrapperSize } from '../CheckboxSkeleton/CheckboxSkeleton.types';
 
 export const loadingAnimation = keyframes`
 
   0% {
-     background: linear-gradient(90deg, rgba(252,252,255,1) 0%, rgba(243,243,245,1) 17%, rgba(156,157,157,1) 100%); left:-40px; top:0px;
+     background: linear-gradient(90deg, rgba(252,252,255,1) 0%, rgba(243,243,245,1) 17%, rgba(156,157,157,1) 100%); left:-80px; top:0px;
      opacity: 0.1;
   }
-  100% {
-     background: linear-gradient(90deg, rgba(252,252,255,1) 0%, rgba(243,243,245,1) 17%, rgba(156,157,157,1) 100%); left:68px; top:0px;
+  50% {
+     background: linear-gradient(90deg, rgba(252,252,255,1) 0%, rgba(243,243,245,1) 17%, rgba(156,157,157,1) 100%); left:140px; top:0px;
      opacity: 0.3;
+  }
+  100% {
+     background: linear-gradient(90deg, rgba(252,252,255,1) 0%, rgba(243,243,245,1) 17%, rgba(156,157,157,1) 100%); left:-80px; top:0px;
+     opacity: 0.1;
   }
 `;
 export const Container = styled.div`
   width: 100%;
+  overflow: hidden;
 `;
 
-const SIZE_DEFAULT = 20;
-const SIZE_WRAPPER_DEFAULT = 20
+const SIZE_DEFAULT = 16;
+const SIZE_WRAPPER_DEFAULT = 16;
 export const SkeletonBar = styled.div<{ size?: 'S' | 'M' | 'L' }>`
-  width: 55%;
+  width: 70%;
   height: ${(props): string => SkeletonSize[props.size as string] || SIZE_DEFAULT}px;
   position: relative;
-  animation: ${loadingAnimation} 1s linear infinite;
+  animation: ${loadingAnimation} 1.2s ease-in-out infinite;
 `;
 
 export const Wrapper = styled.div<{ size?: 'S' | 'M' | 'L' }>`
