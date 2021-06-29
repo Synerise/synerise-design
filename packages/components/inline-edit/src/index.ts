81ea808dc84696518f0378a789aac838cf520098
tomasztaranek <tomasz.taranek@synerise.com>
2021-06-29 15:44:07 +0200

    fix(inline-edit): handle initial value

--- packages/components/inline-edit/src/index.ts
+++ packages/components/inline-edit/src/index.ts
@@ -1,2 +1,4 @@
 export { default } from './InlineEdit';
+export * from './InlineEdit.types';
+export * from './InlineSelect/InlineSelect.types';
 export { default as InlineSelect } from './InlineSelect/InlineSelect';
