6a0daeb4d2c6a02617416855f87b1cc5fb3ac0fe
tomasztaranek <tomasz.taranek@synerise.com>
2021-08-03 18:06:28 +0200

    fix(table): add onScrollTopReach prop

--- packages/components/table/src/InfiniteScroll/constants.ts
+++ packages/components/table/src/InfiniteScroll/constants.ts
@@ -9,6 +9,7 @@ export interface InfiniteScrollProps extends InfiniteScrollState {
   render?: (state: InfiniteScrollState) => React.ReactElement;
   onRetryButtonClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
   onScrollEndReach: () => void;
+  onScrollTopReach?: () => void;
 }
 
 export const infiniteLoaderItemHeight = 64;
