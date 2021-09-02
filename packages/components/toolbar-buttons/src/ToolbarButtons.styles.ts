af83a4b8e7330e49613bcccaaa4a6b0dbb6aeb40
Jakub Sidor <jakub.sidor@synerise.com>
2021-09-02 11:08:42 +0200

    fix(toolbar-buttons): fix onClick prop and styles

--- packages/components/toolbar-buttons/src/ToolbarButtons.styles.ts
+++ packages/components/toolbar-buttons/src/ToolbarButtons.styles.ts
@@ -3,11 +3,13 @@ import ButtonGroup from '@synerise/ds-button-group';
 
 export const Wrapper = styled.div`
   margin-right: 12px;
+  border-radius: 4px;
   background-color: ${(props): string => props.theme.palette.white};
   color: ${(props): string => props.theme.palette['grey-600']};
 `;
 export const WrapperButton = styled.div`
   background-color: ${(props): string => props.theme.palette.white};
+  border-radius: 4px;
 `;
 export const ToolbarWrapper = styled.div`
   display: flex;
@@ -22,7 +24,7 @@ export const ToolbarButtons = styled(ButtonGroup)`
   }
 `;
 export const ViewPercent = styled.div`
-  padding: 8px;
+  padding: 7px;
   font-weight: 500;
   color: ${(props): string => props.theme.palette['grey-600']};
 `;
--- packages/components/toolbar-buttons/src/ToolbarButtons.tsx
+++ packages/components/toolbar-buttons/src/ToolbarButtons.tsx
@@ -6,19 +6,43 @@ import Tooltip from '@synerise/ds-tooltip';
 import * as S from './ToolbarButtons.styles';
 import { ToolbarProps } from './ToolbarButtons.types';
 
+const initialButtonKeys = {
+  'Button 1': false,
+  'Button 2': false,
+  'Button 3': false,
+  'Button 4': false,
+  'Button 5': false,
+  'Button 6': false,
+  'Button 7': false,
+  'Button 8': false,
+};
 const Toolbar: React.FC<ToolbarProps> = ({ textPercent }) => {
-  const [active, setActive] = React.useState(false);
+  const [active, setActive] = React.useState([initialButtonKeys]);
+  const updateActiveKeys = (value: string, key: string): void => {
+    const newActiveKeys = { ...active, [key]: value };
+    setActive(newActiveKeys);
+  };
   return (
     <S.ToolbarWrapper>
       <S.Wrapper>
         <S.ToolbarButtons>
           <Tooltip title="back">
-            <Button activated type="ghost" mode="single-icon">
+            <Button
+              onClick={(): void => updateActiveKeys(!active['Button 1'], 'Button 1')}
+              activated={active['Button 1']}
+              type="ghost"
+              mode="single-icon"
+            >
               <Icon component={<StepBackM />} color="#ffffff" />
             </Button>
           </Tooltip>
           <Tooltip title="forward">
-            <Button activated type="ghost" mode="single-icon">
+            <Button
+              onClick={(): void => updateActiveKeys(!active['Button 2'], 'Button 2')}
+              activated={active['Button 2']}
+              type="ghost"
+              mode="single-icon"
+            >
               <Icon component={<StepForwardM />} color="#ffffff" />
             </Button>
           </Tooltip>
@@ -27,28 +51,29 @@ const Toolbar: React.FC<ToolbarProps> = ({ textPercent }) => {
       <S.Wrapper>
         <S.ToolbarButtons>
           <Tooltip title="fit to view">
-            <Button activated type="ghost" mode="single-icon">
+            <Button
+              onClick={(): void => updateActiveKeys(!active['Button 3'], 'Button 3')}
+              activated={active['Button 3']}
+              type="ghost"
+              mode="single-icon"
+            >
               <Icon component={<FullScreenM />} color="#ffffff" />
             </Button>
           </Tooltip>
-          <Tooltip title={active ? 'hide map' : 'show map'}>
+          <Tooltip title={active['Button 4'] ? 'hide map' : 'show map'}>
             <Button
-              onClick={(): void => {
-                setActive(!active);
-              }}
-              activated
+              onClick={(): void => updateActiveKeys(!active['Button 4'], 'Button 4')}
+              activated={active['Button 4']}
               type="ghost"
               mode="single-icon"
             >
               <Icon component={<LocationM />} color="#ffffff" />
             </Button>
           </Tooltip>
-          <Tooltip title={active ? 'hide notes' : 'show notes'}>
+          <Tooltip title={active['Button 5'] ? 'hide notes' : 'show notes'}>
             <Button
-              onClick={(): void => {
-                setActive(!active);
-              }}
-              activated
+              onClick={(): void => updateActiveKeys(!active['Button 5'], 'Button 5')}
+              activated={active['Button 5']}
               type="ghost"
               mode="single-icon"
             >
@@ -59,14 +84,24 @@ const Toolbar: React.FC<ToolbarProps> = ({ textPercent }) => {
       </S.Wrapper>
       <S.Wrapper>
         <S.ToolbarButtons>
-          <Tooltip title="hide map">
-            <Button activated type="ghost" mode="single-icon">
+          <Tooltip title="fit to view">
+            <Button
+              onClick={(): void => updateActiveKeys(!active['Button 6'], 'Button 6')}
+              activated={active['Button 6']}
+              type="ghost"
+              mode="single-icon"
+            >
               <Icon component={<FullScreenM />} color="#ffffff" />
             </Button>
           </Tooltip>
           <S.ViewPercent>{textPercent}</S.ViewPercent>
           <Tooltip title="hide notes">
-            <Button activated type="ghost" mode="single-icon">
+            <Button
+              onClick={(): void => updateActiveKeys(!active['Button 7'], 'Button 7')}
+              activated={active['Button 7']}
+              type="ghost"
+              mode="single-icon"
+            >
               <Icon component={<NotepadM />} color="#ffffff" />
             </Button>
           </Tooltip>
@@ -74,7 +109,12 @@ const Toolbar: React.FC<ToolbarProps> = ({ textPercent }) => {
       </S.Wrapper>
       <S.WrapperButton>
         <Tooltip title="auto align">
-          <Button activated type="ghost" mode="single-icon">
+          <Button
+            onClick={(): void => updateActiveKeys(!active['Button 8'], 'Button 8')}
+            activated={active['Button 8']}
+            type="ghost"
+            mode="single-icon"
+          >
             <Icon component={<FolderTreeM />} />
           </Button>
         </Tooltip>
