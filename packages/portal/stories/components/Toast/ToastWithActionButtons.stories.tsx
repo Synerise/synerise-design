b555b8af547c632fca82d8024514561f20636dd6
Jakub Sidor <jakub.sidor@synerise.com>
2021-09-21 13:44:05 +0200

    fix(alert): fix styles and add props

--- packages/portal/stories/components/Toast/ToastWithActionButtons.stories.tsx
+++ packages/portal/stories/components/Toast/ToastWithActionButtons.stories.tsx
@@ -73,7 +73,7 @@ const additionalAlertMapper = {
 const stories = {
   WithCloseIcon: () => {
     const message = text('Message', 'Campaign saved!');
-    const type = select('Set type', SECTION_COLOR_TYPES, 'negative');
+    const type = select('Set type', SECTION_COLOR_TYPES, 'success');
     return (
       <div
         style={{
@@ -94,7 +94,37 @@ const stories = {
   },
   WithExpander: () => {
     const message = text('Message', 'Campaign saved!');
-    const type = select('Set type', SECTION_COLOR_TYPES, 'negative');
+    const type = select('Set type', SECTION_COLOR_TYPES, 'success');
+    const [iconCopied, setIconCopied] = React.useState(false);
+    const [opened, setOpened] = React.useState(false);
+    const data = [
+      {
+        label: (
+          <OrderWrapper  color={additionalAlertMapper[type].color} >
+            <div style={{ marginRight: '10px' }}>•</div> Schedule section must be defined
+            <NumberWrapper>(505-456)</NumberWrapper>
+            <Tooltip title={iconCopied ? 'Copied!' : 'Copy'}>
+              <IconOrderWrapper  color={additionalAlertMapper[type].color} ><Icon onClick={(): void => setIconCopied(!iconCopied)} component={<DuplicateS/>}/></IconOrderWrapper>
+            </Tooltip>
+          </OrderWrapper>
+        ),
+        index: (1),
+        id:('list')
+      },
+      {
+        label: (
+          <OrderWrapper color={additionalAlertMapper[type].color} >
+            <div style={{ marginRight: '10px' }}>•</div> Missing email template
+            <NumberWrapper>(505-456)</NumberWrapper>
+            <Tooltip title={iconCopied ? 'Copied!' : 'Copy'}>
+              <IconOrderWrapper color={additionalAlertMapper[type].color}><Icon onClick={(): void => setIconCopied(!iconCopied)} component={<DuplicateS/>}/></IconOrderWrapper>
+            </Tooltip>
+          </OrderWrapper>
+        ),
+        index: (1),
+        id:('list')
+      },
+    ];
     return (
       <div
         style={{
@@ -107,15 +137,20 @@ const stories = {
           message={message}
           type={type}
           expander
+          expanded={opened}
+          onExpand={setOpened}
           color={additionalAlertMapper[type].color}
           colorIcon={additionalAlertMapper[type].colorIcon}
+          expandedContent={<Wrapper>
+            <UnorderedList data={data} indexFormatter={undefined} />
+          </Wrapper>}
         />
       </div>
     );
   },
   WithActionButton: () => {
     const message = text('Message', 'Campaign saved!');
-    const type = select('Set type', SECTION_COLOR_TYPES, 'negative');
+    const type = select('Set type', SECTION_COLOR_TYPES, 'success');
     const description = text('Description', 'No response from server, try again later');
     return (
       <div
