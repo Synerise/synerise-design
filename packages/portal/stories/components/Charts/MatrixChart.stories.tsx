582bafbec7240f9be9b76e61a7ef131a89ef9068
michał osajda <michal.osajda@synerise.com>
2021-09-23 12:31:39 +0200

    chore: update yarn.lock

--- packages/portal/stories/components/Charts/MatrixChart.stories.tsx
+++ packages/portal/stories/components/Charts/MatrixChart.stories.tsx
@@ -71,7 +71,7 @@ const stories = {
     ];
     const categoriesY = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6'];
 
-    return <MatrixChart data={data} categoriesX={categoriesX} categoriesY={categoriesY} width={500} />;
+    return <MatrixChart data={data} categoriesX={categoriesX} categoriesY={categoriesY} width={1000} />;
   },
 };
 
