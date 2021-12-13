96903a480933308254d7e9df04a4ff4cd0e0411d
Jakub Sidor <jakub.sidor@synerise.com>
2021-12-13 11:00:23 +0100

    fix(time-picker): remove console logs

--- packages/components/time-picker/src/Unit.tsx
+++ packages/components/time-picker/src/Unit.tsx
@@ -36,17 +36,6 @@ const Unit: React.FC<UnitProps> = ({ options, disabled, value, unit, onSelect })
     }
   }, [isFirstRender, unitContainerRef]);
 
-  React.useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    console.log('unit.options', unit, options);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    console.dir(options);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-  }, [options, unit]);
-
   const scrollHandler = React.useCallback(() => {
     if (!!unitContainerRef && !!unitContainerRef.current) {
       const pixelsScrolled = unitContainerRef.current.scrollTop;
