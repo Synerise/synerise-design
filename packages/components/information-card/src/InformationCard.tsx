ee3ad8bdc8ec793250415d1e25b78ed3504e33d4
Artur Z <az-166@synerise.com>
2022-04-22 14:57:08 +0200

    feat(information-card): create information-card component
    yarn -W root component:create

---
+++ packages/components/information-card/src/InformationCard.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+export type InformationCardProps = {};
+const InformationCard: React.FC<InformationCardProps> = props => {
+   return (
+       <div>InformationCard example</div>
+   );
+};
+export default InformationCard;
---
+++ packages/portal/stories/components/InformationCard/index.stories.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import InformationCard from '@synerise/ds-information-card';
+
+const stories = {
+  default: () => ({}),
+};
+
+export default {
+name: 'Components/InformationCard',
+  config: {},
+  stories,
+  Component: InformationCard,
+}
