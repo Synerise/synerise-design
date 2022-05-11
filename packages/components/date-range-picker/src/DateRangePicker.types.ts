4d32fb5e9dbf88c24e022a2f12fb1665a2154c24
michał osajda <michal.osajda@synerise.com>
2022-05-11 17:39:47 +0200

    chore(date-range-picker): updates types

--- packages/components/date-range-picker/src/DateRangePicker.types.ts
+++ packages/components/date-range-picker/src/DateRangePicker.types.ts
@@ -13,7 +13,7 @@ export interface Props extends WrappedComponentProps {
   forceAbsolute?: boolean;
   onValueChange?: (value: Partial<DateFilter> | undefined) => void;
   onApply: (value: Partial<DateFilter> | undefined) => void;
-  onVisibleChange?: Pick<PopoverProps, 'onVisibleChange'>;
+  onVisibleChange?: (visible: boolean) => void;
   onFilterSave?: (filters: SavedFilter[]) => void;
   popoverProps?: Partial<PopoverProps>;
   popoverTrigger?: React.ReactNode;
