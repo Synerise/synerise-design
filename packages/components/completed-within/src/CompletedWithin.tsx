b9e05208d75069b03e9671981ea061f0e6183c99
michał osajda <michal.osajda@synerise.com>
2022-05-11 16:45:22 +0200

    fix(completed-within): fixes clearing tooltip

--- packages/components/completed-within/src/CompletedWithin.tsx
+++ packages/components/completed-within/src/CompletedWithin.tsx
@@ -51,20 +51,20 @@ const CompletedWithin: React.FC<CompletedWithinProps> = ({
   const [innerValue, setInnerValue] = React.useState<string | number | undefined>(value.value);
   const [innerPeriod, setInnerPeriod] = React.useState<Period>(value.period);
   const [tooltipVisible, setTooltipVisible] = React.useState(false);
+  const [settingsVisible, setSettingsVisible] = React.useState(false);
 
   const hasValue = React.useMemo(() => value.value !== undefined && value.value > 0, [value]);
 
   const handleVisibleChange = React.useCallback(
     (visible: boolean) => {
+      setSettingsVisible(visible);
+      setTooltipVisible(false);
       if (!visible && innerValue && innerPeriod) {
         const newValue = maxValue && maxValue < innerValue ? maxValue : innerValue;
         onSetValue({
           value: Number(newValue),
           period: innerPeriod,
         });
-      }
-      if (visible) {
-        setTooltipVisible(false);
       }
     },
     [innerPeriod, innerValue, maxValue, onSetValue]
@@ -111,7 +111,7 @@ const CompletedWithin: React.FC<CompletedWithinProps> = ({
           description={tooltip}
           trigger={['hover']}
           onVisibleChange={setTooltipVisible}
-          visible={tooltipVisible}
+          visible={!settingsVisible && tooltipVisible}
         >
           <S.TriggerButton className="ds-completed-within" type="tertiary" mode={triggerMode}>
             <Icon component={<ClockM />} />
