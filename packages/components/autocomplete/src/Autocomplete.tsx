5dceb7ffb7ede3c907ccd667b4dd2db946654b19
michał osajda <michal.osajda@synerise.com>
2022-06-09 16:30:07 +0200

    chore(autocomplete): transform component from class to FC

--- packages/components/autocomplete/src/Autocomplete.tsx
+++ packages/components/autocomplete/src/Autocomplete.tsx
@@ -3,38 +3,47 @@ import '@synerise/ds-core/dist/js/style';
 import './style/index.less';
 import AntdAutoComplete from 'antd/lib/auto-complete';
 import { ErrorText, Description, Label } from '@synerise/ds-typography';
-import { AutocompleteProps } from 'Autocomplete.types';
+import { AutocompleteProps, StaticComponents } from 'Autocomplete.types';
+import Select from 'antd/lib/select';
 import * as S from './Autocomplete.styles';
 
-class Autocomplete extends React.PureComponent<AutocompleteProps> {
-  static Option = AntdAutoComplete.Option;
+const Autocomplete: React.FC<AutocompleteProps> & StaticComponents = props => {
+  const { className, label, description, errorText, disabled, error, handleInputRef } = props;
+  const inputRef = React.useRef<Select | undefined>(undefined);
 
-  render(): React.ReactElement {
-    const { className, label, description, errorText, disabled, error } = this.props;
-    return (
-      <S.AutocompleteWrapper className={`ds-autocomplete ${className || ''}`}>
-        {label && (
-          <S.LabelWrapper>
-            <Label>{label}</Label>
-          </S.LabelWrapper>
-        )}
-        <AntdAutoComplete
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          {...this.props}
-          dropdownClassName="ds-autocomplete-dropdown"
-          className={!!errorText || error ? 'error' : undefined}
-        />
-        {errorText && (
-          <S.ErrorWrapper>
-            <ErrorText>{errorText}</ErrorText>
-          </S.ErrorWrapper>
-        )}
-        {description && (
-          <S.DescWrapper>{description && <Description disabled={disabled}>{description}</Description>}</S.DescWrapper>
-        )}
-      </S.AutocompleteWrapper>
-    );
-  }
-}
+  React.useEffect(() => {
+    handleInputRef && handleInputRef(inputRef);
+  }, [inputRef, handleInputRef]);
+
+  return (
+    <S.AutocompleteWrapper className={`ds-autocomplete ${className || ''}`}>
+      {label && (
+        <S.LabelWrapper>
+          <Label>{label}</Label>
+        </S.LabelWrapper>
+      )}
+      <AntdAutoComplete
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        ref={inputRef}
+        defaultOpen
+        dropdownClassName="ds-autocomplete-dropdown"
+        className={!!errorText || error ? 'error' : undefined}
+      />
+      {errorText && (
+        <S.ErrorWrapper>
+          <ErrorText>{errorText}</ErrorText>
+        </S.ErrorWrapper>
+      )}
+      {description && (
+        <S.DescWrapper>{description && <Description disabled={disabled}>{description}</Description>}</S.DescWrapper>
+      )}
+    </S.AutocompleteWrapper>
+  );
+};
+
+Autocomplete.Option = AntdAutoComplete.Option;
 
 export default Autocomplete;
