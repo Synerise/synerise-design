26beb90854efe80e65b4a0c4ad47bf56eb94a292
michał osajda <michal.osajda@synerise.com>
2022-06-14 12:48:39 +0200

    fix(factors): adds custom arrow color

--- packages/components/factors/src/FactorValue/DateRange/DateRange.tsx
+++ packages/components/factors/src/FactorValue/DateRange/DateRange.tsx
@@ -39,7 +39,16 @@ const DateRangeInput: React.FC<InputProps> = ({ value, onChange, texts, onDeacti
         placement: 'bottomLeft',
         onVisibleChange: handleVisibleChange,
       }}
-      arrowColor={{ bottomLeft: 'grey' }}
+      arrowColor={{
+        bottomLeft: 'grey',
+        bottomRight: 'grey',
+        topLeft: 'grey',
+        topRight: 'grey',
+        leftTop: 'grey',
+        leftBottom: 'grey',
+        rightTop: 'grey',
+        rightBottom: 'grey',
+      }}
     />
   );
 };
