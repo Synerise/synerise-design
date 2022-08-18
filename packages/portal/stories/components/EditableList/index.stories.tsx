78f928642885d4268976c5c722861ed694b408de
Jakub Sidor <jakub.sidor@synerise.com>
2022-08-18 12:06:40 +0200

    feat(form): add new component editable-list

---
+++ packages/portal/stories/components/EditableList/index.stories.tsx
@@ -0,0 +1,85 @@
+import { text } from '@storybook/addon-knobs';
+import * as React from 'react';
+import { action } from '@storybook/addon-actions';
+import Autocomplete from '@synerise/ds-autocomplete';
+import EditableList from '@synerise/ds-form/dist/Editable-List/editable-list';
+import { escapeRegEx } from '@synerise/ds-utils';
+
+const renderLabel = (text: string) => {
+  return <div style={{ maxWidth: '200px', textOverflow: 'ellipsis', overflow: 'hidden' }}>{text}</div>;
+};
+
+const dataSource = ['Score', 'Season', 'Theme', 'Departament'];
+
+const stories = {
+  default: () => {
+    const [results, setResults] = React.useState<string[]>([]);
+    const [value, setValue] = React.useState<string>('');
+    const buttonText = text('Button text', 'Add parametr');
+
+    const renderWithHighlightedText = (highlight, item): React.ReactNode => {
+      if (highlight && typeof item === 'string') {
+        const index = item.toLocaleLowerCase().indexOf(highlight.toLocaleLowerCase());
+        if (index === -1) {
+          return item;
+        }
+        const escapedHighlight = escapeRegEx(highlight);
+        const startOfQuery = item.toLowerCase().search(escapedHighlight.toLowerCase());
+        const endOfQuery = startOfQuery + highlight.length;
+        const resultArray = [
+          item.substring(0, startOfQuery),
+          <span key={item} style={{ fontWeight: 600 }} className="search-highlight">
+          {item.substring(startOfQuery, endOfQuery)}
+        </span>,
+          item.substring(endOfQuery, item.length),
+        ];
+        return resultArray;
+      }
+      return item;
+    };
+
+    const handleSearch = value => {
+      let result;
+      if (!value || value.indexOf('@') >= 0) {
+        result = [];
+      } else {
+        result = dataSource.filter(item => item.toLowerCase().includes(value.toLowerCase()));
+      }
+      setResults(result);
+    };
+
+    const extractContent = (str: string) => {
+      const span = document.createElement('span');
+      span.innerHTML = str;
+      return span.textContent || span.innerText;
+    };
+
+    return (
+      <EditableList
+        onSearch={handleSearch}
+        leftColumnName={renderLabel(text('Select label', 'Parametr'))}
+        rightColumnName={renderLabel(text('Input label', 'Value'))}
+        value={value === 'undefined' ? '' : value}
+        onChange={(value: string) => {
+          action('onChange')(value);
+          setValue(extractContent(value));
+          handleSearch(extractContent(value));
+        }}
+        onRemove={() => {}}
+        onClick={() => {}}
+        text={buttonText}
+        autocompleteOptions={results.map(result => (
+          <Autocomplete.Option key={result}>
+            <span style={{ fontWeight: 400 }}>{renderWithHighlightedText(value, result)}</span>
+          </Autocomplete.Option>
+        ))}
+      />
+    )
+  },
+};
+
+export default {
+  name: 'Components/Form/EditableList',
+  stories,
+  Component: EditableList
+};
