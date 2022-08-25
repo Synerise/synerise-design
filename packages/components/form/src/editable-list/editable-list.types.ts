54a26997a4b6d136273da47f20b14ff17939119b
Marek Marchewka <marek.marchewka@synerise.com>
2022-08-25 14:35:42 +0200

    feat(form): add index for render column functions in EditableList

--- packages/components/form/src/editable-list/editable-list.types.ts
+++ packages/components/form/src/editable-list/editable-list.types.ts
@@ -9,10 +9,11 @@ export type EditListProps = {
   value?: EditableParam[];
   onChange?: (params: EditableParam[]) => void;
   onSearch?: (query: string) => void;
+  onSetParams?: () => void;
   textAddButton?: string;
   renderAddButton?: (params?: EditableParam[]) => JSX.Element;
-  renderLeftColumn?: (props?: EditableParam) => JSX.Element;
-  renderRightColumn?: (props?: EditableParam, ref?: React.ClassAttributes<{}>['ref']) => JSX.Element;
+  renderLeftColumn?: (index: number, param?: EditableParam) => JSX.Element;
+  renderRightColumn?: (index: number, param?: EditableParam, ref?: React.ClassAttributes<{}>['ref']) => JSX.Element;
   renderAdditionalColumn?: (row?: EditableParam[]) => JSX.Element;
   renderActions?: boolean | ((param?: EditableParam, idx?: number, params?: EditableParam[]) => JSX.Element);
 };
