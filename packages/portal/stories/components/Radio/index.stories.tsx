0e5c085bccf108c94622af8a2389eb87708f3913
Jakub Sidor <jakub.sidor@synerise.com>
2022-09-06 11:16:04 +0200

    fix(radio): fix knobs

--- packages/portal/stories/components/Radio/index.stories.tsx
+++ packages/portal/stories/components/Radio/index.stories.tsx
@@ -6,24 +6,28 @@ import Radio from '@synerise/ds-radio';
 import markdown from '@/radio/README.md';
 
 const stories = {
-  default: () => (
+
+  default: () => {
+    const tooltipTitle = text('tooltip label', 'Label');
+    const setTooltip = boolean('show tooltip', false)
+    return (
     <Radio.Group onChange={action('onChange')} defaultValue="A">
       <Radio
         disabled={boolean('disabled', false)}
         description={text('description', 'Description')}
-        title={text('tooltip label', 'Label')}
-        tooltip={boolean('show tooltip', false)}
+        title={tooltipTitle}
+        tooltip={setTooltip}
         value="A"
       />
       <Radio
         disabled={boolean('disabled', false)}
         description={text('description', 'Description')}
-        title={text('tooltip label', 'Label')}
-        tooltip={boolean('show tooltip', false)}
+        title={tooltipTitle}
+        tooltip={setTooltip}
         value="B"
       />
     </Radio.Group>
-  ),
+  )},
   radioButtons: () => (
     <Radio.Group defaultValue="a" buttonStyle="solid" onChange={action('onChange')} fullWidth={boolean('fullWidth', false)} big={boolean('big', false)}>
       <Radio.Button value="a">A</Radio.Button>
