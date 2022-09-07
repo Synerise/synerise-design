1cd5456fc3c20c4d604fc35c21ace871bb50502d
Jakub Sidor <jakub.sidor@synerise.com>
2022-09-07 11:28:40 +0200

    fix(radio): update knobs of radio buttons

--- packages/portal/stories/components/Radio/index.stories.tsx
+++ packages/portal/stories/components/Radio/index.stories.tsx
@@ -10,32 +10,41 @@ const stories = {
   default: () => {
     const tooltipTitle = text('tooltip label', 'Label');
     const setTooltip = boolean('show tooltip', false)
+    const setDescription = text('description', 'Description');
+    const setDisabled = boolean('disabled', false)
     return (
     <Radio.Group onChange={action('onChange')} defaultValue="A">
       <Radio
-        disabled={boolean('disabled', false)}
-        description={text('description', 'Description')}
+        disabled={setDisabled}
+        description={setDescription}
         title={tooltipTitle}
         tooltip={setTooltip}
         value="A"
       />
       <Radio
-        disabled={boolean('disabled', false)}
-        description={text('description', 'Description')}
+        disabled={setDisabled}
+        description={setDescription}
         title={tooltipTitle}
         tooltip={setTooltip}
         value="B"
       />
     </Radio.Group>
   )},
-  radioButtons: () => (
-    <Radio.Group defaultValue="a" buttonStyle="solid" onChange={action('onChange')} fullWidth={boolean('fullWidth', false)} big={boolean('big', false)}>
+  radioButtons: () => {
+    const setFullWidth = boolean('disabled', false);
+    const setDisabled = boolean('disabled B', false);
+    const setBig = boolean('disabled', false)
+
+    return (
+    <Radio.Group defaultValue="a" buttonStyle="solid" onChange={action('onChange')}
+                 fullWidth={setFullWidth} big={setBig}>
       <Radio.Button value="a">A</Radio.Button>
-      <Radio.Button value="b" disabled={boolean('disable B', false)}>B</Radio.Button>
+      <Radio.Button value="b" disabled={setDisabled}>B</Radio.Button>
       <Radio.Button value="c">C</Radio.Button>
       <Radio.Button value="d">D</Radio.Button>
     </Radio.Group>
-  ),
+    )
+  },
 };
 
 export default {
