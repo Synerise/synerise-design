a362eb185e51dae934f7289f23dbf69d9fcc0fc3
Grzegorz Morzycki <grzegorz.morzycki@synerise.com>
2022-11-08 13:38:50 +0100

    fix(card): force ds-card release

--- packages/components/card/src/CardBadge/CardBadge.tsx
+++ packages/components/card/src/CardBadge/CardBadge.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
+
 import Icon from '@synerise/ds-icon';
+
 import * as S from './CardBadge.styles';
 import { CardBadgeProps } from './CardBadge.types';
 
-const CardBadge: React.FC<CardBadgeProps> = ({ icon, status = 'default', ...props }) => {
-  return (
-    <S.CardBadge
-      status={status}
-      /** Necessary for passing down data-* attributes */
-      {...props}
-    >
-      <Icon component={icon} />
-    </S.CardBadge>
-  );
-};
+const CardBadge: React.FC<CardBadgeProps> = ({ icon, status = 'default', ...props }) => (
+  <S.CardBadge
+    status={status}
+    /** Necessary for passing down data-* attributes */
+    {...props}
+  >
+    <Icon component={icon} />
+  </S.CardBadge>
+);
 
 export default CardBadge;
