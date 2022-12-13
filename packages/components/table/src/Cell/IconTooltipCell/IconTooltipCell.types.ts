578d6de6419dcc20c2e5b3ae10ccafc32e33b569
Tomek Kaminski <tomasz.kaminski@synerise.com>
2022-12-13 15:51:59 +0100

    fix(table): make prop optional
    affects: @synerise/ds-table
    
    Make prop optional

--- packages/components/table/src/Cell/IconTooltipCell/IconTooltipCell.types.ts
+++ packages/components/table/src/Cell/IconTooltipCell/IconTooltipCell.types.ts
@@ -4,6 +4,6 @@ import TooltipExtendedProps from '@synerise/ds-tooltip/dist/Tooltip.types';
 export interface Props {
   label: string | React.ReactNode;
   icon: IconProps;
-  tooltipIcon: IconProps;
+  tooltipIcon?: IconProps;
   tooltip?: TooltipExtendedProps;
 }
