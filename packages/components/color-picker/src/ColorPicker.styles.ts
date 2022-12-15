eca2c514e61bb1835925a14e9debbb8b46dbec22
Jakub Sidor <jakub.sidor@synerise.com>
2022-12-15 13:57:45 +0100

    fix(color-picker): fix adding dot to tags and typing,copy text

--- packages/components/color-picker/src/ColorPicker.styles.ts
+++ packages/components/color-picker/src/ColorPicker.styles.ts
@@ -1,29 +1,163 @@
 import styled from 'styled-components';
-import { TagsStyles } from '@synerise/ds-tags';
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import { TagsStyles, Tag } from '@synerise/ds-tags';
+import Select from '@synerise/ds-select';
+import Button from '@synerise/ds-button';
+import Icon from '@synerise/ds-icon';
+import { Input } from '@synerise/ds-input';
+import { BorderLessInput } from '@synerise/ds-input/dist/InputMultivalue/InputMultivalue.styles';
+import { ColorPickerSize } from './ColorPicker.types';
 
-const Container = styled.div`
+export const TagDot = styled.div<{ pressed?: boolean }>`
+  display: ${(props): string => (props.pressed ? 'flex' : 'none')};
+  width: 4px;
+  height: 4px;
+  background-color: ${(props): string => props.theme.palette.white};
+  border-radius: 50%;
+`;
+const SIZE_DEFAULT = 167;
+const Container = styled.div<{ size?: 'S' | 'M' | 'L' }>`
   @media (min-width: 200px) {
     min-width: 200px;
+    margin: -8px;
   }
   .react-colorful__last-control {
-    margin: 16px 46px 8px 16px;
+    margin: 24px 56px 8px 18px;
     border-radius: 4px;
     height: 8px;
+  }
+  .react-colorful__saturation {
+    border-bottom: 0px;
+    border-radius: 4px 4px 0 0;
+    height: 96px;
+    .react-colorful__interactive {
+      height: 96px;
+    }
+  }
+  &&& .ds-tag {
+    width: 16px;
+    height: 16px;
+    margin: 0px 4px 8px;
+    border: 1px solid ${(props): string => props.theme.palette['grey-300']};
+
+    &:hover {
+      ${TagDot} {
+        display: flex;
+      }
+    }
+  }
+
+  .react-colorful__pointer {
+    width: 16px;
+    height: 16px;
+  }
+  .react-colorful {
+    width: 262px;
+    height: ${(props): string => ColorPickerSize[props.size as string] || SIZE_DEFAULT}px;
+  }
+  .react-colorful__hue-pointer {
+    border: 1px solid ${(props): string => props.theme.palette['grey-300']};
+    box-shadow: none !important;
+    .react-colorful__pointer-fill {
+      background-color: ${(props): string => props.theme.palette.white} !important;
+    }
   }
   .ant-divider-horizontal {
     margin: 16px 0px;
   }
+
   ${TagsStyles.Container} {
     margin-bottom: -8px;
   }
 `;
+const SubContainer = styled.div`
+  padding: 8px 16px 16px;
+`;
 
-const Subcontainer = styled.div`
-  padding: 16px;
-  padding-top: 8px;
+export const ColorTag = styled(Tag)`
+  width: 16px;
+  height: 16px;
+  margin: 7px 8px 0px;
+  border: 1px solid ${(props): string => props.theme.palette['grey-300']};
+`;
+
+export const SelectInput = styled(BorderLessInput)`
+  width: 80px;
+`;
+
+export const SelectColorPicker = styled(Select)`
+  .ant-select-single {
+    .ant-select-selector {
+      padding: 0px 12px 0px 0px;
+      .ant-select-selection-search-input {
+        padding-left: 15px;
+      }
+      .ant-select-selection-item {
+        margin-left: -3px;
+      }
+    }
+  }
+  min-width: 100px;
+`;
+export const AddColorButton = styled(Button)`
+  &&&.ant-btn {
+    width: 16px;
+    height: 16px;
+    margin-right: 2px;
+  }
+`;
+export const CopyIcon = styled(Icon)`
+  display: none;
+`;
+export const ColorPickerInput = styled(Input)`
+  &:hover {
+    ${CopyIcon} {
+      display: flex;
+    }
+  }
+`;
+export const PrefixTag = styled.div<{ height?: boolean }>`
+  &&& .ds-tag {
+    width: 24px;
+    height: 24px;
+    border: 1px solid ${(props): string => props.theme.palette['grey-300']};
+    cursor: auto;
+    &:hover:before {
+      cursor: auto;
+      filter: brightness(100%);
+    }
+  }
+  position: absolute;
+  right: ${(props): string => (props.height ? '12px' : '12px')};
+  bottom: ${(props): string => (props.height ? '97px' : '65px')};
+  z-index: 2;
+  cursor: auto;
+`;
+export const SwatchSectionWrapper = styled.div`
+  display: flex;
+  alignitems: center;
+`;
+export const PreffixWrapper = styled.div`
+  padding: 6px 12px;
+`;
+export const ValueWrapper = styled.div`
+  display: flex;
+  font-size: 12px;
 `;
 
 export default {
   Container,
-  Subcontainer,
+  SubContainer,
+  ColorTag,
+  PrefixTag,
+  SelectColorPicker,
+  AddColorButton,
+  SwatchSectionWrapper,
+  PreffixWrapper,
+  ValueWrapper,
+  CopyIcon,
+  ColorPickerInput,
+  TagDot,
+  SelectInput,
 };
--- packages/components/color-picker/src/ColorPicker.tsx
+++ packages/components/color-picker/src/ColorPicker.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { HexColorPicker as ReactColorful } from 'react-colorful';
-import Select from '@synerise/ds-select';
-import Button from '@synerise/ds-button';
 import Divider from '@synerise/ds-divider';
-import { Input } from '@synerise/ds-input';
-import Tags, { TagShape } from '@synerise/ds-tags';
+import Tags, { TagShape, Tag } from '@synerise/ds-tags';
 import Icon, { FormulaPlusM } from '@synerise/ds-icon';
-import ColorPickerStyles from './ColorPicker.styles';
+import { CopyClipboardM } from '@synerise/ds-icon/src';
+import * as copy from 'copy-to-clipboard';
+import Tooltip from '@synerise/ds-tooltip';
 import { ColorPickerProps } from './ColorPicker.types';
+import ColorPickerStyles from './ColorPicker.styles';
 
 export function ColorPicker({
   value = '#ffffff',
@@ -17,14 +17,19 @@ export function ColorPicker({
   infix = (): JSX.Element => <></>,
   placeholder,
   selectProps,
-  maxSavedColors = 10,
+  maxSavedColors = 8,
+  tooltipText,
+  isShownSavedColors,
+  size = 'M',
 }: ColorPickerProps): JSX.Element {
   const [color, setColor] = React.useState(value);
+  const [pressed, setPressed] = React.useState<boolean[]>([]);
   const onChangeColor = (colorValue: string): void => {
     setColor(colorValue);
     onChange && onChange(colorValue);
   };
   const [savedColors, setSavedColors] = React.useState(colors);
+  const hash = '#';
   const saveColor = (): void => {
     setSavedColors(ar => {
       const colorsArray = (color ? [color, ...ar] : ar).slice(0, maxSavedColors);
@@ -32,49 +37,88 @@ export function ColorPicker({
       return colorsArray;
     });
   };
+  const handleSearch = (colorName: string): void => setColor(colorName);
   const swatchSection = (
-    <>
-      <Button mode="single-icon" onClick={saveColor}>
-        <Icon component={<FormulaPlusM />} />
-      </Button>
+    <ColorPickerStyles.SwatchSectionWrapper>
+      <Tooltip title="Save color swatch">
+        <ColorPickerStyles.AddColorButton mode="single-icon" type="ghost" onClick={saveColor}>
+          <Icon size={16} component={<FormulaPlusM />} />
+        </ColorPickerStyles.AddColorButton>
+      </Tooltip>
       {infix({ color, setColor: onChangeColor })}
       {savedColors.length > 0 && (
         <Tags
           tagShape={TagShape.SINGLE_CHARACTER_SQUARE}
+          // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+          // @ts-ignore
           selected={savedColors.map((colorEntry, i) => ({
             id: i,
             key: `color-${i}`,
-            name: '',
+            name: (
+              <Tooltip title={colorEntry}>
+                <ColorPickerStyles.TagDot pressed={pressed[i]} />
+              </Tooltip>
+            ),
             color: colorEntry,
             onClick(): void {
               onChangeColor(colorEntry);
+              setPressed(() => {
+                const newPressed = [...pressed];
+                newPressed[i] = !pressed[i];
+                return newPressed;
+              });
             },
           }))}
           disabled={false}
         />
       )}
-    </>
+    </ColorPickerStyles.SwatchSectionWrapper>
   );
   const dropdown = (
-    <ColorPickerStyles.Container>
+    <ColorPickerStyles.Container size={size}>
       <ReactColorful color={color} onChange={onChangeColor} />
-      <ColorPickerStyles.Subcontainer>
-        <Input
-          value={color}
-          onChange={(ev: React.ChangeEvent<HTMLInputElement>): void => setColor(ev.target.value)}
+      <ColorPickerStyles.PrefixTag height={isShownSavedColors}>
+        <Tag shape={TagShape.SINGLE_CHARACTER_SQUARE} color={color} disabled={false} />
+      </ColorPickerStyles.PrefixTag>
+      <ColorPickerStyles.SubContainer>
+        <ColorPickerStyles.ColorPickerInput
+          value={color.substr(1)}
+          prefixel={<ColorPickerStyles.PreffixWrapper>#</ColorPickerStyles.PreffixWrapper>}
+          onChange={(ev: React.ChangeEvent<HTMLInputElement>): void => setColor(hash + ev.target.value)}
           placeholder={placeholder}
+          icon1={
+            <ColorPickerStyles.CopyIcon
+              onClick={(): void => {
+                copy(color);
+              }}
+              component={<CopyClipboardM />}
+            />
+          }
+          icon1Tooltip={<span>{tooltipText}</span>}
         />
-        <Divider />
-        {swatchSection}
-      </ColorPickerStyles.Subcontainer>
+        {isShownSavedColors && (
+          <div>
+            <Divider />
+            {swatchSection}
+          </div>
+        )}
+      </ColorPickerStyles.SubContainer>
     </ColorPickerStyles.Container>
   );
   return (
     <>
-      <Select
-        showSearch
+      <ColorPickerStyles.SelectColorPicker
         showArrow={false}
-        value={color}
+        onSearch={handleSearch}
+        showSearch
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        value={
+          <ColorPickerStyles.ValueWrapper>
+            <ColorPickerStyles.ColorTag shape={TagShape.SINGLE_CHARACTER_ROUND} color={color} disabled={false} />
+            <div>{color}</div>
+          </ColorPickerStyles.ValueWrapper>
+        }
         dropdownMatchSelectWidth={false}
         filterOption={false}
         dropdownRender={(): JSX.Element => dropdown}
