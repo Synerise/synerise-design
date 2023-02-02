34eaa9bb4a04e848c0640c01f059f9ae195815c6
Kamil Balus <kamil.balus@synerise.com>
2023-02-02 19:50:52 +0100

    fix(autocomplete): refactor parentNode func
    affects: @synerise/ds-autocomplete

--- packages/components/autocomplete/src/Autocomplete.tsx
+++ packages/components/autocomplete/src/Autocomplete.tsx
@@ -28,6 +28,10 @@ const Autocomplete: React.FC<AutocompleteProps> & StaticComponents = props => {
     handleInputRef && handleInputRef(inputRef);
   }, [inputRef, handleInputRef]);
 
+  const getParentNode = (triggerNode: HTMLElement): HTMLElement => {
+    return triggerNode.parentNode as HTMLElement;
+  };
+
   const renderAutoCompleteComponent = (): React.ReactNode => {
     return (
       <AntdAutoComplete
@@ -38,8 +42,7 @@ const Autocomplete: React.FC<AutocompleteProps> & StaticComponents = props => {
         ref={inputRef}
         dropdownClassName="ds-autocomplete-dropdown"
         className={!!errorText || error ? 'error' : undefined}
-        /* eslint-disable-next-line @typescript-eslint/explicit-function-return-type */
-        getPopupContainer={trigger => trigger.parentNode}
+        getPopupContainer={getParentNode}
       />
     );
   };
