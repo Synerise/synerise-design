07e7148667869706b170afeb8faa9f0128525618
msadlo <mateusz.sadlo@synerise.com>
2023-02-22 09:36:27 +0100

    fix(data-format): newline-after-import fix
    affects: @synerise/ds-data-format

--- packages/components/data-format/src/hooks/useSingleIntl.ts
+++ packages/components/data-format/src/hooks/useSingleIntl.ts
@@ -1,4 +1,5 @@
 import { createIntl, createIntlCache, IntlShape } from 'react-intl';
+
 export const useSingleIntl = (locale: string): { intl: IntlShape } => {
   const cache = createIntlCache();
   const intl = createIntl({ locale }, cache);
