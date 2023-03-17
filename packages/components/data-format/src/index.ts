59e60966cb3182d1d15e2aec85f0c4fc93ab315e
msadlo <mateusz.sadlo@synerise.com>
2023-03-17 10:58:58 +0100

    refactor(data-format): extends index.ts exports options
    affects: @synerise/ds-data-format
    
    ISSUES CLOSED: CFM-265

--- packages/components/data-format/src/index.ts
+++ packages/components/data-format/src/index.ts
@@ -1,15 +1,9 @@
 export { DataFormatConfigProvider } from './providers/DataFormatConfigProvider';
 export type { DataFormatConfigProviderProps } from './providers/DataFormatConfigProvider';
 
-export { FormattedDate } from './components';
-export { FormattedDateTime } from './components';
-export { FormattedNumber } from './components';
-export { FormattedTime } from './components';
+export { FormattedDate, FormattedDateTime, FormattedNumber, FormattedTime } from './components';
 
-export { useDataFormat } from './hooks';
-export { useDataFormatUtils } from './hooks';
-export { useDataFormatConfig } from './hooks';
-export { useDataFormatIntls } from './hooks';
+export { useDataFormat, useDataFormatUtils, useDataFormatConfig, useDataFormatIntls } from './hooks';
 export type { UseDataFormatProps } from './hooks';
 
 export { withDataFormat } from './hocs/withDataFormat';
@@ -17,7 +11,7 @@ export type { WithDataFormatProps } from './hocs/withDataFormat';
 
 export { getDataFormatConfigFromNotation, getDefaultDataTimeOptions } from './utils';
 
-export { DEFAULT_DATA_FORMAT_CONFIG, DEFAULT_DATA_FORMAT_NOTATION, US_NOTATION, EU_NOTATION } from './constants';
+export * from './constants';
 
 export type {
   DataFormatNotationType,
@@ -25,4 +19,14 @@ export type {
   DateToFormatOptions,
   DataFormatConfig,
   Delimiter,
+  TargetFormat,
+  NamingConvention,
+  DateToFormat,
+  OverloadFormatValue,
+  OverloadFormatMultipleValues,
+  CommonFormatOptions,
+  DateConstantsTargetFormat,
+  OverloadGetConstants,
+  DateTimePartFormat,
+  DataFormatIntls,
 } from './types';
