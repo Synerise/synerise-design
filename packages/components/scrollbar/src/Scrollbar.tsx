c3c6a6cc8d7eefc07be308454dc14cffdb96319d
michał osajda <michal.osajda@synerise.com>
2023-03-30 14:27:36 +0200

    feat(scrollbar): improves styles of the loading state

--- packages/components/scrollbar/src/Scrollbar.tsx
+++ packages/components/scrollbar/src/Scrollbar.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import Icon, { SpinnerM } from '@synerise/ds-icon';
+import theme from '@synerise/ds-core/dist/js/DSProvider/ThemeProvider/theme';
+
 import * as S from './Scrollbar.styles';
 import { ScrollbarProps } from './Scrollbar.types';
 import { DnDScrollbar } from './DnDScrollbar';
@@ -17,14 +19,14 @@ const Scrollbar = React.forwardRef<HTMLElement, ScrollbarProps>(
       );
     }, [children, fetchData, forwardedRef, props]);
 
-    return fetchData ? (
+    return loading ? (
       <S.ScrollbarContainer>
         {renderScrollbar}
-        {loading && (
+        <S.LoaderWrapper>
           <S.Loader loading={loading}>
-            <Icon component={<SpinnerM />} color="#6a7580" />
+            <Icon component={<SpinnerM />} color={theme.palette['grey-600']} />
           </S.Loader>
-        )}
+        </S.LoaderWrapper>
       </S.ScrollbarContainer>
     ) : (
       renderScrollbar
