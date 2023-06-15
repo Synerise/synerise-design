dda1d6901aa15b036bc58af83aaf6a3fb989a5a9
malgorzata.wierzbicka <malgorzata.wierzbicka@synerise.com>
2023-06-15 12:08:36 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- packages/components/table/src/Cell/IconTooltipCell/IconTooltipCell.types.ts
+++ packages/components/table/src/Cell/IconTooltipCell/IconTooltipCell.types.ts
@@ -2,7 +2,7 @@ import { IconProps } from '@synerise/ds-icon';
 import TooltipExtendedProps from '@synerise/ds-tooltip/dist/Tooltip.types';
 
 export interface Props {
-  label?: string | React.ReactNode;
+  label?: React.ReactNode;
   icon?: IconProps;
   tooltipIcon?: IconProps;
   tooltip?: TooltipExtendedProps;
