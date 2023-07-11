62db42f65142c2f4a49467f63dd3d6f2bf50ccb5
Artur Z <az-166@synerise.com>
2023-07-11 18:13:01 +0200

    fix(ds-tags): split value and type export

--- packages/components/tags/src/index.ts
+++ packages/components/tags/src/index.ts
@@ -1,4 +1,5 @@
 export { default } from './Tags';
 export { default as Tag } from './Tag/Tag';
 export * as TagsStyles from './Tags.styles';
-export type { TagShape, Props as TagProps } from './Tag/Tag.types';
+export { TagShape } from './Tag/Tag.types';
+export type { Props as TagProps } from './Tag/Tag.types';
