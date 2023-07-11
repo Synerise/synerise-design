443031a23d7656c0f692dcf6172013ad2eb20a7e
Artur Z <az-166@synerise.com>
2023-07-11 13:49:42 +0200

    fix(ds-tags): specify that export is exported

--- packages/components/tags/src/index.ts
+++ packages/components/tags/src/index.ts
@@ -1,4 +1,4 @@
 export { default } from './Tags';
 export { default as Tag } from './Tag/Tag';
 export * as TagsStyles from './Tags.styles';
-export { TagShape, Props as TagProps } from './Tag/Tag.types';
+export type { TagShape, Props as TagProps } from './Tag/Tag.types';
