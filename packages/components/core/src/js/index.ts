dd3faca8494c35d9ca761d2e7ed0a8cf08a14cb0
Artur Z <az-166@synerise.com>
2023-07-13 15:47:21 +0200

    fix(core): export default vs export const default

--- packages/components/core/src/js/index.ts
+++ packages/components/core/src/js/index.ts
@@ -1,5 +1,5 @@
 export { default as DSProvider } from './DSProvider';
 export { default as mediaQuery } from './mediaQuery/mediaQuery';
-export { default as theme, useTheme, defaultColorsOrder } from './DSProvider/ThemeProvider/theme';
+export { theme, useTheme, defaultColorsOrder } from './DSProvider/ThemeProvider/theme';
 
 export type { ThemePropsVars, ThemeProps, WithTheme, DefaultColor } from './DSProvider/ThemeProvider/theme';
