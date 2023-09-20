2b0c37f5f905880bc8586f4a404996549bf50c0f
malgorzata.wierzbicka <malgorzata.wierzbicka@synerise.com>
2023-09-20 10:20:13 +0000

    fix(menu): added styled wrappers to exports

--- packages/components/menu/src/index.ts
+++ packages/components/menu/src/index.ts
@@ -1,4 +1,22 @@
+import * as MainMenuStyles from './Menu.styles';
+import * as BreadcrumbStyles from './Elements/Breadcrumb/Breadcrumb.styles';
+import * as HeaderStyles from './Elements/Header/Header.styles';
+import * as ItemDangerStyles from './Elements/Item/Danger/Danger.styles';
+import * as ItemSelectStyles from './Elements/Item/Select/Select.styles';
+import * as ItemSubmenuTextStyles from './Elements/Item/SubmenuText/SubmenuText.styles';
+import * as ItemTextStyles from './Elements/Item/Text/Text.styles';
+
 export { default } from './Menu';
 export * from './Menu.types';
 export * from './Elements/Item/MenuItem.types';
 export * from './utils';
+
+export const MenuStyles = {
+  MenuStyles: MainMenuStyles,
+  Breadcrumb: BreadcrumbStyles,
+  Header: HeaderStyles,
+  ItemDanger: ItemDangerStyles,
+  ItemSelect: ItemSelectStyles,
+  ItemSubmenuText: ItemSubmenuTextStyles,
+  ItemText: ItemTextStyles,
+};
