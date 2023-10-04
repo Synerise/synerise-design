07317bf916319faedf9fd7837c6ede49a693f5cf
malgorzata.wierzbicka <malgorzata.wierzbicka@synerise.com>
2023-10-04 18:34:21 +0000

    ds-divider: adds support for label above & below divider

---
+++ packages/components/divider/src/Divider.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Title } from '@synerise/ds-typography';
+
+export const Label = styled(Title)`
+  text-transform: uppercase;
+  color: ${({ theme }): string => theme.palette['grey-500']};
+  height: 16px;
+  margin: 12px;
+  line-height: 1.6;
+  letter-spacing: 0.1px;
+`;
--- packages/components/divider/src/Divider.tsx
+++ packages/components/divider/src/Divider.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
+import React, {} from 'react';
 import '@synerise/ds-core/dist/js/style';
 import './style/index.less';
 import AntdDivider from 'antd/lib/divider';
+import * as S from './Divider.styles';
 import { DividerProps } from './Divider.types';
 
-const Divider: React.FC<DividerProps> = ({ marginBottom, marginTop, style, ...antdDividerProps }) => {
-  return (
+const Divider = ({ marginBottom, marginTop, style, labelAbove, labelBelow, ...antdDividerProps }: DividerProps) => {
+  const divider = (
     <AntdDivider
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...antdDividerProps}
@@ -16,6 +17,15 @@ const Divider: React.FC<DividerProps> = ({ marginBottom, marginTop, style, ...an
       }}
     />
   );
+  const contentAbove = labelAbove && <S.Label level={7}>{labelAbove}</S.Label>;
+  const contentBelow = labelBelow && <S.Label level={7}>{labelBelow}</S.Label>;
+  return (
+    <>
+    {contentAbove}
+    {divider}
+    {contentBelow}
+    </>
+  )
 };
 
 export default Divider;
