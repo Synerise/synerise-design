a6870ccfc98f778ff93647a2e30262524a9e5737
Malgorzata Wierzbicka <malgorzata.wierzbicka@synerise.com>
2024-02-07 19:13:31 +0100

    fix(empty-states): conditional rendering of optional props

--- packages/components/empty-states/src/EmptyStates.types.ts
+++ packages/components/empty-states/src/EmptyStates.types.ts
@@ -1,18 +1,21 @@
-import * as React from 'react';
+import { ReactElement, ReactNode } from 'react';
 
 export type EmptyStatesProps = {
   size?: EmptyStatesSize;
   fontSize?: EmptyStatesSize;
-  text?: string | React.ReactNode;
-  button?: string | React.ReactNode;
-  label?: string | React.ReactNode;
+  text?: ReactNode;
+  button?: ReactNode;
+  label?: ReactNode;
   labelPosition?: 'bottom' | 'right';
   mode?: 'absolute';
-  customIcon?: React.ReactElement;
+  customIcon?: ReactElement;
+  className?: string;
 };
+
 export enum EmptyStatesSize {
   SMALL = 'small',
   MEDIUM = 'medium',
 }
+
 export const IconSize = { [EmptyStatesSize.SMALL]: 48, [EmptyStatesSize.MEDIUM]: 96 };
 export const FontSize = { [EmptyStatesSize.SMALL]: 14, [EmptyStatesSize.MEDIUM]: 18 };
