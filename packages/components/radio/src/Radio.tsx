3d91be15c44e6ef883c5707a7b40b4abf6cd7eaf
malgorzata.wierzbicka <malgorzata.wierzbicka@synerise.com>
2024-03-12 11:52:39 +0000

    fix(radio): fixed description type

--- packages/components/radio/src/Radio.tsx
+++ packages/components/radio/src/Radio.tsx
@@ -5,12 +5,7 @@ import AntdRadio, { RadioGroupProps } from 'antd/lib/radio';
 import * as S from './Radio.styles';
 import { Props } from './Radio.types';
 
-const Group: React.FC<RadioGroupProps & { fullWidth?: boolean; big?: boolean }> = ({
-  children,
-  fullWidth,
-  big,
-  ...props
-}) => {
+const Group = ({ children, fullWidth, big, ...props }: RadioGroupProps & { fullWidth?: boolean; big?: boolean }) => {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <S.AntRadioGroup fullWidth={fullWidth} big={big} {...props}>
@@ -19,23 +14,17 @@ const Group: React.FC<RadioGroupProps & { fullWidth?: boolean; big?: boolean }> 
   );
 };
 
-class Radio extends React.Component<Props> {
-  static Group = Group;
-  static Button = AntdRadio.Button;
+const RadioComponent = ({ description, ...antdRadioButtonProps }: Props) => {
+  return (
+    <S.RadioWrapper>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <AntdRadio {...antdRadioButtonProps} />
+      <S.AdditionalData>
+        {description && <S.Description disabled={antdRadioButtonProps.disabled}>{description}</S.Description>}
+      </S.AdditionalData>
+    </S.RadioWrapper>
+  );
+};
 
-  render(): React.ReactNode {
-    const { description, ...antdRadioButtonProps } = this.props;
-
-    return (
-      <S.RadioWrapper>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <AntdRadio {...antdRadioButtonProps} />
-        <S.AdditionalData>
-          {description && <S.Description disabled={antdRadioButtonProps.disabled}>{description}</S.Description>}
-        </S.AdditionalData>
-      </S.RadioWrapper>
-    );
-  }
-}
-
+const Radio = Object.assign(RadioComponent, { Group, Button: AntdRadio.Button });
 export default Radio;
