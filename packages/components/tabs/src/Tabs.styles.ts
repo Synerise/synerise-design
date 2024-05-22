ac08ed30b7541b327e91af00050b80b0e82ba110
malgorzata.wierzbicka <malgorzata.wierzbicka@synerise.com>
2024-05-22 06:46:59 +0000

    fix(tabs): fixed layout issues

--- packages/components/tabs/src/Tabs.styles.ts
+++ packages/components/tabs/src/Tabs.styles.ts
@@ -5,7 +5,7 @@ import Menu from '@synerise/ds-menu';
 import { AntdMenuProps } from '@synerise/ds-menu/dist/Menu.types';
 
 export const TabsContainer = styled.div<{ block?: boolean }>`
-  margin-top: 5px;
+  padding-top: 5px;
   display: flex;
   flex-direction: row;
   align-items: ${(props): string | false => (props.block ? `center` : `flex-end`)};
@@ -13,7 +13,7 @@ export const TabsContainer = styled.div<{ block?: boolean }>`
   max-width: 100%;
   overflow-x: hidden;
   margin-bottom: -1px;
-  height: 38px;
+  height: 43px;
 `;
 
 export const TabsDropdownContainer = styled.div`
