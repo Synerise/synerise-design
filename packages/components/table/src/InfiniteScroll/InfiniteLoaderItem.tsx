455929012ab463980f0dd5de7230636a0ee058b6
malgorzata.wierzbicka <malgorzata.wierzbicka@synerise.com>
2024-06-28 09:58:30 +0000

    fix(table): infinite loader - trigger loading without scrolling

--- packages/components/table/src/InfiniteScroll/InfiniteLoaderItem.tsx
+++ packages/components/table/src/InfiniteScroll/InfiniteLoaderItem.tsx
@@ -1,65 +1,29 @@
-import * as React from 'react';
-import Alert from '@synerise/ds-alert';
-import Button from '@synerise/ds-button';
-import Icon, { RefreshM } from '@synerise/ds-icon';
-import { TableLocaleContext } from '../utils/locale';
-import { Loader } from './InfiniteLoaderItem.styles';
+import React, { useEffect } from 'react';
 import { LoaderItemProps } from './InfiniteLoaderItem.types';
 import { infiniteScrollPropsToState } from './utils';
+import { ErrorItem } from './ErrorItem';
+import { LoadingItem } from './LoadingItem';
+import { NoMoreItem } from './NoMoreItem';
 
-const LoadingItem = (): React.ReactElement => (
-  <TableLocaleContext.Consumer>
-    {(tableLocale): React.ReactElement => <Loader size="M" label={tableLocale.infiniteScrollLoading} />}
-  </TableLocaleContext.Consumer>
-);
-
-const NoMoreItem = (): React.ReactElement => (
-  <TableLocaleContext.Consumer>
-    {(tableLocale): React.ReactElement => (
-      <Alert.InlineAlert
-        // @ts-ignore
-        type="info"
-        message={tableLocale.infiniteScrollNoMoreData}
-      />
-    )}
-  </TableLocaleContext.Consumer>
-);
-
-interface ErrorItemProps {
-  onRetryClick?: () => void;
-}
-
-const ErrorItem = ({ onRetryClick }: ErrorItemProps): React.ReactElement => (
-  <TableLocaleContext.Consumer>
-    {(tableLocale): React.ReactElement => (
-      <div style={{ display: 'flex' }}>
-        <Alert.InlineAlert
-          // @ts-ignore
-          type="alert"
-          message={tableLocale.infiniteScrollError}
-        />
-        {onRetryClick && (
-          <Button
-            onClick={onRetryClick}
-            type="ghost"
-            mode="icon-label"
-            icon={<Icon component={<RefreshM />} />}
-            style={{ marginLeft: 8 }}
-          >
-            {tableLocale.infiniteScrollRetry}
-          </Button>
-        )}
-      </div>
-    )}
-  </TableLocaleContext.Consumer>
-);
-
-const InfiniteLoaderItem = ({ infiniteScroll, position }: LoaderItemProps): React.ReactElement => {
-  const { hasMore, isLoading, hasError, onRetryButtonClick, render } = infiniteScroll;
+const InfiniteLoaderItem = ({ infiniteScroll, position }: LoaderItemProps) => {
+  const { hasMore, isLoading, onScrollEndReach, hasError, onRetryButtonClick, render } = infiniteScroll;
 
   const handleRetryClick = () => {
     onRetryButtonClick && onRetryButtonClick(position);
   };
+
+  // @ts-expect-error TS7030: Not all code paths return a value
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (!isLoading && hasMore) {
+      if ((!position || position === 'BOTTOM') && typeof onScrollEndReach === 'function') {
+        const timeout = setTimeout(onScrollEndReach, 0);
+        return () => {
+          clearTimeout(timeout);
+        };
+      }
+    }
+  }, [hasMore, isLoading, onScrollEndReach, position]);
 
   if (typeof render === 'function') {
     return render(infiniteScrollPropsToState(infiniteScroll));
