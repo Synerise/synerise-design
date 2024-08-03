985e47d518372f1cd268da6e4eb8bd10c6149695
Malgorzata Wierzbicka <malgorzata.wierzbicka@synerise.com>
2024-08-03 11:21:58 +0200

    test(data-format): remaingin timezone tests added
    affects: @synerise/ds-data-format

---
+++ packages/components/data-format/src/__specs__/Timezones.data.ts
@@ -0,0 +1,38 @@
+export const TZ_DATE_TO_FORMAT = new Date('2023-06-25T16:40:00Z');
+export const TIMEZONE_DATA = [
+  {
+    timeZone: 'Europe/Warsaw', // +02:00
+    EUDate: '25.06.2023',
+    EUTime: '18:40',
+    USDate: '6/25/2023',
+    USTime: '6:40 PM',
+  },
+  {
+    timeZone: 'UTC', // +00:00
+    EUDate: '25.06.2023',
+    EUTime: '16:40',
+    USDate: '6/25/2023',
+    USTime: '4:40 PM',
+  },
+  {
+    timeZone: 'America/New_York', // -04:00
+    EUDate: '25.06.2023',
+    EUTime: '12:40',
+    USDate: '6/25/2023',
+    USTime: '12:40 PM',
+  },
+  {
+    timeZone: 'America/Nome', // -08:00
+    EUDate: '25.06.2023',
+    EUTime: '08:40',
+    USDate: '6/25/2023',
+    USTime: '8:40 AM',
+  },
+  {
+    timeZone: 'Australia/Darwin', // +09:30
+    EUDate: '26.06.2023',
+    EUTime: '02:10',
+    USDate: '6/26/2023',
+    USTime: '2:10 AM',
+  },
+];
