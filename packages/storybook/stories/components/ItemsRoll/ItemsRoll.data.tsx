04f1d95b5b8f62c1e1a881f31ec8f70ae87bae88
malgorzata.wierzbicka <malgorzata.wierzbicka@synerise.com>
2024-09-16 11:30:16 +0000

    feat(storybook7): items roll stories

---
+++ packages/storybook/stories/components/ItemsRoll/ItemsRoll.data.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { v4 as uuid } from 'uuid';
+
+import Icon, { FileTypeTableM, FileDownloadM, ExpressionM, AggregateM, ParamsBadgeM, Add3M } from '@synerise/ds-icon';
+import Tooltip from '@synerise/ds-tooltip';
+import { theme } from '@synerise/ds-core';
+
+export const SEARCH_PLACEHOLDER = 'Search...';
+export const TEXT = 'Example item';
+
+export const hundredItems = Array.from(Array(100).keys());
+export const thousandItems = Array.from(Array(1000).keys());
+
+export const ITEMS_100 = hundredItems.map(key => ({
+  id: uuid(),
+  text: `${TEXT}-${key}`,
+}));
+
+export const ITEMS_1000 = thousandItems.map(key => ({
+  id: uuid(),
+  text: `${TEXT}-${key}`,
+}));
+
+const tenParams = Array.from(Array(13).keys()).map(key => ({
+  group: 'Parameter',
+  id: uuid(),
+  text: `Parameter-${key}`,
+  prefixel: (
+    <Tooltip title="Parameter">
+      <div>
+        <Icon component={<ParamsBadgeM />} size={20} color={theme.palette['red-600']} />
+      </div>
+    </Tooltip>
+  ),
+}));
+
+const tenAggregates = Array.from(Array(25).keys()).map(key => ({
+  group: 'Aggregate',
+  id: uuid(),
+  text: `Aggregate-${key}`,
+  prefixel: (
+    <Tooltip title="Aggregate">
+      <div>
+        <Icon component={<AggregateM />} size={20} color={theme.palette['red-600']} />
+      </div>
+    </Tooltip>
+  ),
+}));
+
+const tenExpressions = Array.from(Array(6).keys()).map(key => ({
+  group: 'Expression',
+  id: uuid(),
+  text: `Expression-${key}`,
+  prefixel: (
+    <Tooltip title="Expression">
+      <div>
+        <Icon component={<ExpressionM />} size={20} color={theme.palette['red-600']} />
+      </div>
+    </Tooltip>
+  ),
+}));
+
+export const GROUPED_ITEMS = {
+  items: [...tenParams, ...tenAggregates, ...tenExpressions],
+  groups: ['Parameter', 'Aggregate', 'Expression'],
+};
+
+export const ACTIONS = [
+  {
+    id: uuid(),
+    onClick: action('OnImportClick'),
+    text: 'Import',
+    prefixel: <Icon component={<FileTypeTableM />} color={theme.palette['grey-600']} />,
+  },
+  {
+    id: uuid(),
+    onClick: action('OnExportClick'),
+    text: 'Export',
+    prefixel: <Icon component={<FileDownloadM color={theme.palette['grey-600']} />} />,
+  },
+];
+
+export const ICONS = {
+  default: undefined,
+  add3M: <Add3M />,
+};
---
+++ packages/storybook/stories/components/ItemsRoll/ItemsRoll.stories.tsx
@@ -0,0 +1,175 @@
+import React, { useState } from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import ItemsRoll, { ItemsRollProps } from '@synerise/ds-items-roll';
+import Dropdown from '@synerise/ds-dropdown';
+import { focusWithArrowKeys } from '@synerise/ds-utils';
+import Menu from '@synerise/ds-menu';
+import Button from '@synerise/ds-button';
+import Icon, { SaveM } from '@synerise/ds-icon';
+import { theme } from '@synerise/ds-core';
+
+import { ACTIONS, GROUPED_ITEMS, ICONS, ITEMS_100, ITEMS_1000, SEARCH_PLACEHOLDER } from './ItemsRoll.data';
+import {
+  BOOLEAN_CONTROL,
+  centeredPaddedWrapper,
+  controlFromOptionsArray,
+  fixedWrapper800,
+  NUMBER_CONTROL,
+  STRING_CONTROL,
+} from '../../utils';
+
+type StoryType = ItemsRollProps & {
+  withChangeSelectionDropdown: boolean;
+};
+type Story = StoryObj<StoryType>;
+
+export default {
+  component: ItemsRoll,
+  title: 'Components/ItemsRoll',
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [fixedWrapper800, centeredPaddedWrapper],
+  render: ({ withChangeSelectionDropdown, ...args }) => {
+    const [items, setItems] = useState(args.items);
+    const [searchValue, setSearchValue] = useState('');
+
+    const onClearAll = () => {
+      setSearchValue('');
+      setItems([]);
+      args.onClearAll?.();
+    };
+
+    const onSearchClear = () => {
+      setSearchValue('');
+      setItems(args.items);
+      args.onSearchClear?.();
+    };
+
+    const onItemRemove = (itemId: string, grouId?: string) => {
+      setItems(items.filter(item => item.id !== itemId));
+      args.onItemRemove?.(itemId, grouId);
+    };
+
+    const onSearch = (value: string) => {
+      const lookupItems = args.items;
+
+      if (searchValue !== value) {
+        const filteredItems = lookupItems.filter(item => `${item.text}`.toLowerCase().includes(value.toLowerCase()));
+        setSearchValue(value);
+        setItems(filteredItems);
+      }
+      if (value === '') {
+        setItems(lookupItems);
+      }
+      args.onSearch?.(value);
+    };
+
+    const [visible, setVisible] = useState(false);
+    const changeSelectionDropdownProps = withChangeSelectionDropdown
+      ? {
+          overlay: (
+            <Dropdown.Wrapper
+              style={{ width: '157px' }}
+              onKeyDown={event => focusWithArrowKeys(event, 'ds-menu-item', () => {})}
+            >
+              <Menu
+                dataSource={[{ text: 'Option 1' }, { text: 'Option 2' }]}
+                asDropdownMenu={true}
+                style={{ width: '100%' }}
+              />
+            </Dropdown.Wrapper>
+          ),
+          trigger: ['click' as const],
+          visible,
+          onVisibleChange: () => setVisible(!visible),
+        }
+      : undefined;
+
+    return (
+      <ItemsRoll
+        {...args}
+        onItemRemove={onItemRemove}
+        onSearch={onSearch}
+        onSearchClear={onSearchClear}
+        items={items}
+        onClearAll={onClearAll}
+        searchValue={searchValue}
+        changeSelectionDropdownProps={changeSelectionDropdownProps}
+      />
+    );
+  },
+  argTypes: {
+    maxToShowItems: NUMBER_CONTROL,
+    showMoreStep: NUMBER_CONTROL,
+    useFooter: BOOLEAN_CONTROL,
+    useVirtualizedList: BOOLEAN_CONTROL,
+    hideSearch: BOOLEAN_CONTROL,
+    searchPlaceholder: STRING_CONTROL,
+    items: {
+      control: false,
+    },
+    changeSelectionIcon: {
+      ...controlFromOptionsArray('select', Object.keys(ICONS)),
+      mapping: ICONS,
+    },
+  },
+  args: {
+    actions: ACTIONS,
+    items: ITEMS_100,
+    searchPlaceholder: SEARCH_PLACEHOLDER,
+    maxToShowItems: 10,
+    showMoreStep: 10,
+    useFooter: true,
+    useVirtualizedList: false,
+    hideSearch: false,
+    withChangeSelectionDropdown: false,
+  },
+} as Meta<StoryType>;
+
+export const Default: Story = {};
+
+export const Empty: Story = {
+  args: {
+    items: [],
+  },
+};
+
+export const VirtualisedList: Story = {
+  args: {
+    items: ITEMS_1000,
+  },
+};
+
+export const Grouped: Story = {
+  args: {
+    items: GROUPED_ITEMS.items,
+    groups: GROUPED_ITEMS.groups,
+  },
+};
+
+export const ChangeSelectionDropdown: Story = {
+  args: {
+    withChangeSelectionDropdown: true,
+  },
+};
+
+export const CustomSidebarActions: Story = {
+  args: {
+    customSidebarActions: (
+      <div style={{ display: 'flex', marginRight: '8px' }}>
+        <Button
+          mode="icon-label"
+          type="ghost"
+          icon={<Icon component={<SaveM />} color={theme.palette['grey-600']} />}
+          onClick={action('Click custom action')}
+        >
+          Save list
+        </Button>
+      </div>
+    ),
+  },
+};
