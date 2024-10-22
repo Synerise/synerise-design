b67688632a30a8e66286c70211018b4cc0f4c994
Jakub Sidor <jakub.sidor@synerise.com>
2024-10-22 09:27:38 +0000

    Feature/new stories pagination

---
+++ packages/storybook/stories/components/Pagination/Pagination.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import type { StoryObj, Meta } from '@storybook/react';
+
+import Pagination from '@synerise/ds-pagination';
+
+import {
+  centeredPaddedWrapper,
+  CLASSNAME_ARG_CONTROL,
+  NUMBER_CONTROL,
+} from '../../utils';
+
+
+export default {
+  title: "Components/Pagination",
+  tags: ['autodocs'],
+  component: Pagination,
+  decorators: [centeredPaddedWrapper],
+  argTypes: {
+    className: CLASSNAME_ARG_CONTROL,
+    defaultCurrent: NUMBER_CONTROL,
+    total: NUMBER_CONTROL,
+  },
+} as Meta<typeof Pagination>;
+
+type Story = StoryObj<typeof Pagination>;
+
+export const Default: Story = {
+  args: {
+    default: 1,
+    total: 5000,
+  },
+};
