3d0ff442c2bb6da9c8bdd4505637ded4ed759e95
malgorzata.wierzbicka <malgorzata.wierzbicka@synerise.com>
2024-11-28 15:07:10 +0000

    fix(icon): color icons export missing

--- packages/components/icon/src/index.ts
+++ packages/components/icon/src/index.ts
@@ -3,6 +3,7 @@ export * from './Icon.types';
 export * from './Icon.styles';
 export * from './icons';
 export * from './icons/L';
+export * from './icons/colorIcons';
 export * from './icons/XL';
 // Some of this icons are duplicated in XL folder, consider removing it before uploading new version
 export * from './icons/additional';
