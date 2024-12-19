6775a73e15d884a575bcabc0101545450a0be659
Jakub Sidor <jakub.sidor@synerise.com>
2024-12-19 13:50:50 +0000

    feat(message): new stories for message

---
+++ packages/storybook/stories/components/Message/Message.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+
+import message from '@synerise/ds-message';
+import Button from '@synerise/ds-button';
+
+
+
+export default {
+  title: 'Components/Message',
+  component: message,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  render: args => {
+    return (
+      <Button
+        {...args}
+        onClick={() => message.success('Thanks!')}
+      >
+        Click me!
+      </Button>
+    );
+  },
+  argTypes: {
+  },
+
+} as Meta<typeof message>;
+
+type Story = StoryObj<typeof message>;
+
+export const Default: Story = {};
