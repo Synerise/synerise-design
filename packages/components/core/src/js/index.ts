ba996d4e9fe57d0152479f47cb4725e852690e10
Malgorzata Wierzbicka <malgorzata.wierzbicka@synerise.com>
2025-02-07 16:17:16 +0100

    chore(core): test tgz packing
    affects: @synerise/ds-core

--- packages/components/core/src/js/index.ts
+++ packages/components/core/src/js/index.ts
@@ -1,5 +1,4 @@
 export { default as DSProvider } from './DSProvider';
 export { default as mediaQuery } from './mediaQuery/mediaQuery';
 export { theme, useTheme, defaultColorsOrder } from './DSProvider/ThemeProvider/theme';
-
 export type { ThemePropsVars, ThemeProps, WithTheme, DefaultColor } from './DSProvider/ThemeProvider/theme';
