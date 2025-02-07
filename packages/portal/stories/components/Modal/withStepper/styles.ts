cad126f480faf6f28c99864e1e17421d44ce1ecb
Malgorzata Wierzbicka <malgorzata.wierzbicka@synerise.com>
2025-02-07 09:16:20 +0100

    chore: remove storybook6.5
    affects: @synerise/ds-action-area, @synerise/ds-alert, @synerise/ds-app-menu,
    @synerise/ds-autocomplete, @synerise/ds-avatar-group, @synerise/ds-avatar, @synerise/ds-badge,
    @synerise/ds-banner, @synerise/ds-block, @synerise/ds-button-group, @synerise/ds-button,
    @synerise/ds-card-select, @synerise/ds-card-tabs, @synerise/ds-card, @synerise/ds-cascader,
    @synerise/ds-checkbox-tristate, @synerise/ds-checkbox, @synerise/ds-code-area,
    @synerise/ds-code-snippet, @synerise/ds-collector, @synerise/ds-color-picker,
    @synerise/ds-column-manager, @synerise/ds-completed-within, @synerise/ds-condition,
    @synerise/ds-context-selector, @synerise/ds-core, @synerise/ds-cruds, @synerise/ds-data-format,
    @synerise/ds-date-picker, @synerise/ds-date-range-picker, @synerise/ds-description,
    @synerise/ds-divider, @synerise/ds-drawer, @synerise/ds-dropdown, @synerise/ds-editable-items-list,
    @synerise/ds-empty-states, @synerise/ds-factors, @synerise/ds-field-set, @synerise/ds-file-uploader,
    @synerise/ds-filter, @synerise/ds-flag, @synerise/ds-flex-box, @synerise/ds-footer,
    @synerise/ds-form, @synerise/ds-format-picker, @synerise/ds-grid, @synerise/ds-icon-picker,
    @synerise/ds-icon, @synerise/ds-information-card, @synerise/ds-inline-edit,
    @synerise/ds-input-number, @synerise/ds-input, @synerise/ds-item-filter, @synerise/ds-item-picker,
    @synerise/ds-items-roll, @synerise/ds-layout, @synerise/ds-list-item, @synerise/ds-list,
    @synerise/ds-loader, @synerise/ds-logic, @synerise/ds-manageable-list, @synerise/ds-menu,
    @synerise/ds-message, @synerise/ds-modal, @synerise/ds-navbar, @synerise/ds-operators,
    @synerise/ds-ordered-list, @synerise/ds-page-header, @synerise/ds-pagination,
    @synerise/ds-panels-resizer, @synerise/ds-popconfirm, @synerise/ds-progress-bar, @synerise/ds-radio,
    @synerise/ds-result, @synerise/ds-scrollbar, @synerise/ds-search-bar, @synerise/ds-search,
    @synerise/ds-select, @synerise/ds-short-cuts, @synerise/ds-sidebar-object, @synerise/ds-sidebar,
    @synerise/ds-skeleton, @synerise/ds-slider, @synerise/ds-status, @synerise/ds-step-card,
    @synerise/ds-stepper, @synerise/ds-subject, @synerise/ds-subtle-form, @synerise/ds-switch,
    @synerise/ds-table, @synerise/ds-tabs, @synerise/ds-tags, @synerise/ds-time-picker,
    @synerise/ds-toolbar, @synerise/ds-tooltip, @synerise/ds-typography, @synerise/ds-unordered-list,
    @synerise/ds-utils, @synerise/ds-wizard

--- packages/portal/stories/components/Modal/withStepper/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StepperWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 72px auto 200px;
-`;
