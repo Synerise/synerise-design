452ef6c5d9b651ba6d0e16013edab06b3c0592b5
malgorzata.wierzbicka <malgorzata.wierzbicka@synerise.com>
2025-06-02 11:40:26 +0000

    fix(button): button group hover styles

--- packages/components/button-group/src/ButtonDivider/ButtonDivider.styles.tsx
+++ packages/components/button-group/src/ButtonDivider/ButtonDivider.styles.tsx
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 import Divider from '@synerise/ds-divider';
 
 export const ButtonDivider = styled(Divider)`
-  border-color: ${props => props.theme.palette['grey-300']};
-  height: auto;
-  margin: 0px;
-  top: 0;
+  && {
+    border-color: ${props => props.theme.palette['grey-300']};
+    height: auto;
+    margin: 0px;
+    top: 0;
+  }
 `;
