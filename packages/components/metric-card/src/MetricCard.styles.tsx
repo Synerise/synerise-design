5fe50a7b87592990d6e4be1e04599fce350a91fd
Jakub Sidor <jakub.sidor@synerise.com>
2026-01-07 10:43:13 +0000

    feat(metric-card): new component

---
+++ packages/components/metric-card/src/MetricCard.styles.tsx
@@ -0,0 +1,118 @@
+import styled, { css } from 'styled-components';
+
+import Copy from '@synerise/ds-copy-icon';
+import InlineAlert from '@synerise/ds-inline-alert';
+import Skeleton from '@synerise/ds-skeleton';
+import { Text } from '@synerise/ds-typography';
+
+export const WrapperFormattedNumber = styled.div`
+  display: inline;
+`;
+
+export const WrapperNumber = styled.div`
+  display: none;
+`;
+
+export const MetricContainer = styled.div<{ greyBackground?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.palette.white};
+  padding: 8px 8px 14px 24px;
+  gap: 2px;
+  width: 100%;
+  border-radius: 5px;
+  ${(props) =>
+    props.greyBackground
+      ? `
+         box-shadow: 0 4px 12px 0 rgba(35, 41, 54, 0.04);`
+      : `
+         box-shadow: 0 0 0 1px ${props.theme.palette['grey-200']}`};
+`;
+export const CopyIcon = styled(Copy)`
+  margin-left: 8px;
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.15s ease;
+`;
+
+export const MetricSkeleton = styled(Skeleton)`
+  padding: 0 0 2px;
+  width: 50%;
+`;
+
+export const MetricInlineAlert = styled(InlineAlert)`
+  padding: 8px 0 2px;
+`;
+
+export const MetricContent = styled.div<{ copyable?: boolean }>`
+  flex: 1;
+  min-width: 0;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  margin: 0 4px 2px 0;
+  ${CopyIcon} {
+    ${(props) => {
+      return (
+        props.copyable &&
+        css`
+          cursor: pointer;
+        `
+      );
+    }}
+  }
+  &.hovered {
+    ${CopyIcon} {
+      visibility: visible;
+      opacity: 1;
+    }
+    ${WrapperFormattedNumber} {
+      display: none;
+    }
+    ${WrapperNumber} {
+      display: inline;
+    }
+  }
+`;
+export const MetricValue = styled(Text)`
+  color: ${(props): string => props.theme.palette['grey-800']};
+  font-size: 32px;
+  line-height: 100%;
+  padding-bottom: 2px;
+  align-items: center;
+  cursor: pointer;
+  font-weight: 500;
+  justify-content: start;
+`;
+
+export const MetricHeaderBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TitleWrapper = styled.div<{ header?: boolean }>`
+  flex: 1;
+  min-width: 0;
+  display: flex;
+  align-items: center;
+  min-height: 32px;
+`;
+
+export const IconWrapper = styled.span`
+  display: inline-block;
+  color: ${(props): string => props.theme.palette['grey-400']};
+  cursor: pointer;
+`;
+
+export const HeaderRightSide = styled.div`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+`;
+
+export const WrapperCopyIcon = styled.div`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+`;
---
+++ packages/storybook/stories/components/MetricCard/MetricCard.data.tsx
@@ -0,0 +1,6 @@
+export const COMPACT_NUMBER_OPTIONS = {
+  targetFormat: 'compact-larger-number',
+  compactDisplay: 'short',
+  maximumFractionDigits: 1,
+  minimumFractionDigits: 0,
+} as const;
